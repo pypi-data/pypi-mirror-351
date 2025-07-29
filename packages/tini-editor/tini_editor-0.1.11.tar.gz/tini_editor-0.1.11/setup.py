1ef6b10a926ca3b89415742e464b2f9fab9e86ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:28 +0000

    Add package tini-editor/0.1.11/tini_editor-0.1.11.tar.gz

---
+++ packages/tini-editor/tini_editor-0.1.11.tar.gz/tini_editor-0.1.11/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+def parse_requirements(filename):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, filename), 'r') as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith('#')]
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="tini-editor",
+    version="0.1.11",
+    packages=find_packages(),
+    install_requires=parse_requirements('requirements.txt'),
+    entry_points={
+        "console_scripts": [
+            "tini=tini.app:main",
+        ],
+    },
+    author="WolfQuery",
+    description="A tiny terminal text editor built with Textual",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=["textual", "terminal", "text editor", "nano", "tini"],
+    url="https://codeberg.org/WolfQuery/tini-editor",
+    license="CC-BY-NC-SA-4.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+    ],
+    python_requires=">=3.12",
+    license_files = ("LICENSE",),
+)
