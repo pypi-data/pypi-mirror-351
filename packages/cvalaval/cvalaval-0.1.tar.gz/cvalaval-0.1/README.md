e5f24b4c6f4e0b528d8f92ee5aafada92bbf57b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:49 +0000

    Add package cvalaval/0.1/cvalaval-0.1.tar.gz

---
+++ packages/cvalaval/cvalaval-0.1.tar.gz/cvalaval-0.1/README.md
@@ -0,0 +1,2 @@
+# Project Name
+A library with fast, comutational math assets (mostly for personal use).
---
+++ packages/cvalaval/cvalaval-0.1.tar.gz/cvalaval-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cvalaval',
+    version='0.1',
+    packages=find_packages(),
+    description='Fast math computations and helpers',
+    author='Airwolftomato',
+    python_requires='>=3.7',
+)
