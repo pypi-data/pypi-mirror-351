dc29cfb5a629741a62db0066200fc9c15ecba94c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:21 +0000

    Add package api-swedeb/0.2.9/api_swedeb-0.2.9-py3-none-any.whl

---
+++ packages/api-swedeb/api_swedeb-0.2.9-py3-none-any.whl/api_swedeb/schemas/metadata_schema.py
@@ -0,0 +1,83 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+
+
+class Year(BaseModel):
+    year: str = Field(pattern=r"^\d{4}$")
+
+
+class PartyItem(BaseModel):
+    party_id: int
+    party: str
+    party_abbrev: str
+    party_color: str
+
+
+class PartyList(BaseModel):
+    party_list: List[PartyItem]
+
+
+class GenderItem(BaseModel):
+    gender_id: int
+    gender: str
+    gender_abbrev: str
+
+
+class GenderList(BaseModel):
+    gender_list: List[GenderItem]
+
+
+class SearchHits(BaseModel):
+    """When doing a search for word trends, this endpoint returs a list of hits for the search term
+        One search can return zero, one or multiple hits in the corpus
+    Args:
+        BaseModel (_type_): _description_
+    """
+
+    hit_list: List[str]
+
+
+class ChamberItem(BaseModel):
+    chamber_id: int
+    chamber: str
+    chamber_abbrev: str = None
+
+
+class ChamberList(BaseModel):
+    chamber_list: List[ChamberItem]
+
+
+class OfficeTypeItem(BaseModel):
+    office_type_id: int
+    office: str
+
+
+class OfficeTypeList(BaseModel):
+    office_type_list: List[OfficeTypeItem]
+
+
+class SubOfficeTypeItem(BaseModel):
+    sub_office_type_id: int
+    office_type_id: int
+    identifier: str | None
+
+
+class SubOfficeTypeList(BaseModel):
+    sub_office_type_list: List[SubOfficeTypeItem]
+
+
+class Genders(BaseModel):
+    genders: List[str]
+
+
+class SpeakerItem(BaseModel):
+    name: str
+    party_abbrev: str  # TODO change to List[str] or otherwise handle multiple parties
+    year_of_birth: int = None
+    year_of_death: int = None
+    person_id: str = None
+
+
+class SpeakerResult(BaseModel):
+    speaker_list: List[SpeakerItem]
---
+++ packages/api-swedeb/api_swedeb-0.2.9-py3-none-any.whl/api_swedeb/schemas/speeches_schema.py
@@ -0,0 +1,32 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+
+
+class SpeechesResultItem(BaseModel):
+    name: str = Field(None, description="Name of speaker")
+    year: int = Field(None, description="Year of speech", examples=[1960])
+    gender: str = Field(None, description="Gender of speaker")
+    gender_abbrev: str = Field(None, description="Gender of speaker")
+    party_abbrev: str = Field(None, description="Party of speaker")
+    party: str = Field(None, description="Full party name of speaker")
+    speech_link: str = Field(None, description="Source of speech")
+    document_name: str = Field(None, description="Unique id of speech")
+    link: str = Field(None, description="Link to the speaker")
+    speech_name: str = Field(None, description="Formatted speech id")
+    chamber_abbrev: str = Field(None, description="Chamber of speech")
+    speech_id: str = Field(None, description="Unique id of speech")
+    wiki_id: str = Field(None, description="Wiki id of speaker")
+    document_id: int = Field(None, description="Document system id")
+
+
+class SpeechesResult(BaseModel):
+    speech_list: List[SpeechesResultItem]
+
+
+class SpeechesResultItemWT(SpeechesResultItem):
+    node_word: str = Field(None, description="Search hit in speech")
+
+
+class SpeechesResultWT(BaseModel):
+    speech_list: List[SpeechesResultItemWT]
