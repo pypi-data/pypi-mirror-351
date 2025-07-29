4d8ef022ca48952bfa040fd48d670876446f7dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:07:29 +0000

    Add package baresquare-sdk/0.1.2/baresquare_sdk-0.1.2.tar.gz

---
+++ packages/baresquare-sdk/baresquare_sdk-0.1.2.tar.gz/baresquare_sdk-0.1.2/baresquare_sdk/__init__.py
@@ -0,0 +1,16 @@
+"""Baresquare SDK for Python.
+
+This package provides core utilities and AWS integrations for Baresquare services.
+"""
+
+# Make submodules available for import
+from . import core
+
+# Only import aws if dependencies are available
+try:
+    from . import aws
+except ImportError:
+    # AWS dependencies not installed
+    aws = None
+
+__all__ = ["core", "aws"]
