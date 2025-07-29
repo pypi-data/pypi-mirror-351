eeb46386472b4ab78d1832914ce90c8ef2b2564f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:26 +0000

    Add package catzilla/0.1.0/catzilla-0.1.0-cp39-cp39-win_amd64.whl

---
+++ packages/catzilla/catzilla-0.1.0-cp39-cp39-win_amd64.whl/catzilla/__init__.py
@@ -0,0 +1,22 @@
+"""
+Catzilla Web Framework
+"""
+
+from .app import App
+from .response import ResponseBuilder, response
+from .routing import Router, RouterGroup
+from .types import HTMLResponse, JSONResponse, Request, Response
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "App",
+    "Request",
+    "Response",
+    "JSONResponse",
+    "HTMLResponse",
+    "response",
+    "ResponseBuilder",
+    "Router",
+    "RouterGroup",
+]
