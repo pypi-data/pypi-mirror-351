fbf880ed7ef0886b1c29ed114bf47b316ba4022d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:08:50 +0000

    Add package chaturbate-poller/3.3.3/chaturbate_poller-3.3.3.tar.gz

---
+++ packages/chaturbate-poller/chaturbate_poller-3.3.3.tar.gz/chaturbate_poller-3.3.3/docs/conf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from chaturbate_poller import __version__
+
+project = "chaturbate_poller"
+project_copyright = "2025, MountainGod2"
+author = "MountainGod2"
+extensions: list[str] = [
+    "myst_nb",
+    "autoapi.extension",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_autodoc_typehints",
+]
+version: str = __version__
+release: str = __version__
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+autoapi_dirs: list[str] = ["../src"]
+exclude_patterns: list[str] = ["_build", "Thumbs.db", ".DS_Store"]
+html_theme = "sphinx_rtd_theme"
+linkcheck_ignore: list[str] = [
+    "https://chaturbate.com/statsapi/authtoken/",
+    r"https://github\.com/.*",
+    r"http://localhost:\d+",
+    r"file://.*",
+]
