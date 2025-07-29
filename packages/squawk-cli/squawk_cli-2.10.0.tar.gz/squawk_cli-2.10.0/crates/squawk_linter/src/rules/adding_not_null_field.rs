9828ef725eedca24a6f36938e7bd5cf07c37bb79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:24 +0000

    Add package squawk-cli/2.10.0/squawk_cli-2.10.0.tar.gz

---
+++ packages/squawk-cli/squawk_cli-2.10.0.tar.gz/squawk_cli-2.10.0/crates/squawk_linter/src/rules/adding_not_null_field.rs
@@ -0,0 +1,99 @@
+use squawk_syntax::{
+    ast::{self, AstNode},
+    Parse, SourceFile,
+};
+
+use crate::{Linter, Rule, Version, Violation};
+
+pub(crate) fn adding_not_null_field(ctx: &mut Linter, parse: &Parse<SourceFile>) {
+    if ctx.settings.pg_version >= Version::new(11, 0, 0) {
+        return;
+    }
+    let file = parse.tree();
+    for stmt in file.stmts() {
+        if let ast::Stmt::AlterTable(alter_table) = stmt {
+            for action in alter_table.actions() {
+                if let ast::AlterTableAction::AlterColumn(alter_column) = action {
+                    let Some(option) = alter_column.option() else {
+                        continue;
+                    };
+
+                    if matches!(option, ast::AlterColumnOption::SetNotNull(_)) {
+                        ctx.report(Violation::new(
+                            Rule::AddingNotNullableField,
+                            "Setting a column `NOT NULL` blocks reads while the table is scanned."
+                                .into(),
+                            option.syntax().text_range(),
+                            "Make the field nullable and use a `CHECK` constraint instead."
+                                .to_string(),
+                        ));
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use insta::assert_debug_snapshot;
+
+    use crate::{Linter, Rule, Version};
+
+    #[test]
+    fn set_not_null() {
+        let sql = r#"
+ALTER TABLE "core_recipe" ALTER COLUMN "foo" SET NOT NULL;
+        "#;
+        let file = squawk_syntax::SourceFile::parse(sql);
+        let mut linter = Linter::from([Rule::AddingNotNullableField]);
+        linter.settings.pg_version = Version::new(10, 0, 0);
+        let errors = linter.lint(file, sql);
+        assert!(!errors.is_empty());
+        assert_debug_snapshot!(errors);
+    }
+
+    #[test]
+    fn adding_field_that_is_not_nullable() {
+        let sql = r#"
+BEGIN;
+-- This will cause a table rewrite for Postgres versions before 11, but that is handled by
+-- adding-field-with-default.
+ALTER TABLE "core_recipe" ADD COLUMN "foo" integer DEFAULT 10 NOT NULL;
+ALTER TABLE "core_recipe" ALTER COLUMN "foo" DROP DEFAULT;
+COMMIT;
+        "#;
+        let file = squawk_syntax::SourceFile::parse(sql);
+        let mut linter = Linter::from([Rule::AddingNotNullableField]);
+        let errors = linter.lint(file, sql);
+        assert!(errors.is_empty());
+    }
+
+    #[test]
+    fn adding_field_that_is_not_nullable_without_default() {
+        let sql = r#"
+-- This won't work if the table is populated, but that error is caught by adding-required-field.
+ALTER TABLE "core_recipe" ADD COLUMN "foo" integer NOT NULL;
+        "#;
+        let file = squawk_syntax::SourceFile::parse(sql);
+        let mut linter = Linter::from([Rule::AddingNotNullableField]);
+        let errors = linter.lint(file, sql);
+        assert!(errors.is_empty());
+    }
+
+    #[test]
+    fn adding_field_that_is_not_nullable_in_version_11() {
+        let sql = r#"
+BEGIN;
+--
+-- Add field foo to recipe
+--
+ALTER TABLE "core_recipe" ADD COLUMN "foo" integer NOT NULL DEFAULT 10;
+COMMIT;
+        "#;
+        let file = squawk_syntax::SourceFile::parse(sql);
+        let mut linter = Linter::from([Rule::AddingNotNullableField]);
+        let errors = linter.lint(file, sql);
+        assert!(errors.is_empty());
+    }
+}
