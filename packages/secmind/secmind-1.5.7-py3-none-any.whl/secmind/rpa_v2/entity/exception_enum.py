26d5e16a0e825f18e820832d508f6fca515e7cbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:22 +0000

    Add package secmind/1.5.7/secmind-1.5.7-py3-none-any.whl

---
+++ packages/secmind/secmind-1.5.7-py3-none-any.whl/secmind/rpa_v2/entity/exception_enum.py
@@ -0,0 +1,22 @@
+exception_map = {
+    "FileNotFoundError": {
+        "ch": "文件未找到",
+        "en": "The file is missing"
+    },
+    "timed out": {
+        "ch": "连接超时",
+        "en": "connection timeout"
+    },
+    "Access denied'": {
+        "ch": "账号，密码错误或服务器地址不允许访问",
+        "en": "Account number, password error or server address not allowed to access"
+    },
+    "Connection refused": {
+        "ch": "连接被拒绝",
+        "en": "Connection refused"
+    },
+    "mysqladmin flush-hosts": {
+        "ch": "由于连接失败次数过多，服务器地址被锁定，请手动连接到数据库使用mysqladmin flush-hosts",
+        "en": "Due to too many connection failures, the server address is locked. Please manually connect to the database and use mysqladmin flush-hosts"
+    }
+}
