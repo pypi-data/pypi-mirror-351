397cf9a282289dd422ae4b2419e76efc7fe175c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:32 +0000

    Add package dataplicity/0.5.13a1/dataplicity-0.5.13a1.tar.gz

---
+++ packages/dataplicity/dataplicity-0.5.13a1.tar.gz/dataplicity-0.5.13a1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import sys
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Programming Language :: Python",
+]
+
+# http://bit.ly/2alyerp
+with open("dataplicity/_version.py") as f:
+    exec(f.read())
+
+with open("README.md") as f:
+    long_desc = f.read()
+
+# Only require enum34 for Python versions without built-in enum support
+install_requires = ["six==1.10.0", "lomond==0.3.3", "distro==1.6.0"]
+if sys.version_info < (3, 4):
+    install_requires.append("enum34==1.1.6")
+
+setup(
+    name="dataplicity",
+    version=__version__,
+    description="Platform for connected devices",
+    long_description=long_desc,
+    author="WildFoundry",
+    author_email="support@dataplicity.com",
+    url="https://www.dataplicity.com",
+    platforms=["any"],
+    packages=find_packages(),
+    classifiers=classifiers,
+    entry_points={"console_scripts": ["dataplicity = dataplicity.app:main"]},
+    install_requires=install_requires,
+    zip_safe=True,
+)
