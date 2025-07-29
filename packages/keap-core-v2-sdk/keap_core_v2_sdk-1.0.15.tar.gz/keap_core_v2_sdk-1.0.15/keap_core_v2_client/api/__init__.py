bb7cf17608a19796337f3fda2ccb0d1a9da8da93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:01 +0000

    Add package keap-core-v2-sdk/1.0.15/keap_core_v2_sdk-1.0.15.tar.gz

---
+++ packages/keap-core-v2-sdk/keap_core_v2_sdk-1.0.15.tar.gz/keap_core_v2_sdk-1.0.15/keap_core_v2_client/api/__init__.py
@@ -0,0 +1,35 @@
+# flake8: noqa
+
+# import apis into api package
+from keap_core_v2_client.api.affiliate_api import AffiliateApi
+from keap_core_v2_client.api.automation_api import AutomationApi
+from keap_core_v2_client.api.automation_category_api import AutomationCategoryApi
+from keap_core_v2_client.api.business_profile_api import BusinessProfileApi
+from keap_core_v2_client.api.campaign_api import CampaignApi
+from keap_core_v2_client.api.company_api import CompanyApi
+from keap_core_v2_client.api.contact_api import ContactApi
+from keap_core_v2_client.api.custom_fields_api import CustomFieldsApi
+from keap_core_v2_client.api.date_expression_api import DateExpressionApi
+from keap_core_v2_client.api.deals_api import DealsApi
+from keap_core_v2_client.api.display_forms_api import DisplayFormsApi
+from keap_core_v2_client.api.email_api import EmailApi
+from keap_core_v2_client.api.files_api import FilesApi
+from keap_core_v2_client.api.lead_source_categories_api import LeadSourceCategoriesApi
+from keap_core_v2_client.api.lead_source_expenses_api import LeadSourceExpensesApi
+from keap_core_v2_client.api.lead_source_recurring_expenses_api import LeadSourceRecurringExpensesApi
+from keap_core_v2_client.api.lead_sources_api import LeadSourcesApi
+from keap_core_v2_client.api.note_api import NoteApi
+from keap_core_v2_client.api.opportunity_api import OpportunityApi
+from keap_core_v2_client.api.orders_api import OrdersApi
+from keap_core_v2_client.api.payment_method_configs_api import PaymentMethodConfigsApi
+from keap_core_v2_client.api.pipelines_api import PipelinesApi
+from keap_core_v2_client.api.product_interest_bundles_api import ProductInterestBundlesApi
+from keap_core_v2_client.api.reporting_api import ReportingApi
+from keap_core_v2_client.api.sales_api import SalesApi
+from keap_core_v2_client.api.settings_api import SettingsApi
+from keap_core_v2_client.api.stages_api import StagesApi
+from keap_core_v2_client.api.subscriptions_api import SubscriptionsApi
+from keap_core_v2_client.api.tags_api import TagsApi
+from keap_core_v2_client.api.task_api import TaskApi
+from keap_core_v2_client.api.users_api import UsersApi
+
