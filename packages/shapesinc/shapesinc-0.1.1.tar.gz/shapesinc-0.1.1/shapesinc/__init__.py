73aebcb8984e037aaf0ad2afdf8b7ddc81bfa844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:07:22 +0000

    Add package shapesinc/0.1.1/shapesinc-0.1.1.tar.gz

---
+++ packages/shapesinc/shapesinc-0.1.1.tar.gz/shapesinc-0.1.1/shapesinc/__init__.py
@@ -0,0 +1,19 @@
+from .abc import (
+  ShapeUser,
+  ShapeChannel,
+   Message,
+  MessageContent,
+  PromptResponse,
+  ContentType,
+  APIError,
+  RateLimitError
+)
+from .shape import (
+  ShapeBase,
+  Shape,
+  AsyncShape,
+  shape
+)
+
+
+__version__ = "0.1.1"
