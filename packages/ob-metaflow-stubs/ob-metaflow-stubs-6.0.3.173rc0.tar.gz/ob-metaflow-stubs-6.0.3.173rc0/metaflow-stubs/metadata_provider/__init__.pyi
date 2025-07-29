f3d0bc444795e36fa54f3caf507df9c31ee9d63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:25 +0000

    Add package ob-metaflow-stubs/6.0.3.173rc0/ob-metaflow-stubs-6.0.3.173rc0.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.173rc0.tar.gz/ob-metaflow-stubs-6.0.3.173rc0/metaflow-stubs/metadata_provider/__init__.pyi
@@ -0,0 +1,16 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.14.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-05-28T01:05:50.847258                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import metadata as metadata
+from .metadata import DataArtifact as DataArtifact
+from .metadata import MetadataProvider as MetadataProvider
+from .metadata import MetaDatum as MetaDatum
+from . import util as util
+from . import heartbeat as heartbeat
+
