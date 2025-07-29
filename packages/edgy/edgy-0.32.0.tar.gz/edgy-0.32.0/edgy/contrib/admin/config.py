4c956d7187ce4056996a2c03d868fe5ed5cf7c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:57 +0000

    Add package edgy/0.32.0/edgy-0.32.0.tar.gz

---
+++ packages/edgy/edgy-0.32.0.tar.gz/edgy-0.32.0/edgy/contrib/admin/config.py
@@ -0,0 +1,15 @@
+import os
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class AdminConfig(BaseSettings):
+    model_config = SettingsConfigDict(extra="allow", arbitrary_types_allowed=True)
+    admin_prefix_url: str = "/admin"
+    title: str = "Edgy Admin"
+    menu_title: str = "Edgy Admin"
+    favicon: str = "https://raw.githubusercontent.com/dymmond/edgy/refs/heads/main/docs/statics/images/favicon.ico"
+    sidebar_bg_colour: str = "#1C4C74"
+    dashboard_title: str = "Edgy Admin Dashboard"
+    SECRET_KEY: str | bytes = Field(default_factory=lambda: os.urandom(64))
---
+++ packages/edgy/edgy-0.32.0.tar.gz/edgy-0.32.0/edgy/core/marshalls/metaclasses.py
@@ -0,0 +1,125 @@
+from typing import TYPE_CHECKING, Any
+
+from monkay import load
+from pydantic._internal._model_construction import ModelMetaclass
+
+from edgy.core.db.models.metaclasses import handle_annotations
+from edgy.core.marshalls.config import ConfigMarshall
+from edgy.core.marshalls.fields import BaseMarshallField
+from edgy.core.utils.functional import extract_field_annotations_and_defaults
+from edgy.exceptions import MarshallFieldDefinitionError
+
+if TYPE_CHECKING:
+    from edgy.core.db.models import Model
+    from edgy.core.marshalls import Marshall
+
+
+class MarshallMeta(ModelMetaclass):
+    """
+    Base metaclass for the Marshalls
+    """
+
+    __slots__ = ()
+
+    def __new__(cls, name: str, bases: tuple[type, ...], attrs: dict[str, Any]) -> Any:
+        base_annotations: dict[str, Any] = {}
+        show_pk: bool = False
+        marshall_config: ConfigMarshall = attrs.pop("marshall_config", None)
+        # TODO: should have correct types
+        attrs, model_fields = extract_field_annotations_and_defaults(attrs, BaseMarshallField)
+
+        has_parents = any(isinstance(parent, MarshallMeta) for parent in bases)
+
+        if not has_parents:
+            return super().__new__(cls, name, bases, attrs)
+
+        model_class: type[Marshall] = super().__new__(cls, name, bases, attrs)
+        if name in ("Marshall",):
+            return model_class
+
+        if marshall_config is None:
+            raise MarshallFieldDefinitionError(
+                "The 'marshall_config' was not found. Make sure it is declared and set."
+            )
+
+        # The declared model
+        _model: type[Model] | str | None = marshall_config.get("model", None)
+        assert _model is not None, "'model' must be declared in the 'ConfigMarshall'."
+
+        if isinstance(_model, str):
+            model: type[Model] = load(_model)
+            marshall_config["model"] = model
+        else:
+            model = _model
+
+        base_fields_include = marshall_config.get("fields", None)
+        base_fields_exclude = marshall_config.get("exclude", None)
+
+        assert base_fields_include is None or base_fields_exclude is None, (
+            "Use either 'fields' or 'exclude', not both."
+        )
+        assert base_fields_include is not None or base_fields_exclude is not None, (
+            "Either 'fields' or 'exclude' must be declared."
+        )
+
+        base_model_fields: dict[str, Any] = {}
+
+        # Define the fields for the Marshall
+        if base_fields_exclude is not None:
+            base_model_fields = {
+                k: v for k, v in model.model_fields.items() if k not in base_fields_exclude
+            }
+        elif base_fields_include is not None and "__all__" in base_fields_include:
+            base_model_fields = {
+                k: v for k, v in model.meta.fields.items() if k not in model_fields
+            }
+            show_pk = True
+        else:
+            base_model_fields = {
+                k: v for k, v in model.model_fields.items() if k in base_fields_include
+            }
+        base_model_fields.update(model_fields)
+
+        # Handles with the fields not declared in the model.
+        custom_fields: dict[str, BaseMarshallField] = {}
+
+        # For custom model_fields
+        for k, v in attrs.items():
+            if isinstance(v, BaseMarshallField):  # noqa: SIM102
+                # Make sure the custom fields are flagged.
+                if k not in model.meta.fields:
+                    custom_fields[k] = v
+
+        # Handle the check of the custom fields
+        for name, field in custom_fields.items():
+            if (
+                field.__is_method__
+                and not field.source
+                and not hasattr(model_class, f"get_{name}")
+            ):
+                raise MarshallFieldDefinitionError(
+                    f"Field '{name}' declared but no 'get_{name}' found in '{model_class.__name__}'."
+                )
+
+        model_fields_on_class = getattr(model_class, "__pydantic_fields__", None)
+        if model_fields_on_class is None:
+            model_fields_on_class = model_class.model_fields
+        for key in model_fields:
+            del model_fields_on_class[key]
+        model_fields_on_class.update(base_model_fields)
+
+        # Handle annotations
+        annotations: dict[str, Any] = handle_annotations(bases, base_annotations, attrs)
+        model_class.__init_annotations__ = annotations
+        model_class.__show_pk__ = show_pk
+        model_class.__custom_fields__ = custom_fields
+        model_class.marshall_config = marshall_config
+        model_class.model_rebuild(force=True)
+
+        # Raise for error if any of the required fields is not in the Marshall
+        required_fields: set[str] = {k for k, v in model.model_fields.items() if v.is_required()}
+        model_fields = model_class.model_fields
+        model_class.__incomplete_fields__ = tuple(
+            sorted(name for name in required_fields if name not in model_fields_on_class)
+        )
+        return model_class
