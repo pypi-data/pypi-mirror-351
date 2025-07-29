76af968b5f8ed1d53d4d6a830601f36a5dd692eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:08:11 +0000

    Add package groclake/0.5.0/groclake-0.5.0-py3-none-any.whl

---
+++ packages/groclake/groclake-0.5.0-py3-none-any.whl/groclake/toollake/__init__.py
@@ -0,0 +1,37 @@
+'''
+Toollake - Tools module for various integrations
+'''
+
+from .calendar import GoogleCalendar
+from .comm.gupshup import Gupshup
+from .crm.salesforce import Salesforce
+from .devops.jira import Jira
+from .devops.aws import AWS
+from .comm.slack import Slack
+from .apm.newrelic import NewRelic
+from .apm.datadog import Datadog
+from .code.github import GitHub
+from .erp.sap import SAP
+from .comm.gmail import Gmail
+from .comm.twilio import Twilio
+from .comm.mailchimp import Mailchimp
+from .ecomm.shopify import Shopify
+from .apm.solarwinds import SolarWinds
+from .apm.instana import Instana
+from .apm.appdynamics import AppDynamics
+from .apm.dynatrace import Dynatrace
+from .cloudstorage.sharepoint import SharePoint
+from .comm.outlook import Outlook
+from .collab.teams import Teams
+from .crm.dynamics365 import Microsoft365Dynamics
+from .db.mongodb import MongoDB
+
+from .db.esvector import ESVector
+from .db.elastic import Elastic
+from .db.mysqldb import MysqlDB
+from .cloudstorage.awss3 import AWSS3
+from .db.mongovector import MongoVector
+from .analytics.powerbi import PowerBI
+from .cloudstorage.confluence import Confluence
+
+__all__ = ['GoogleCalendar', 'Gupshup', 'Salesforce', 'Jira', 'NewRelic','Slack', 'GitHub','Gmail','Mailchimp','Twilio','SAP','Shopify','Datadog','AWS','SolarWinds','Instana','AppDynamics','Dynatrace','SharePoint','Outlook','Teams','Microsoft365Dynamics','MongoDB','ESVector','Elastic','MysqlDB','MongoVector','PowerBI','Confluence','AWSS3']
---
+++ packages/groclake/groclake-0.5.0-py3-none-any.whl/groclake/toollake/cloudstorage/__init__.py
@@ -0,0 +1,4 @@
+from .awss3 import AWSS3
+from .sharepoint import SharePoint
+from .confluence import Confluence
+__all__ = ['AWSS3','SharePoint','Confluence']
