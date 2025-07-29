ca85e4eeb437cd669e7c38c9f32f33b5e5085346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:09 +0000

    Add package zzzz-mylib-23-4/0.106/zzzz_mylib_23_4-0.106.tar.gz

---
+++ packages/zzzz-mylib-23-4/zzzz_mylib_23_4-0.106.tar.gz/zzzz_mylib_23_4-0.106/Mylib/fit_incremental_sl_model.py
@@ -0,0 +1,50 @@
+from xgboost import XGBClassifier, XGBRegressor
+from lightgbm import LGBMClassifier, LGBMRegressor
+from Mylib.myclasses import CustomStackingClassifier, CustomStackingRegressor
+from sklearn.ensemble import (
+    RandomForestClassifier,
+    ExtraTreesClassifier,
+    RandomForestRegressor,
+    ExtraTreesRegressor,
+)
+
+
+def fit_model_incremental_learning(model, feature, target):
+    """Fit model với cài đặt incremental learning, một số model đặc biệt như XGBClassifier, LGBMClassifier, CustomStackingClassifier, ...
+
+    Args:
+        model (_type_): _description_
+        feature (_type_): _description_
+        target (_type_): _description_
+    """
+    if isinstance(model, (XGBClassifier, XGBRegressor)):
+        model.fit(feature, target, xgb_model=model.get_booster())
+        return
+
+    if isinstance(model, (LGBMClassifier, LGBMRegressor)):
+        model.fit(feature, target, init_model=model.booster_)
+
+        return
+
+    if isinstance(model, (CustomStackingClassifier, CustomStackingRegressor)):
+        model.fit_model_incremental_learning(feature, target)
+
+        return
+
+    # Mấy này có warm_start = True nhưng phải tăng số cây lên thì mới học tiếp được
+    if isinstance(
+        model,
+        (
+            RandomForestClassifier,
+            ExtraTreesClassifier,
+            RandomForestRegressor,
+            ExtraTreesRegressor,
+        ),
+    ):
+        model.n_estimators += 50
+        model.fit(feature, target)
+
+        return
+
+    # Các model khác có tham số warm_start = True là đủ rồi
+    model.fit(feature, target)
