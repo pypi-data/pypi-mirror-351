695e7bcf8b469ae0bcc648605b0ddf8e0d252277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:54 +0000

    Add package solax-py-library/1.0.0.34/solax_py_library-1.0.0.34.tar.gz

---
+++ packages/solax-py-library/solax_py_library-1.0.0.34.tar.gz/solax_py_library-1.0.0.34/solax_py_library/smart_scene/core/condition/system_condition.py
@@ -0,0 +1,35 @@
+from solax_py_library.smart_scene.core.condition.base import BaseCondition
+from solax_py_library.smart_scene.types.condition import (
+    SystemConditionItemData,
+    SystemConditionType,
+    ConditionType,
+)
+
+
+class SystemCondition(BaseCondition):
+    condition_type = ConditionType.systemCondition
+
+    def __init__(self, update_value_function, **kwargs):
+        super().__init__(update_value_function, **kwargs)
+
+    def meet_func(self, data: SystemConditionItemData, ctx):
+        if not self.value:
+            return False
+        child_data = data.childData
+        function_value = child_data.function
+        compare_value = None
+        if data.childType == SystemConditionType.systemExportPower:
+            compare_value = self.value.get("grid_active_power")
+            if compare_value < 0:
+                return False
+        elif data.childType == SystemConditionType.systemImportPower:
+            compare_value = self.value.get("grid_active_power")
+            if compare_value > 0:
+                return False
+        elif data.childType == SystemConditionType.systemSoc:
+            compare_value = self.value.get("system_soc")
+        if compare_value is None:
+            return False
+        return function_value.function()(
+            abs(compare_value), child_data.data[0]
+        )
