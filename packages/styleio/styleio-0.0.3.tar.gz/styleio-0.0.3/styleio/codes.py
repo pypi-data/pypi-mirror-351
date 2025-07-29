be0d9d5f99f9511e944a801b7e1a887013f22ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:02 +0000

    Add package styleio/0.0.3/styleio-0.0.3.tar.gz

---
+++ packages/styleio/styleio-0.0.3.tar.gz/styleio-0.0.3/styleio/codes.py
@@ -0,0 +1,35 @@
+codes = {
+    "graphics": {
+        "bold": "1",
+        "dim": "2",
+        "italic": "3",
+        "underline": "4",
+        "blink": "5",
+        "inverse": "7",
+        "hidden": "8",
+        "strikethrough": "9"
+    },
+
+    "colors16": {
+        "black": "30",
+        "red": "31",
+        "green": "32",
+        "yellow": "33",
+        "blue": "34",
+        "magenta": "35",
+        "cyan": "36",
+        "white": "37",
+        "default": "39"
+    },
+
+    "brightcolors": {
+        "black": "90",
+        "red": "91",
+        "green": "92",
+        "yellow": "93",
+        "blue": "94",
+        "magneta": "95",
+        "cyan": "96",
+        "white": "97"
+    }
+}
