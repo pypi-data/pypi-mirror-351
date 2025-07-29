6983c9772dd5ae4d0c655d3e9800dbdab5405fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:47 +0000

    Add package ep-sdk-4pd/1.1.3/ep_sdk_4pd-1.1.3.tar.gz

---
+++ packages/ep-sdk-4pd/ep_sdk_4pd-1.1.3.tar.gz/ep_sdk_4pd-1.1.3/tests/test_predict_data.py
@@ -0,0 +1,15 @@
+from ep_sdk_4pd.ep_data import EpData
+
+
+def test_predict_data():
+    print('-------------test_predict_data-------------')
+
+    data = EpData.get_predict_data()
+    weather = data['weather']
+    plant = data['plant']
+    print(data)
+    print('-------------------------------------')
+
+
+if __name__ == '__main__':
+    test_predict_data()
