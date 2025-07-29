6a3bbc8c2d3a919143868ecc7ee193f859b4d15d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:06 +0000

    Add package pumpia/0.5.10/pumpia-0.5.10.tar.gz

---
+++ packages/pumpia/pumpia-0.5.10.tar.gz/pumpia-0.5.10/pumpia/file_handling/dicom_tags/__init__.py
@@ -0,0 +1,7 @@
+from pumpia.file_handling.dicom_tags.bases import Tag, TagLink, get_tag
+from pumpia.file_handling.dicom_tags.all_tags import DicomTags
+from pumpia.file_handling.dicom_tags.ct_tags import CTTags
+from pumpia.file_handling.dicom_tags.mri_tags import MRTags
+from pumpia.file_handling.dicom_tags.nuc_med_tags import NucMedTags
+from pumpia.file_handling.dicom_tags.us_tags import USTags
+from pumpia.file_handling.dicom_tags.xray_tags import XRAYTags
