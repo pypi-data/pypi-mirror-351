4d81a2edf487977b36ab8e3f7e560f5d73aae1e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:58:13 +0000

    Add package streamlit-fill-in-blanks/0.1.1/streamlit_fill_in_blanks-0.1.1.tar.gz

---
+++ packages/streamlit-fill-in-blanks/streamlit_fill_in_blanks-0.1.1.tar.gz/streamlit_fill_in_blanks-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import setuptools
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="streamlit-fill-in-blanks",
+    version="0.1.1",
+    author="Felix Hammer",
+    author_email="fhammer@uos.de",
+    description="Streamlit component for fill-in-the-blanks exercises",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ffhammer/streamlit-fill-in-blanks",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.8",
+    install_requires=[
+        "streamlit >= 1.0",
+    ],
+)
