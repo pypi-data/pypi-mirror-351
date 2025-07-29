a168d5fd6889b5247e0cf12435a76ae2a6e2ff51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:51 +0000

    Add package kaldi-native-fbank/1.21.2/kaldi_native_fbank-1.21.2-cp38-cp38-win32.whl

---
+++ packages/kaldi-native-fbank/kaldi_native_fbank-1.21.2-cp38-cp38-win32.whl/kaldi_native_fbank/__init__.py
@@ -0,0 +1,18 @@
+from _kaldi_native_fbank import (
+    FbankOptions,
+    FeatureWindowFunction,
+    FrameExtractionOptions,
+    IStft,
+    MelBanksOptions,
+    MfccOptions,
+    OnlineFbank,
+    OnlineMfcc,
+    OnlineWhisperFbank,
+    Rfft,
+    Stft,
+    StftConfig,
+    StftResult,
+    WhisperFeatureOptions,
+)
+__version__ = '1.21.2'
+__version__ = '1.21.2'
