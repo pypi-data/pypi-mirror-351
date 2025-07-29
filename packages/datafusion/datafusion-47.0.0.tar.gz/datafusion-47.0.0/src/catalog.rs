262b76dc0f35438a79cff788b8f4296b27286b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:15 +0000

    Add package datafusion/47.0.0/datafusion-47.0.0.tar.gz

---
+++ packages/datafusion/datafusion-47.0.0.tar.gz/datafusion-47.0.0/src/catalog.rs
@@ -0,0 +1,147 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use std::collections::HashSet;
+use std::sync::Arc;
+
+use pyo3::exceptions::PyKeyError;
+use pyo3::prelude::*;
+
+use crate::errors::{PyDataFusionError, PyDataFusionResult};
+use crate::utils::wait_for_future;
+use datafusion::{
+    arrow::pyarrow::ToPyArrow,
+    catalog::{CatalogProvider, SchemaProvider},
+    datasource::{TableProvider, TableType},
+};
+
+#[pyclass(name = "Catalog", module = "datafusion", subclass)]
+pub struct PyCatalog {
+    pub catalog: Arc<dyn CatalogProvider>,
+}
+
+#[pyclass(name = "Database", module = "datafusion", subclass)]
+pub struct PyDatabase {
+    pub database: Arc<dyn SchemaProvider>,
+}
+
+#[pyclass(name = "Table", module = "datafusion", subclass)]
+pub struct PyTable {
+    pub table: Arc<dyn TableProvider>,
+}
+
+impl PyCatalog {
+    pub fn new(catalog: Arc<dyn CatalogProvider>) -> Self {
+        Self { catalog }
+    }
+}
+
+impl PyDatabase {
+    pub fn new(database: Arc<dyn SchemaProvider>) -> Self {
+        Self { database }
+    }
+}
+
+impl PyTable {
+    pub fn new(table: Arc<dyn TableProvider>) -> Self {
+        Self { table }
+    }
+
+    pub fn table(&self) -> Arc<dyn TableProvider> {
+        self.table.clone()
+    }
+}
+
+#[pymethods]
+impl PyCatalog {
+    fn names(&self) -> Vec<String> {
+        self.catalog.schema_names()
+    }
+
+    #[pyo3(signature = (name="public"))]
+    fn database(&self, name: &str) -> PyResult<PyDatabase> {
+        match self.catalog.schema(name) {
+            Some(database) => Ok(PyDatabase::new(database)),
+            None => Err(PyKeyError::new_err(format!(
+                "Database with name {name} doesn't exist."
+            ))),
+        }
+    }
+
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!(
+            "Catalog(schema_names=[{}])",
+            self.names().join(";")
+        ))
+    }
+}
+
+#[pymethods]
+impl PyDatabase {
+    fn names(&self) -> HashSet<String> {
+        self.database.table_names().into_iter().collect()
+    }
+
+    fn table(&self, name: &str, py: Python) -> PyDataFusionResult<PyTable> {
+        if let Some(table) = wait_for_future(py, self.database.table(name))? {
+            Ok(PyTable::new(table))
+        } else {
+            Err(PyDataFusionError::Common(format!(
+                "Table not found: {name}"
+            )))
+        }
+    }
+
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!(
+            "Database(table_names=[{}])",
+            Vec::from_iter(self.names()).join(";")
+        ))
+    }
+
+    // register_table
+    // deregister_table
+}
+
+#[pymethods]
+impl PyTable {
+    /// Get a reference to the schema for this table
+    #[getter]
+    fn schema(&self, py: Python) -> PyResult<PyObject> {
+        self.table.schema().to_pyarrow(py)
+    }
+
+    /// Get the type of this table for metadata/catalog purposes.
+    #[getter]
+    fn kind(&self) -> &str {
+        match self.table.table_type() {
+            TableType::Base => "physical",
+            TableType::View => "view",
+            TableType::Temporary => "temporary",
+        }
+    }
+
+    fn __repr__(&self) -> PyResult<String> {
+        let kind = self.kind();
+        Ok(format!("Table(kind={kind})"))
+    }
+
+    // fn scan
+    // fn statistics
+    // fn has_exact_statistics
+    // fn supports_filter_pushdown
+}
---
+++ packages/datafusion/datafusion-47.0.0.tar.gz/datafusion-47.0.0/src/utils.rs
@@ -0,0 +1,107 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use crate::common::data_type::PyScalarValue;
+use crate::errors::{PyDataFusionError, PyDataFusionResult};
+use crate::TokioRuntime;
+use datafusion::common::ScalarValue;
+use datafusion::execution::context::SessionContext;
+use datafusion::logical_expr::Volatility;
+use pyo3::exceptions::PyValueError;
+use pyo3::prelude::*;
+use pyo3::types::PyCapsule;
+use std::future::Future;
+use std::sync::OnceLock;
+use tokio::runtime::Runtime;
+
+/// Utility to get the Tokio Runtime from Python
+#[inline]
+pub(crate) fn get_tokio_runtime() -> &'static TokioRuntime {
+    // NOTE: Other pyo3 python libraries have had issues with using tokio
+    // behind a forking app-server like `gunicorn`
+    // If we run into that problem, in the future we can look to `delta-rs`
+    // which adds a check in that disallows calls from a forked process
+    // https://github.com/delta-io/delta-rs/blob/87010461cfe01563d91a4b9cd6fa468e2ad5f283/python/src/utils.rs#L10-L31
+    static RUNTIME: OnceLock<TokioRuntime> = OnceLock::new();
+    RUNTIME.get_or_init(|| TokioRuntime(tokio::runtime::Runtime::new().unwrap()))
+}
+
+/// Utility to get the Global Datafussion CTX
+#[inline]
+pub(crate) fn get_global_ctx() -> &'static SessionContext {
+    static CTX: OnceLock<SessionContext> = OnceLock::new();
+    CTX.get_or_init(SessionContext::new)
+}
+
+/// Utility to collect rust futures with GIL released
+pub fn wait_for_future<F>(py: Python, f: F) -> F::Output
+where
+    F: Future + Send,
+    F::Output: Send,
+{
+    let runtime: &Runtime = &get_tokio_runtime().0;
+    py.allow_threads(|| runtime.block_on(f))
+}
+
+pub(crate) fn parse_volatility(value: &str) -> PyDataFusionResult<Volatility> {
+    Ok(match value {
+        "immutable" => Volatility::Immutable,
+        "stable" => Volatility::Stable,
+        "volatile" => Volatility::Volatile,
+        value => {
+            return Err(PyDataFusionError::Common(format!(
+                "Unsupportad volatility type: `{value}`, supported \
+                 values are: immutable, stable and volatile."
+            )))
+        }
+    })
+}
+
+pub(crate) fn validate_pycapsule(capsule: &Bound<PyCapsule>, name: &str) -> PyResult<()> {
+    let capsule_name = capsule.name()?;
+    if capsule_name.is_none() {
+        return Err(PyValueError::new_err(
+            "Expected schema PyCapsule to have name set.",
+        ));
+    }
+
+    let capsule_name = capsule_name.unwrap().to_str()?;
+    if capsule_name != name {
+        return Err(PyValueError::new_err(format!(
+            "Expected name '{}' in PyCapsule, instead got '{}'",
+            name, capsule_name
+        )));
+    }
+
+    Ok(())
+}
+
+pub(crate) fn py_obj_to_scalar_value(py: Python, obj: PyObject) -> PyResult<ScalarValue> {
+    // convert Python object to PyScalarValue to ScalarValue
+
+    let pa = py.import("pyarrow")?;
+
+    // Convert Python object to PyArrow scalar
+    let scalar = pa.call_method1("scalar", (obj,))?;
+
+    // Convert PyArrow scalar to PyScalarValue
+    let py_scalar = PyScalarValue::extract_bound(scalar.as_ref())
+        .map_err(|e| PyValueError::new_err(format!("Failed to extract PyScalarValue: {}", e)))?;
+
+    // Convert PyScalarValue to ScalarValue
+    Ok(py_scalar.into())
+}
