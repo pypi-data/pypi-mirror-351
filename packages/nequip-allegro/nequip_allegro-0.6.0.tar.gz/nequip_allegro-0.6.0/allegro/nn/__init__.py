2cbce5c3b3f10cc839e631f8b20186b82517ba63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:06:18 +0000

    Add package nequip-allegro/0.6.0/nequip_allegro-0.6.0.tar.gz

---
+++ packages/nequip-allegro/nequip_allegro-0.6.0.tar.gz/nequip_allegro-0.6.0/allegro/nn/__init__.py
@@ -0,0 +1,17 @@
+# This file is a part of the `allegro` package. Please see LICENSE and README at the root for information on using it.
+from ._allegro import Allegro_Module
+from .edgewise import EdgewiseReduce
+from ._edgeembed import ProductTypeEmbedding
+from .scalarembed import TwoBodyBesselScalarEmbed, TwoBodySplineScalarEmbed
+from .tensorembed import TwoBodySphericalHarmonicTensorEmbed
+from ._misc import ScalarMultiply
+
+__all__ = [
+    Allegro_Module,
+    EdgewiseReduce,
+    ProductTypeEmbedding,
+    TwoBodyBesselScalarEmbed,
+    TwoBodySplineScalarEmbed,
+    TwoBodySphericalHarmonicTensorEmbed,
+    ScalarMultiply,
+]
