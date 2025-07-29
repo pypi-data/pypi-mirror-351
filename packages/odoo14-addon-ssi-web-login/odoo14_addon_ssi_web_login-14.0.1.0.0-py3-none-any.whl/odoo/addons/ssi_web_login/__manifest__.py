54f3f784dedd74806adf7bb126a90db715d2e43f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:09 +0000

    Add package odoo14-addon-ssi-web-login/14.0.1.0.0/odoo14_addon_ssi_web_login-14.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-web-login/odoo14_addon_ssi_web_login-14.0.1.0.0-py3-none-any.whl/odoo/addons/ssi_web_login/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2025 OpenSynergy Indonesia
+# Copyright 2025 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "SSI Web Login",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "views/webclient_templates.xml",
+    ],
+    "demo": [],
+}
