276dc171cf17d1a28ad220abca55c47939108900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:17 +0000

    Add package aimhub-client/1.1.2/aimhub_client-1.1.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/aimhub-client/aimhub_client-1.1.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/aim/pytorch.py
@@ -0,0 +1,2 @@
+# Alias to SDK PyTorch utils
+from aim._sdk.integrations.pytorch import track_params_dists, track_gradients_dists # noqa
---
+++ packages/aimhub-client/aimhub_client-1.1.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/aim/tensorflow.py
@@ -0,0 +1,2 @@
+# Alias to SDK TensorFlow Keras interface
+from aim._sdk.integrations.tensorflow import AimCallback, AimTracker  # noqa F401
