44dc00a027ce05f45350d6a1c780ca5d46be5784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:38 +0000

    Add package libpyfoscamcgi/0.0.6/libpyfoscamcgi-0.0.6.tar.gz

---
+++ packages/libpyfoscamcgi/libpyfoscamcgi-0.0.6.tar.gz/libpyfoscamcgi-0.0.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="libpyfoscamcgi",  
+    version="0.0.6",  
+    author="Foscam-wangzhengyu",  
+    author_email="wangzhengyu@foscam.com",
+    url='https://github.com/Foscam-wangzhengyu/libfoscamcgi',
+    description="foscam camera cgi", 
+    include_package_data=True,
+    license='LGPLv3+'    
+)
