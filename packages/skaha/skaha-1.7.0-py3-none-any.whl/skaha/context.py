f438e27e2df0dbb90e641fc6deac1cddd9c2defe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:02 +0000

    Add package skaha/1.7.0/skaha-1.7.0-py3-none-any.whl

---
+++ packages/skaha/skaha-1.7.0-py3-none-any.whl/skaha/context.py
@@ -0,0 +1,47 @@
+"""Get available resources from the skaha server."""
+
+from typing import Any, Dict
+
+from httpx import Response
+
+from skaha.client import SkahaClient
+
+
+class Context(SkahaClient):
+    """Skaha Context.
+
+    Args:
+        SkahaClient (skaha.client.SkahaClient): Configured Skaha Client.
+    """
+
+    def resources(self) -> Dict[str, Any]:
+        """Get available resources from the skaha server.
+
+        Returns:
+            A dictionary of available resources.
+
+        Examples:
+            >>> from skaha.context import Context
+            >>> context = Context()
+            >>> context.resources()
+            {'cores': {
+              'default': 1,
+              'defaultRequest': 1,
+              'defaultLimit': 16,
+              'defaultHeadless': 1,
+              'options': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+              },
+             'memoryGB': {
+              'default': 2,
+              'defaultRequest': 4,
+              'defaultLimit': 192,
+              'defaultHeadless': 4,
+              'options': [1,2,4...192]
+             },
+            'gpus': {
+             'options': [1,2, ... 28]
+             }
+            }
+        """
+        response: Response = self.client.get(url="context")
+        return response.json()
---
+++ packages/skaha/skaha-1.7.0-py3-none-any.whl/skaha/images.py
@@ -0,0 +1,49 @@
+"""Skaha Image Management."""
+
+from typing import Any, Dict, List, Optional
+
+from httpx import Response
+
+from skaha.client import SkahaClient
+from skaha.utils.logs import get_logger
+
+log = get_logger(__name__)
+
+
+class Images(SkahaClient):
+    """Skaha Image Management.
+
+    Args:
+        SkahaClient (skaha.client.SkahaClient): Configured Skaha Client.
+
+    Returns:
+        Images: Skaha Image Management Object.
+    """
+
+    def fetch(self, kind: Optional[str] = None) -> List[str]:
+        """Get images from Skaha Server.
+
+        Args:
+            kind (Optional[str], optional): Type of image. Defaults to None.
+
+        Returns:
+            List[str]: A list of images on the skaha server.
+
+        Examples:
+            >>> from skaha.images import Images
+            >>> images = Images()
+            >>> images.fetch(kind="headless")
+            ['images.canfar.net/chimefrb/sample:latest',
+             ...
+             'images.canfar.net/skaha/terminal:1.1.1']
+        """
+        data: Dict[str, str] = {}
+        # If kind is not None, add it to the data dictionary
+        if kind:
+            data["type"] = kind
+        response: Response = self.client.get("image", params=data)  # type: ignore # noqa
+        payload: Dict[str, Any] = response.json()
+        reply: List[str] = []
+        for image in payload:
+            reply.append(image["id"])  # type: ignore
+        return reply
