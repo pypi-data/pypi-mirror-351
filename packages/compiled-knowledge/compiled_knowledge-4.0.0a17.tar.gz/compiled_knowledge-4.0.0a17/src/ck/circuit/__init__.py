a3a04aad158205b1d809bcca8511838e1215528e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:44 +0000

    Add package compiled-knowledge/4.0.0a17/compiled_knowledge-4.0.0a17.tar.gz

---
+++ packages/compiled-knowledge/compiled_knowledge-4.0.0a17.tar.gz/compiled_knowledge-4.0.0a17/src/ck/circuit/__init__.py
@@ -0,0 +1,13 @@
+# from ._circuit_py import (
+from ._circuit_cy import (
+    Circuit,
+    CircuitNode,
+    VarNode,
+    ConstNode,
+    OpNode,
+    Args,
+    ConstValue,
+    MUL,
+    ADD,
+)
+from .tmp_const import TmpConst
---
+++ packages/compiled-knowledge/compiled_knowledge-4.0.0a17.tar.gz/compiled_knowledge-4.0.0a17/src/ck/pgm_compiler/support/circuit_table/__init__.py
@@ -0,0 +1,9 @@
+# from ._circuit_table_py import (
+from ._circuit_table_cy import (
+    CircuitTable,
+    TableInstance,
+    sum_out,
+    sum_out_all,
+    project,
+    product,
+)
