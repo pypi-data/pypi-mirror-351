3fc5e4c739c70908ffaeb27494d07b67ac31497c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:57 +0000

    Add package zafer/0.1.0/zafer-0.1.0-py3-none-any.whl

---
+++ packages/zafer/zafer-0.1.0-py3-none-any.whl/zafer/hasnain.py
@@ -0,0 +1,14 @@
+class Student:
+
+    def __init__(self):
+        self.name = name
+        self.age = age 
+
+    def printStudent(self):
+
+        print(f"Student : {self.name}, Age : {self.age}")
+
+
+        
+def testFunc():
+    print("This is a test func method")
