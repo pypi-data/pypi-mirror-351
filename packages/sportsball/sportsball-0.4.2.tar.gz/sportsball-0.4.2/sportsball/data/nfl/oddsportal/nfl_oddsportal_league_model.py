0e5cbaf616518967b1706c06c3938f83e168adc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:06:23 +0000

    Add package sportsball/0.4.2/sportsball-0.4.2.tar.gz

---
+++ packages/sportsball/sportsball-0.4.2.tar.gz/sportsball-0.4.2/sportsball/data/nfl/oddsportal/nfl_oddsportal_league_model.py
@@ -0,0 +1,21 @@
+"""NFL OddsPortal league model."""
+
+# pylint: disable=line-too-long
+
+import requests_cache
+
+from ...league import League
+from ...oddsportal.oddsportal_league_model import OddsPortalLeagueModel
+
+
+class NFLOddsPortalLeagueModel(OddsPortalLeagueModel):
+    """NFL OddsPortal implementation of the league model."""
+
+    def __init__(
+        self, session: requests_cache.CachedSession, position: int | None = None
+    ) -> None:
+        super().__init__(League.NFL, session, position=position)
+
+    @classmethod
+    def name(cls) -> str:
+        return "nfl-oddsportal-league-model"
