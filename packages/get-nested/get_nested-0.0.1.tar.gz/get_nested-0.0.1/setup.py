bef410d13ec28dde3a1429876ad2b3962cc9924b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:11 +0000

    Add package get-nested/0.0.1/get_nested-0.0.1.tar.gz

---
+++ packages/get-nested/get_nested-0.0.1.tar.gz/get_nested-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='get_nested',
+    version='0.0.1',
+    packages=find_packages(),
+    author='temp try',
+    description='This is a package for demonstration purposes.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
