75fb34a1403a787f691f6c781a688128992081b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:57:28 +0000

    Add package caosdb/0.18.0/caosdb-0.18.0.tar.gz

---
+++ packages/caosdb/caosdb-0.18.0.tar.gz/caosdb-0.18.0/src/linkahead/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM linkahead SETUP.PY
+#
+short_version = '0.18.0'
+version = '0.18.0'
+full_version = '0.18.0'
+git_revision = '643e749eb1e3e6b83226d55685940f0c578db572'
+release = True
+
+if not release:
+    version = full_version
