e7c875506a6edfc371e0986e7edda7edbc1d6c01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:55 +0000

    Add package gestao-imc/0.1.5/gestao_imc-0.1.5.tar.gz

---
+++ packages/gestao-imc/gestao_imc-0.1.5.tar.gz/gestao_imc-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from os import path
+
+working_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(working_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='gestao_imc',
+    version='0.1.5',
+    url='https://github.com/Goncalo-Soares12/MADS-Grupo-4---Gestao-IMC-',
+    author='Gonçalo Soares, Ruben Abreu, Flávio Santos, Jessica Gonçalves',
+    author_email='seu@email.com',
+    description='Ferramenta para gestão de IMC com base em dados de pessoas.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=['pandas', 'matplotlib', 'datetime', 'IPython'],
+)
