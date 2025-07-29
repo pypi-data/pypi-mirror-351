ba430764091a2a144bf0f1b66a90e158e92c8241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:54 +0000

    Add package openbb-ai/1.2.0/openbb_ai-1.2.0-py3-none-any.whl

---
+++ packages/openbb-ai/openbb_ai-1.2.0-py3-none-any.whl/openbb_ai/__init__.py
@@ -0,0 +1,6 @@
+from .helpers import get_widget_data as get_widget_data
+from .helpers import message_chunk as message_chunk
+from .helpers import reasoning_step as reasoning_step
+from .models import QueryRequest as QueryRequest
+from .models import Widget as Widget
+from .models import WidgetRequest as WidgetRequest
