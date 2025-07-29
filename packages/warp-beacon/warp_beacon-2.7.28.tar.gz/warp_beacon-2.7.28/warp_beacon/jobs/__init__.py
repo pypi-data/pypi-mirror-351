fb2c31ca60ebdc5d2e83f42b5eb98f628bc2b600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:39 +0000

    Add package warp-beacon/2.7.28/warp_beacon-2.7.28.tar.gz

---
+++ packages/warp-beacon/warp_beacon-2.7.28.tar.gz/warp_beacon-2.7.28/warp_beacon/jobs/__init__.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class Origin(Enum):
+	INSTAGRAM = "instagram"
+	YT_SHORTS = "yt_shorts"
+	YT_MUSIC = "yt_music"
+	YOUTUBE = "youtube"
+	YOUTU_BE = "youtu_be"
+	UNKNOWN = "unknown"
