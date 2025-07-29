5aafdcd9c1d40d8c677632eed8867bc2c335f3c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:06 +0000

    Add package pertpy/0.11.5/pertpy-0.11.5-py3-none-any.whl

---
+++ packages/pertpy/pertpy-0.11.5-py3-none-any.whl/pertpy/__init__.py
@@ -0,0 +1,25 @@
+"""Top-level package for pertpy."""
+
+__author__ = "Lukas Heumos"
+__email__ = "lukas.heumos@posteo.net"
+__version__ = "0.11.5"
+
+import warnings
+
+from matplotlib import MatplotlibDeprecationWarning
+from numba import NumbaDeprecationWarning
+
+warnings.filterwarnings("ignore", category=NumbaDeprecationWarning)
+warnings.filterwarnings("ignore", category=MatplotlibDeprecationWarning)
+warnings.filterwarnings("ignore", category=SyntaxWarning)
+warnings.filterwarnings("ignore", category=UserWarning, module="scvi._settings")
+
+import mudata
+
+mudata.set_options(pull_on_update=False)
+
+from . import data as dt
+from . import metadata as md
+from . import plot as pl
+from . import preprocessing as pp
+from . import tools as tl
