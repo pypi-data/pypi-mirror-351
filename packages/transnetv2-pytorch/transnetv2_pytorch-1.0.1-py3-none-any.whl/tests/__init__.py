a9aef31f396644319ff59788b6e340c802648f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:02 +0000

    Add package transnetv2-pytorch/1.0.1/transnetv2_pytorch-1.0.1-py3-none-any.whl

---
+++ packages/transnetv2-pytorch/transnetv2_pytorch-1.0.1-py3-none-any.whl/tests/__init__.py
@@ -0,0 +1,10 @@
+"""
+Test package for transnetv2_pytorch
+
+This package contains test files and test data for the transnetv2_pytorch package.
+"""
+
+import os
+
+# Make the test video path easily accessible
+TEST_VIDEO_PATH = os.path.join(os.path.dirname(__file__), 'test.mp4') 
