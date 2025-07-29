3d2599d430d413a82f1d3279c5242b6ffe7480f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:41 +0000

    Add package gllm-agents-binary/0.2.9/gllm_agents_binary-0.2.9-cp311-cp311-win_amd64.whl

---
+++ packages/gllm-agents-binary/gllm_agents_binary-0.2.9-cp311-cp311-win_amd64.whl/gllm_agents/examples/hello_world_multi_agent_coordinator.pyi
@@ -0,0 +1,5 @@
+from gllm_agents.agent.langgraph_agent import LangGraphAgent as LangGraphAgent
+from gllm_agents.examples.tools.langchain_arithmetic_tools import add_numbers as add_numbers
+from gllm_agents.examples.tools.weather_tool import weather_tool as weather_tool
+
+async def main() -> None: ...
