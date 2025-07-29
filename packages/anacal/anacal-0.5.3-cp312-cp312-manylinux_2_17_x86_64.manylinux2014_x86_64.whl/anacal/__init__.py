df0031088b0bab4fcb44f37790a82c3f04729338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:26 +0000

    Add package anacal/0.5.3/anacal-0.5.3-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/anacal/anacal-0.5.3-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/anacal/__init__.py
@@ -0,0 +1,33 @@
+from . import _anacal, base, fpfs, simulation, utils
+from .__version__ import __version__
+from ._anacal import (
+    detector,
+    geometry,
+    image,
+    mask,
+    math,
+    model,
+    ngmix,
+    noise,
+    psf,
+    table,
+    task,
+)
+
+__all__ = [
+    "_anacal",
+    "image",
+    "fpfs",
+    "model",
+    "base",
+    "noise",
+    "psf",
+    "mask",
+    "simulation",
+    "utils",
+    "math",
+    "ngmix",
+    "detector",
+    "geometry",
+    "task",
+]
