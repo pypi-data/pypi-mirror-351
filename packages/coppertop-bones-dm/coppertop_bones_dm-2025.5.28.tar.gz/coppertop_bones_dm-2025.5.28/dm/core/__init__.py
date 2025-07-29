b3aa46c9079347bb6f3f229d231495cd6141255b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:28 +0000

    Add package coppertop-bones-dm/2025.5.28/coppertop_bones_dm-2025.5.28.tar.gz

---
+++ packages/coppertop-bones-dm/coppertop_bones_dm-2025.5.28.tar.gz/coppertop_bones_dm-2025.5.28/dm/core/__init__.py
@@ -0,0 +1,37 @@
+# **********************************************************************************************************************
+# Copyright 2025 David Briant, https://github.com/coppertop-bones. Licensed under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance with the License. You may obtain a copy of the  License at
+# http://www.apache.org/licenses/LICENSE-2.0. Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  KIND,
+# either express or implied. See the License for the specific language governing permissions and limitations under the
+# License. See the NOTICE file distributed with this work for additional information regarding copyright ownership.
+# **********************************************************************************************************************
+
+# this first set of imports makes PyCharm happy
+# from dm.core.aggman import *
+# from dm.core.comparisons import *
+# from dm.core.contextual_scope import *
+# from dm.core.conv import *
+# from dm.core.datetime import *
+# from dm.core.frame import *
+# from dm.core.maths import *
+# from dm.core.misc import *
+# from dm.core.stats import *
+# from dm.core.text import *
+# from dm.core.types import *
+
+# redo so we have the correct overloads
+from _.dm.core.aggman import *
+from _.dm.core.comparisons import *
+from _.dm.core.contextual_scope import *
+from _.dm.core.conv import *
+from _.dm.core.datetime import *
+from _.dm.core.frame import *
+from _.dm.core.maths import *
+from _.dm.core.misc import *
+from _.dm.core.stats import *
+from _.dm.core.text import *
+from _.dm.core.types import *
+
+import dm
+dm.__version__ = "2023.03.21"
---
+++ packages/coppertop-bones-dm/coppertop_bones_dm-2025.5.28.tar.gz/coppertop_bones_dm-2025.5.28/setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, find_packages
+
+# read the contents of README.md file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+CP_VERSION = '2025.5.28'
+DM_VERSION = "2025.5.28"
+
+
+# print(find_packages())
+
+
+setup(
+  name = 'coppertop-bones-dm',
+  install_requires=[
+    f'coppertop-bones=={CP_VERSION}'
+  ],
+  version=DM_VERSION,
+  packages = [
+    'dm',
+    'dm._core',
+    'dm.core',
+    'dm.finance',
+    'dm.frame',
+    'dm.linalg',
+    'dm.linalg.algos',
+    'dm.utils',
+  ],
+  # package_dir = {'': 'core'},
+  # namespace_packages=['coppertop_'],
+  python_requires = '>=3.11',
+  license = 'OSI Approved :: Apache Software License',
+  description = 'The dangermouse standard library for coppertop and bones',
+  long_description_content_type='text/markdown',
+  long_description=long_description,
+  author = 'David Briant',
+  author_email = 'dangermouseb@forwarding.cc',
+  url = 'https://github.com/coppertop-bones/dm',
+  download_url = '',
+  # download_url = f'https://github.com/coppertop-bones/dm/archive/{version}.tar.gz', not maintained
+  keywords = ['multiple', 'dispatch', 'piping', 'pipeline', 'pipe', 'functional', 'multimethods', 'multidispatch',
+            'functools', 'lambda', 'curry', 'currying', 'dataframe', 'polars', 'pandas'],
+  include_package_data=True,
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Intended Audience :: End Users/Desktop',
+    'Intended Audience :: Science/Research',
+    'Topic :: Utilities',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3.11',
+  ],
+  zip_safe=False,
+)
