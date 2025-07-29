279cd542ebe9a07f2a79984f48ed17c366e8502d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:04 +0000

    Add package aimhub-client/1.1.2/aimhub_client-1.1.2-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/aimhub-client/aimhub_client-1.1.2-cp311-cp311-macosx_11_0_arm64.whl/aim/_sdk/interfaces/container.py
@@ -0,0 +1,75 @@
+from abc import abstractmethod
+from typing import Iterable, Iterator, TypeVar, List, Any, Optional, Tuple
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .sequence import SequenceCollection
+
+
+class Container(object):
+    @abstractmethod
+    def __setitem__(self, key, value):
+        ...
+
+    @abstractmethod
+    def __getitem__(self, key):
+        ...
+
+    @abstractmethod
+    def __delitem__(self, key):
+        ...
+
+    @abstractmethod
+    def get(self, key, default: Any = None, strict: bool = False):
+        ...
+
+    @abstractmethod
+    def set(self, key, value: Any, strict: bool):
+        ...
+
+    @property
+    @abstractmethod
+    def sequences(self) -> 'SequenceCollection':
+        ...
+
+    @abstractmethod
+    def match(self, expr) -> bool:
+        ...
+
+    def track(self, value, name: str, step: Optional[int] = None, context: dict = Optional[None], **axis):
+        raise NotImplementedError
+
+
+ContainerType = TypeVar('ContainerType', bound=Container)
+
+
+class ContainerCollection(Iterable[ContainerType]):
+    @abstractmethod
+    def __iter__(self) -> Iterator[ContainerType]:
+        ...
+
+    @abstractmethod
+    def filter(self, expr: str) -> 'ContainerCollection[ContainerType]':
+        ...
+
+    @abstractmethod
+    def sparse_filter(self, expr: str) -> 'ContainerCollection[ContainerType]':
+        ...
+
+    @abstractmethod
+    def limit(self, n: int) -> 'ContainerCollection[ContainerType]':
+        ...
+
+    @abstractmethod
+    def offset(self, offset: Tuple[int, str]) -> 'ContainerCollection[ContainerType]':
+        ...
+
+    def all(self) -> List[ContainerType]:
+        return list(iter(self))
+
+    def first(self) -> ContainerType:
+        return next(iter(self))
+
+    def count(self) -> int:
+        # default implementation
+        return sum(1 for _ in iter(self))
---
+++ packages/aimhub-client/aimhub_client-1.1.2-cp311-cp311-macosx_11_0_arm64.whl/aim/optuna.py
@@ -0,0 +1,2 @@
+# Alias to SDK Keras-Tuner interface
+from aim._sdk.integrations.optuna import AimCallback  # noqa F401
