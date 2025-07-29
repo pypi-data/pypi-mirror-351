f0622552f72548d9fb363efee4096d8a03686d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:02 +0000

    Add package health-watch/1.0.6/health_watch-1.0.6.tar.gz

---
+++ packages/health-watch/health_watch-1.0.6.tar.gz/health_watch-1.0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='health-watch',
+    version='1.0.6',
+    description='Cross-platform system monitoring utility',
+    author='Yashwant Jain',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'python-dotenv'
+    ],classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
