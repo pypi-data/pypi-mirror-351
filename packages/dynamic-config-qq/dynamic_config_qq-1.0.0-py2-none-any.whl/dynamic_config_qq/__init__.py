84ce33c70b6cf8ab21e460b83ada5825c145ca00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:43 +0000

    Add package dynamic-config-qq/1.0.0/dynamic_config_qq-1.0.0-py2-none-any.whl

---
+++ packages/dynamic-config-qq/dynamic_config_qq-1.0.0-py2-none-any.whl/dynamic_config_qq/__init__.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+#
+from .dynamic_config_client import DynamicConfigClient
+from .dynamic_config_manager import DynamicConfigManager
+from .exceptions import (
+    ConfigNotFoundError,
+    ConfigExistsError
+)
+__version__ = '1.0.0'  # 明确版本号
+__all__ = ['DynamicConfigClient', 'DynamicConfigManager', '__version__', 'ConfigNotFoundError', 'ConfigExistsError']
