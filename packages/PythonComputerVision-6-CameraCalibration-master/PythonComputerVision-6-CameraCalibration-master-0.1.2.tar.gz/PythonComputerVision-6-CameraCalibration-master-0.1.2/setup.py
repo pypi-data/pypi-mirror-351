cfeffced86a9a86d32cbad60270414a7d2110de4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:39 +0000

    Add package PythonComputerVision-6-CameraCalibration-master/0.1.2/PythonComputerVision-6-CameraCalibration-master-0.1.2.tar.gz

---
+++ packages/PythonComputerVision-6-CameraCalibration-master/PythonComputerVision-6-CameraCalibration-master-0.1.2.tar.gz/PythonComputerVision-6-CameraCalibration-master-0.1.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PythonComputerVision-6-CameraCalibration-master',
+    version='0.1.2',
+    packages=find_packages(),  # 使用 find_packages() 自動查找包
+)
