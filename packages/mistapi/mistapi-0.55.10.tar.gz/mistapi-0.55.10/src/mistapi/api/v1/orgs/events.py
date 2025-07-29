31adace40b4c92e67a6fd0d1d69cd734f349564e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:22 +0000

    Add package mistapi/0.55.10/mistapi-0.55.10.tar.gz

---
+++ packages/mistapi/mistapi-0.55.10.tar.gz/mistapi-0.55.10/src/mistapi/api/v1/orgs/events.py
@@ -0,0 +1,52 @@
+'''
+--------------------------------------------------------------------------------
+------------------------- Mist API Python CLI Session --------------------------
+
+    Written by: Thomas Munzer (tmunzer@juniper.net)
+    Github    : https://github.com/tmunzer/mistapi_python
+
+    This package is licensed under the MIT License.
+
+--------------------------------------------------------------------------------
+'''
+
+from mistapi import APISession as _APISession
+from mistapi.__api_response import APIResponse as _APIResponse
+import deprecation
+
+def searchOrgEvents(mist_session:_APISession, org_id:str, type:str=None, start:int=None, end:int=None, duration:str="1d", limit:int=100) -> _APIResponse:
+    """
+    API doc: https://www.juniper.net/documentation/us/en/software/mist/api/http/api/orgs/search-org-events
+    
+    PARAMS
+    -----------
+    mistapi.APISession : mist_session
+        mistapi session including authentication and Mist host information
+    
+    PATH PARAMS
+    -----------
+    org_id : str        
+    
+    QUERY PARAMS
+    ------------
+    type : str
+    start : int
+    end : int
+    duration : str, default: 1d
+    limit : int, default: 100        
+    
+    RETURN
+    -----------
+    mistapi.APIResponse
+        response from the API call
+    """
+    uri = f"/api/v1/orgs/{org_id}/events/search"
+    query_params={}
+    if type: query_params["type"]=type
+    if start: query_params["start"]=start
+    if end: query_params["end"]=end
+    if duration: query_params["duration"]=duration
+    if limit: query_params["limit"]=limit
+    resp = mist_session.mist_get(uri=uri, query=query_params)
+    return resp
+    
