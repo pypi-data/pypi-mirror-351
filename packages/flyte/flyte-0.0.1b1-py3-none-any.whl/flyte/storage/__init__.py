eac482b9cdbab9213932dcd8db3996048afb657e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:02 +0000

    Add package flyte/0.0.1b1/flyte-0.0.1b1-py3-none-any.whl

---
+++ packages/flyte/flyte-0.0.1b1-py3-none-any.whl/flyte/storage/__init__.py
@@ -0,0 +1,24 @@
+__all__ = [
+    "get",
+    "get_random_local_directory",
+    "get_random_local_path",
+    "get_stream",
+    "get_underlying_filesystem",
+    "is_remote",
+    "join",
+    "put",
+    "put_stream",
+    "put_stream",
+]
+
+from ._storage import (
+    get,
+    get_random_local_directory,
+    get_random_local_path,
+    get_stream,
+    get_underlying_filesystem,
+    is_remote,
+    join,
+    put,
+    put_stream,
+)
