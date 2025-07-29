59e1a7a6a613205834abcd705da09415a6e9fadc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:01 +0000

    Add package core-spec-pyscf/0.0/core_spec_pyscf-0.0.tar.gz

---
+++ packages/core-spec-pyscf/core_spec_pyscf-0.0.tar.gz/core_spec_pyscf-0.0/pyscf/zora/__init__.py
@@ -0,0 +1,11 @@
+'''
+Core spectroscopy utilities:
+ - ZORA approximation
+TODO:
+ - Core-valence separation for TDDFT
+ - Davidson diagonalization
+'''
+
+__version__ = '0.0.0'
+
+from . import ZORA
