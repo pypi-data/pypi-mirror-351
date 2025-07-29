6d133b63934bf4df077b5793dbd60f346d3b95e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:33 +0000

    Add package openagents-sdk/0.0.2/openagents_sdk-0.0.2.tar.gz

---
+++ packages/openagents-sdk/openagents_sdk-0.0.2.tar.gz/openagents_sdk-0.0.2/README.md
@@ -0,0 +1,11 @@
+# OpenAgents SDK
+
+一个用于 PyPI 测试发布的包。
+
+示例：
+
+```
+from openagent import hello
+
+hello()
+```
---
+++ packages/openagents-sdk/openagents_sdk-0.0.2.tar.gz/openagents_sdk-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='openagents-sdk',                 
+    version='0.0.2',              
+    description='A test package', 
+    author='Your Name',
+    packages=find_packages(),
+    install_requires=[],          
+    python_requires='>=3.6',
+)
