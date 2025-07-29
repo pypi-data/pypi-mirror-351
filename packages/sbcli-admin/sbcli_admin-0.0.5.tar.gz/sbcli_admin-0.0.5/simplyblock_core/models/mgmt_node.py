9081f04380c43b1146c2c8dfe8058dd2d32c9e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:12 +0000

    Add package sbcli-admin/0.0.5/sbcli_admin-0.0.5.tar.gz

---
+++ packages/sbcli-admin/sbcli_admin-0.0.5.tar.gz/sbcli_admin-0.0.5/simplyblock_core/models/mgmt_node.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+from simplyblock_core.models.base_model import BaseNodeObject
+
+
+class MgmtNode(BaseNodeObject):
+
+    baseboard_sn: str = ""
+    cluster_id: str = ""
+    docker_ip_port: str = ""
+    hostname: str = ""
+    mgmt_ip: str = ""
+    secret: str = ""
+    db_connection: str = ""
+    endpoint: str = ""
