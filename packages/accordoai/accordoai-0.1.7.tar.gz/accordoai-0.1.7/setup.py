5f70796292297f9747bc436e9df72a05c550d625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:08 +0000

    Add package accordoai/0.1.7/accordoai-0.1.7.tar.gz

---
+++ packages/accordoai/accordoai-0.1.7.tar.gz/accordoai-0.1.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="accordoai",
+    version="0.1.7",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "tensorflow>=2.0.0",
+        "numpy",
+        "librosa",
+        "music21",
+        "filetype",
+        "pandas",
+        "scipy",
+        "numpy",
+        "moviepy"
+    ],
+    author="Valenteno Lenora",
+    author_email="valentenocavlenora@gmail.com",
+    description="Chord prediction model from Accordo.ai",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NightKing-V/Chord-Classification-Model-accordo.ai-",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
