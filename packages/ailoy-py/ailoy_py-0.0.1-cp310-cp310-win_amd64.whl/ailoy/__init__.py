8c804161677dba26b0861dec01c18ae25f4458be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:55 +0000

    Add package ailoy-py/0.0.1/ailoy_py-0.0.1-cp310-cp310-win_amd64.whl

---
+++ packages/ailoy-py/ailoy_py-0.0.1-cp310-cp310-win_amd64.whl/ailoy/__init__.py
@@ -0,0 +1,14 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_10_1():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'ailoy_py.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_10_1()
+del _delvewheel_patch_1_10_1
+# end delvewheel patch
+
+from .agent import Agent  # noqa: F401
+from .runtime import AsyncRuntime, Runtime  # noqa: F401
+from .vector_store import VectorStore  # noqa: F401
