284d7a26aa9469e922ff91e44c2bcf7545911d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:51 +0000

    Add package catzilla/0.0.2/catzilla-0.0.2-cp312-cp312-macosx_10_15_x86_64.whl

---
+++ packages/catzilla/catzilla-0.0.2-cp312-cp312-macosx_10_15_x86_64.whl/catzilla/__init__.py
@@ -0,0 +1,22 @@
+"""
+Catzilla Web Framework
+"""
+
+from .app import App
+from .response import ResponseBuilder, response
+from .routing import Router, RouterGroup
+from .types import HTMLResponse, JSONResponse, Request, Response
+
+__version__ = "0.0.2"
+
+__all__ = [
+    "App",
+    "Request",
+    "Response",
+    "JSONResponse",
+    "HTMLResponse",
+    "response",
+    "ResponseBuilder",
+    "Router",
+    "RouterGroup",
+]
