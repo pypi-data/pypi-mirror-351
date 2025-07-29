b23d63d65f64dd4ea8828ac8786cd3eb0487ea22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:27 +0000

    Add package rt-hermes/1.0.0/rt_hermes-1.0.0-py3-none-any.whl

---
+++ packages/rt-hermes/rt_hermes-1.0.0-py3-none-any.whl/hermes/repositories/tests/test_fixtures.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from sqlalchemy import text
+
+from hermes.repositories.project import ProjectRepository
+from hermes.schemas.project_schemas import Project
+
+
+class TestFixtures:
+
+    def test_fixture_forecastseries(self, connection, forecastseries):
+        project_db = connection.execute(text('SELECT * FROM project'))
+        assert len(project_db.all()) == 1
+        forecastseries_db = connection.execute(
+            text('SELECT * FROM forecastseries'))
+        assert len(forecastseries_db.all()) == 1
+
+    def test_fixture_rollback(self, session, connection):
+        project = Project(name='test_project2', starttime=datetime.now())
+        ProjectRepository.create(session, project)
+        project_db = connection.execute(text('SELECT * FROM project'))
+        assert len(project_db.all()) == 1
