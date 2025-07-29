ee680855253e588957ec5aa8be3534568f7936f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:18 +0000

    Add package aignostics/0.2.16/aignostics-0.2.16.tar.gz

---
+++ packages/aignostics/aignostics-0.2.16.tar.gz/aignostics-0.2.16/src/aignostics/qupath/__init__.py
@@ -0,0 +1,17 @@
+"""QuPath module."""
+
+from importlib.util import find_spec
+
+__all__ = []
+
+# advertise PageBuilder to enable auto-discovery
+if find_spec("paquo") and find_spec("nicegui"):
+    from ._cli import cli
+    from ._gui import PageBuilder
+    from ._service import Service
+
+    __all__ += [
+        "PageBuilder",
+        "Service",
+        "cli",
+    ]
