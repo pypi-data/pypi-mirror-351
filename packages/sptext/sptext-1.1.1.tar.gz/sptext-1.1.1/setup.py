4784f7f7371b7ec4697f1f1b255a7d3476e1c6b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:12 +0000

    Add package sptext/1.1.1/sptext-1.1.1.tar.gz

---
+++ packages/sptext/sptext-1.1.1.tar.gz/sptext-1.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sptext',
+    version='1.1.1',
+    packages=find_packages(),
+    description='sptext',
+    long_description=open('README.md', encoding='utf-8').read(),  # <-- thêm dòng này
+    long_description_content_type='text/markdown',                # <-- và dòng này
+    author='Ron AOV',
+    author_email='ronlatoiday@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
