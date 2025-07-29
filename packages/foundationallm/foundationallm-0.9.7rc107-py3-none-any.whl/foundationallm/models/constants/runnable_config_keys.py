0cf4d84b4aa365354ba2791d9c942dd3bc44ba75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:58:12 +0000

    Add package foundationallm/0.9.7rc107/foundationallm-0.9.7rc107-py3-none-any.whl

---
+++ packages/foundationallm/foundationallm-0.9.7rc107-py3-none-any.whl/foundationallm/models/constants/runnable_config_keys.py
@@ -0,0 +1,4 @@
+class RunnableConfigKeys:
+    """The keys for RunnableConfig values."""
+    ORIGINAL_USER_PROMPT = 'original_user_prompt'
+    ORIGINAL_USER_PROMPT_REWRITE = 'original_user_prompt_rewrite'
