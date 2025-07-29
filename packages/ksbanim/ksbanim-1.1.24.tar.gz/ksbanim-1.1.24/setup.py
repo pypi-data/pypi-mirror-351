80e62c47736c83d6f4bc53c85b660068574fddb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:57:44 +0000

    Add package ksbanim/1.1.24/ksbanim-1.1.24.tar.gz

---
+++ packages/ksbanim/ksbanim-1.1.24.tar.gz/ksbanim-1.1.24/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='ksbanim',
+    version='1.1.24',
+    py_modules=['ksbanim'],
+    install_requires=[
+        'PyQt5',
+        'PyOpenGL',
+        'imageio[ffmpeg]',
+        'requests',
+        'triangle',
+        'shapely',
+        'setuptools'
+    ],
+    author='Biasini Dario',
+    author_email='tunnelrind@lernbaum.ch',
+    description='A module for animation based on PyQt5, OpenGL, and imageio[ffmpeg]',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/tunnelrind/ksbanim', 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
