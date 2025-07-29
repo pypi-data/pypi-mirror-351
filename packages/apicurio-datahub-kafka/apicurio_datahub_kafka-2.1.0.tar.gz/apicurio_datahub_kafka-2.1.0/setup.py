b0e83b124aa87f0fdf286aeb568864d7678066ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:08:14 +0000

    Add package apicurio-datahub-kafka/2.1.0/apicurio_datahub_kafka-2.1.0.tar.gz

---
+++ packages/apicurio-datahub-kafka/apicurio_datahub_kafka-2.1.0.tar.gz/apicurio_datahub_kafka-2.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup_output = setup(
+    name="apicurio_datahub_kafka",
+    version="2.1.0",
+    description="DataHub ApiCurio Schema Registry for Kafka Source",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    install_requires=["acryl-datahub[kafka]"],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
