969d8b5c5dee6a72c7e31911d3dc37c5c2fca257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:51 +0000

    Add package move-ugc-python/3.2.0/move_ugc_python-3.2.0-py3-none-any.whl

---
+++ packages/move-ugc-python/move_ugc_python-3.2.0-py3-none-any.whl/move_ugc/schemas/constants.py
@@ -0,0 +1,20 @@
+"""Constant values for the client."""
+
+from typing import Literal
+
+AUTHORIZATION_HEADER = "authorization"
+MINIMUM_API_KEY_LENGTH = 12
+ALLOWED_EXPAND_ATTRS = Literal[
+    "client",
+    "outputs",
+    "take",
+    "sources",
+    "volume",
+    "rig",
+]
+CLIENT_LITERAL = "client"
+TAKE_LITERAL = "take"
+OUTPUTS_LITERAL = "outputs"
+SOURCES_LITERAL = "sources"
+VOLUME_LITERAL = "volume"
+RIGS_LITERAL = "rig"
