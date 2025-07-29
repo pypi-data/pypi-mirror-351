fdf0b3e1daa59a21b690b8b4516b4be04cfa8fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:33 +0000

    Add package lingua-language-detector/1.4.2/lingua_language_detector-1.4.2.tar.gz

---
+++ packages/lingua-language-detector/lingua_language_detector-1.4.2.tar.gz/lingua_language_detector-1.4.2/lingua/_model.py
@@ -0,0 +1,223 @@
+#
+# Copyright © 2022-present Peter M. Stahl pemistahl@gmail.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import brotli
+import json
+import regex
+
+from collections import Counter, defaultdict, OrderedDict
+from dataclasses import dataclass
+from enum import Enum, auto
+from fractions import Fraction
+from math import log
+from pathlib import Path
+from typing import Any, Optional
+
+from .language import Language
+from ._ngram import _NgramRange, _get_ngram_name_by_length
+
+
+class _NgramProbabilitiesJSONEncoder(json.JSONEncoder):
+    def default(self, obj: Any) -> Any:
+        if isinstance(obj, _TrainingDataLanguageModel):
+            language = obj.language.name
+            ngrams = self.encode_frequencies(obj.relative_frequencies)
+            return {"language": language, "ngrams": ngrams}
+        return json.JSONEncoder.default(self, obj)
+
+    def encode_frequencies(self, obj: Optional[dict[str, Fraction]]) -> dict[str, str]:
+        fractions_to_ngrams = defaultdict(list)
+        if obj is not None:
+            for ngram, fraction in obj.items():
+                fractions_to_ngrams[fraction].append(ngram)
+
+        fractions_to_joined_ngrams = OrderedDict()
+        for fraction, ngrams in fractions_to_ngrams.items():
+            fraction_str = f"{fraction.numerator}/{fraction.denominator}"
+            fractions_to_joined_ngrams[fraction_str] = " ".join(
+                sorted(map(lambda n: n, ngrams))
+            )
+        return fractions_to_joined_ngrams
+
+
+class _NgramProbabilitiesJSONDecoder(json.JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+
+    def object_hook(self, obj: Any) -> Any:
+        if isinstance(obj, dict) and "language" in obj and "ngrams" in obj:
+            language = Language[obj["language"]]
+            ngrams = self.parse_frequencies(obj["ngrams"])
+            return _NgramProbabilityModel(language, ngrams)
+        return obj
+
+    def parse_frequencies(self, obj: dict[str, str]) -> dict[str, float]:
+        frequencies = {}
+        for fraction, ngrams in obj.items():
+            numerator, denominator = fraction.split("/")
+            frequency = log(int(numerator) / int(denominator))
+            for ngram in ngrams.split(" "):
+                frequencies[ngram] = frequency
+        return frequencies
+
+
+class _NgramsJSONDecoder(json.JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+
+    def object_hook(self, obj: Any) -> Any:
+        if isinstance(obj, dict) and "language" in obj and "ngrams" in obj:
+            language = Language[obj["language"]]
+            ngrams = self.object_hook(obj["ngrams"])
+            return _NgramCountModel(language, frozenset(ngrams))
+        return obj
+
+
+@dataclass
+class _NgramProbabilityModel:
+    language: Language
+    ngrams: dict[str, float]
+
+
+@dataclass
+class _NgramCountModel:
+    language: Language
+    ngrams: frozenset[str]
+
+
+class _NgramModelType(Enum):
+    UNIQUE = auto()
+    MOSTCOMMON = auto()
+
+
+def _load_ngram_probability_model(
+    language: Language, ngram_length: int
+) -> Optional[_NgramProbabilityModel]:
+    ngram_name = _get_ngram_name_by_length(ngram_length)
+    iso_code = language.iso_code_639_1.name.lower()
+    relative_file_path = f"./language-models/{iso_code}/{ngram_name}s.json.br"
+    absolute_file_path = Path(__file__).parent / relative_file_path
+    try:
+        with open(absolute_file_path, mode="rb") as ngrams_file:
+            ngrams_json = brotli.decompress(ngrams_file.read()).decode("utf-8")
+            return json.loads(ngrams_json, cls=_NgramProbabilitiesJSONDecoder)
+    except FileNotFoundError:
+        return None
+
+
+def _load_ngram_count_model(
+    language: Language, ngram_length: int, model_type: _NgramModelType
+) -> Optional[_NgramCountModel]:
+    ngram_name = _get_ngram_name_by_length(ngram_length)
+    iso_code = language.iso_code_639_1.name.lower()
+    relative_file_path = (
+        f"./language-models/{iso_code}/{model_type.name.lower()}_{ngram_name}s.json.br"
+    )
+    absolute_file_path = Path(__file__).parent / relative_file_path
+    try:
+        with open(absolute_file_path, mode="rb") as unique_ngrams_file:
+            unique_ngrams_json = brotli.decompress(unique_ngrams_file.read()).decode(
+                "utf-8"
+            )
+            return json.loads(unique_ngrams_json, cls=_NgramsJSONDecoder)
+    except FileNotFoundError:
+        return None
+
+
+@dataclass
+class _TrainingDataLanguageModel:
+    language: Language
+    absolute_frequencies: Optional[dict[str, int]]
+    relative_frequencies: Optional[dict[str, Fraction]]
+
+    @classmethod
+    def from_text(
+        cls,
+        text: list[str],
+        language: Language,
+        ngram_length: int,
+        char_class: str,
+        lower_ngram_absolute_frequencies: Optional[dict[str, int]],
+    ) -> "_TrainingDataLanguageModel":
+        absolute_frequencies = cls.compute_absolute_frequencies(
+            text, ngram_length, char_class
+        )
+        relative_frequencies = cls.compute_relative_frequencies(
+            ngram_length, absolute_frequencies, lower_ngram_absolute_frequencies
+        )
+        return _TrainingDataLanguageModel(
+            language=language,
+            absolute_frequencies=absolute_frequencies,
+            relative_frequencies=relative_frequencies,
+        )
+
+    def to_json(self) -> str:
+        return regex.sub(
+            r"([:,])\s*",
+            r"\1",
+            json.dumps(self, ensure_ascii=False, cls=_NgramProbabilitiesJSONEncoder),
+        )
+
+    @classmethod
+    def compute_absolute_frequencies(
+        cls, text: list[str], ngram_length: int, char_class: str
+    ) -> dict[str, int]:
+        absolute_frequencies: Counter[str] = Counter()
+        regexp = regex.compile(r"^[{}]+$".format(char_class))
+        for line in text:
+            lowercased_line = line.lower()
+            for i in range(0, len(lowercased_line) - ngram_length + 1):
+                substr = lowercased_line[i : i + ngram_length]
+                if regexp.match(substr) is not None:
+                    absolute_frequencies.update([substr])
+        return absolute_frequencies
+
+    @classmethod
+    def compute_relative_frequencies(
+        cls,
+        ngram_length: int,
+        absolute_frequencies: dict[str, int],
+        lower_ngram_absolute_frequencies: Optional[dict[str, int]],
+    ) -> dict[str, Fraction]:
+        if lower_ngram_absolute_frequencies is None:
+            return {}
+        ngram_probabilities = {}
+        total_ngram_frequency = sum(absolute_frequencies.values())
+        for ngram, frequency in absolute_frequencies.items():
+            if ngram_length == 1 or len(lower_ngram_absolute_frequencies) == 0:
+                denominator = total_ngram_frequency
+            else:
+                substr = ngram[: ngram_length - 1]
+                denominator = lower_ngram_absolute_frequencies[substr]
+            ngram_probabilities[ngram] = Fraction(frequency, denominator)
+        return ngram_probabilities
+
+
+def _create_ngrams(words: list[str], ngram_length: int) -> frozenset[str]:
+    if ngram_length not in range(1, 6):
+        raise ValueError(f"ngram length {ngram_length} is not in range 1..6")
+    ngrams = set()
+    for word in words:
+        chars_count = len(word)
+        if chars_count >= ngram_length:
+            for i in range(0, chars_count - ngram_length + 1):
+                substr = word[i : i + ngram_length]
+                ngrams.add(substr)
+    return frozenset(ngrams)
+
+
+def _create_lower_order_ngrams(words: list[str], ngram_length: int) -> list[list[str]]:
+    ngrams = _create_ngrams(words, ngram_length)
+    return [list(_NgramRange(ngram)) for ngram in ngrams]
