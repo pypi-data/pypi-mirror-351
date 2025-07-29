2ea90d98d4be6f3cf26f9432c10930584ae5c497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:57:12 +0000

    Add package dopynion/0.4.11/dopynion-0.4.11-py3-none-any.whl

---
+++ packages/dopynion/dopynion-0.4.11-py3-none-any.whl/dopynion/data_model.py
@@ -0,0 +1,93 @@
+from datetime import datetime
+from enum import StrEnum
+
+from pydantic import BaseModel, Field
+
+
+class CardName(StrEnum):  # Create with a metaclass
+    ADVENTURER = "adventurer"
+    BUREAUCRAT = "bureaucrat"
+    CELLAR = "cellar"
+    CHANCELLOR = "chancellor"
+    CHAPEL = "chapel"
+    COPPER = "copper"
+    COUNCILROOM = "councilroom"
+    CURSE = "curse"
+    DUCHY = "duchy"
+    ESTATE = "estate"
+    FEAST = "feast"
+    FESTIVAL = "festival"
+    GARDENS = "gardens"
+    GOLD = "gold"
+    LABORATORY = "laboratory"
+    LIBRARY = "library"
+    MARKET = "market"
+    MILITIA = "militia"
+    MINE = "mine"
+    MONEYLENDER = "moneylender"
+    NONE = "NONE"
+    PROVINCE = "province"
+    REMODEL = "remodel"
+    SILVER = "silver"
+    SMITHY = "smithy"
+    VILLAGE = "village"
+    WITCH = "witch"
+    WOODCUTTER = "woodcutter"
+    WORKSHOP = "workshop"
+
+    def __repr__(self) -> str:
+        return self.value.title()
+
+
+class Cards(BaseModel):
+    quantities: dict[CardName, int] = Field(default_factory=dict)
+
+
+class Player(BaseModel):
+    name: str
+    hand: Cards | None
+    score: int
+
+
+class Game(BaseModel):
+    finished: bool
+    players: list[Player]
+    stock: Cards
+
+
+class ActionRecord(BaseModel):
+    action: str
+    player: Player
+    score: int
+
+
+class ErrorRecord(BaseModel):
+    error: str
+    player: Player
+
+
+class PlayerTurnRecord(BaseModel):
+    actions: list[ActionRecord | ErrorRecord] = Field(default_factory=list)
+
+
+class GameRecord(BaseModel):
+    date: datetime
+    turns: list[PlayerTurnRecord] = Field(default_factory=list)
+    scores: dict[str, int] = Field(default_factory=dict)
+
+
+class CardNameAndHand(BaseModel):
+    card_name: CardName
+    hand: list[CardName]
+
+
+class Hand(BaseModel):
+    hand: list[CardName]
+
+
+class PossibleCards(BaseModel):
+    possible_cards: list[CardName]
+
+
+class MoneyCardsInHand(BaseModel):
+    money_in_hand: list[CardName]
