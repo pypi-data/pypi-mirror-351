21d055a0813516a90baac27d12af0f5b6c45c74e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:41 +0000

    Add package ui-server/0.0.7/ui_server-0.0.7.tar.gz

---
+++ packages/ui-server/ui_server-0.0.7.tar.gz/ui_server-0.0.7/README.md
@@ -0,0 +1,3 @@
+## UIServer
+
+A UI server for [RLtools](https://rl.tools) environments. Check out e.g. [rl-tools/l2f](https://github.com/rl-tools/l2f)
---
+++ packages/ui-server/ui_server-0.0.7.tar.gz/ui_server-0.0.7/ui_server/rl-tools/tools/deepsweep/client/client.sh
@@ -0,0 +1,20 @@
+set -e
+: "${SWEEP_SERVER:?SWEEP_SERVER is not set}"
+: "${JOB_ID:?JOB_ID is not set}"
+
+# work
+cd rl-tools
+while true; do
+  echo curl --fail -X POST $SWEEP_SERVER/jobs/$JOB_ID/tasks
+  task=$(curl --fail -X POST $SWEEP_SERVER/jobs/$JOB_ID/tasks)
+  echo task: $task
+  task_id=$(echo $task | jq -r '.task_id')
+  echo $task $task_id
+  CMD="MKL_NUM_THREADS=1 OPENBLAS_NUM_THREADS=1 RL_TOOLS_SAVE_TRAJECTORIES=0 RL_TOOLS_NN_ANALYTICS=0 RL_TOOLS_EXTRACK_EXPERIMENT=$JOB_ID ./build/src/foundation_policy/foundation_policy_pre_training $(echo $task | jq -r '.spec')"
+  echo $CMD
+  eval $CMD
+  result='{"return": "finished"}'
+  curl -X POST -H 'Content-Type: application/json' --data "$result" $SWEEP_SERVER/jobs/$JOB_ID/tasks/$task_id
+done
+
+# done
