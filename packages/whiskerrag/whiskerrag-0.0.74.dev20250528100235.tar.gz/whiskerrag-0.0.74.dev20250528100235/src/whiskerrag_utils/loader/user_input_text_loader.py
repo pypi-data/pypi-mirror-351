8eabcad91b2eccd4c184c1a5a28c6bc2b4b10d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:04 +0000

    Add package whiskerrag/0.0.74.dev20250528100235/whiskerrag-0.0.74.dev20250528100235.tar.gz

---
+++ packages/whiskerrag/whiskerrag-0.0.74.dev20250528100235.tar.gz/whiskerrag-0.0.74.dev20250528100235/src/whiskerrag_utils/loader/user_input_text_loader.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from whiskerrag_types.interface.loader_interface import BaseLoader
+from whiskerrag_types.model.knowledge import KnowledgeSourceEnum, TextSourceConfig
+from whiskerrag_types.model.multi_modal import Text
+from whiskerrag_utils.registry import RegisterTypeEnum, register
+
+
+@register(RegisterTypeEnum.KNOWLEDGE_LOADER, KnowledgeSourceEnum.USER_INPUT_TEXT)
+class TextLoader(BaseLoader[Text]):
+
+    async def load(self) -> List[Text]:
+        if isinstance(self.knowledge.source_config, TextSourceConfig):
+            return [Text(content=self.knowledge.source_config.text, metadata={})]
+        raise AttributeError(
+            "source_config does not have a 'text' attribute for the current type."
+        )
