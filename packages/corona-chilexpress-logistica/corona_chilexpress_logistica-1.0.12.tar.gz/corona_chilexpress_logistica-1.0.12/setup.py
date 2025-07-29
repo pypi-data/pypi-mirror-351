f5dbc10e725e8d34402ef90a14f2c3fa1071872f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:45 +0000

    Add package corona-chilexpress-logistica/1.0.12/corona_chilexpress_logistica-1.0.12.tar.gz

---
+++ packages/corona-chilexpress-logistica/corona_chilexpress_logistica-1.0.12.tar.gz/corona_chilexpress_logistica-1.0.12/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+setup(
+    name='corona-chilexpress-logistica',
+    version='1.0.12',
+    packages=['chilexpress'],
+    description='Django Chilexpress integration',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Corona Development Team',
+    author_email='rolguin@corona.cl',
+    url='https://gitlab.com/corona-projects/python-libraries/django-chilexpress/',
+    license='MIT',
+    python_requires=">=3.7",
+    install_requires=[
+        'Django>=3',
+        'requests>=2.25.1'
+    ]
+)
