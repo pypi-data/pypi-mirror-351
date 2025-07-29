d280ebb1b8208b351295ee575d04235398a9025f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:48 +0000

    Add package aes-gcm-txt-enc-lib/0.1.0/aes_gcm_txt_enc_lib-0.1.0-py3-none-any.whl

---
+++ packages/aes-gcm-txt-enc-lib/aes_gcm_txt_enc_lib-0.1.0-py3-none-any.whl/aes_gcm_txt_enc_lib/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for aes_gcm_txt_enc_lib."""
+
+__author__ = """Komal Singh"""
+__email__ = 'komalsin9h@gmail.com'
+__version__ = '0.1.0'
+
+
+from aes_gcm_txt_enc_lib.aes_gcm_encryptor import AesGcmEncryptor
+from aes_gcm_txt_enc_lib.core import generate_key, encrypt, decrypt
+
+
