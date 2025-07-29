4863292b21887498ea81b7d7188be685ac920725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:20 +0000

    Add package aiogramx/3.1.0/aiogramx-3.1.0-py3-none-any.whl

---
+++ packages/aiogramx/aiogramx-3.1.0-py3-none-any.whl/aiogramx/__init__.py
@@ -0,0 +1,12 @@
+from .calendar import Calendar
+from .checkbox import Checkbox
+from .pagination import Paginator
+from .time_selector import TimeSelectorGrid, TimeSelectorModern
+
+__all__ = [
+    "Paginator",
+    "Calendar",
+    "TimeSelectorModern",
+    "TimeSelectorGrid",
+    "Checkbox",
+]
