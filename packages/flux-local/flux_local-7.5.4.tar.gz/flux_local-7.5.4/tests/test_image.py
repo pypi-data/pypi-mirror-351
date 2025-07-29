e33616dce096e6cbc4c96d83aa1c819feb69ccf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:27 +0000

    Add package flux-local/7.5.4/flux_local-7.5.4.tar.gz

---
+++ packages/flux-local/flux_local-7.5.4.tar.gz/flux_local-7.5.4/tests/test_image.py
@@ -0,0 +1,50 @@
+"""Tests for image."""
+
+from pathlib import Path
+from typing import Any
+
+import pytest
+from syrupy.assertion import SnapshotAssertion
+
+from flux_local.git_repo import build_manifest, ResourceSelector, PathSelector
+from flux_local.image import ImageVisitor
+
+TESTDATA = Path("tests/testdata/cluster8")
+CWD = Path.cwd()
+
+
+@pytest.mark.parametrize(
+    ("test_path", "expected"),
+    [
+        (
+            CWD / "tests/testdata/cluster8",
+            {"flux-system/apps": {"alpine", "busybox"}},
+        ),
+        (
+            CWD / "tests/testdata/cluster7",
+            {},
+        ),
+        (
+            CWD / "tests/testdata/cluster",
+            {
+                "flux-system/infra-configs": {
+                    "ceph/ceph:v16.2.6",
+                    "ghcr.io/cloudnative-pg/postgis:17-3.4",
+                },
+            },
+        ),
+    ],
+)
+async def test_image_visitor(
+    snapshot: SnapshotAssertion, test_path: str, expected: dict[str, Any]
+) -> None:
+    """Tests for building the manifest."""
+
+    image_visitor = ImageVisitor()
+    query = ResourceSelector(
+        path=PathSelector(Path(test_path)),
+        doc_visitor=image_visitor.repo_visitor(),
+    )
+    await build_manifest(selector=query)
+
+    assert image_visitor.images == expected
