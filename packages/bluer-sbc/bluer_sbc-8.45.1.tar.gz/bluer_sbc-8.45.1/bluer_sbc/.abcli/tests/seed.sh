2c2a4766ad1acc51aad180340c47adfb344d0205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:57:13 +0000

    Add package bluer-sbc/8.45.1/bluer_sbc-8.45.1.tar.gz

---
+++ packages/bluer-sbc/bluer_sbc-8.45.1.tar.gz/bluer_sbc-8.45.1/bluer_sbc/.abcli/tests/seed.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+function test_bluer_sbc_seed() {
+    local options=$1
+
+    local target
+    for target in \
+        headless_rpi \
+        jetson \
+        rpi; do
+        bluer_ai_eval ,$options \
+            bluer_ai_seed $target screen
+        [[ $? -ne 0 ]] && return 1
+    done
+
+    return 0
+}
