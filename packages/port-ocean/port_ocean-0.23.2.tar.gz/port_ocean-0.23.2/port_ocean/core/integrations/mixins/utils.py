582459bc3213f0188ba38c3f2690e353f8b47cf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:04 +0000

    Add package port-ocean/0.23.2/port_ocean-0.23.2.tar.gz

---
+++ packages/port-ocean/port_ocean-0.23.2.tar.gz/port_ocean-0.23.2/port_ocean/core/integrations/mixins/utils.py
@@ -0,0 +1,90 @@
+from contextlib import contextmanager
+from typing import Awaitable, Generator, Callable
+
+from loguru import logger
+
+import asyncio
+import multiprocessing
+
+from port_ocean.core.ocean_types import (
+    ASYNC_GENERATOR_RESYNC_TYPE,
+    RAW_RESULT,
+    RESYNC_EVENT_LISTENER,
+    RESYNC_RESULT,
+)
+from port_ocean.core.utils.utils import validate_result
+from port_ocean.exceptions.core import (
+    RawObjectValidationException,
+    OceanAbortException,
+    KindNotImplementedException,
+)
+
+
+@contextmanager
+def resync_error_handling() -> Generator[None, None, None]:
+    try:
+        yield
+    except RawObjectValidationException as error:
+        err_msg = f"Failed to validate raw data for returned data from resync function, error: {error}"
+        logger.exception(err_msg)
+        raise OceanAbortException(err_msg) from error
+    except StopAsyncIteration:
+        raise
+    except Exception as error:
+        err_msg = f"Failed to execute resync function, error: {error}"
+        logger.exception(err_msg)
+        raise OceanAbortException(err_msg) from error
+
+
+async def resync_function_wrapper(
+    fn: Callable[[str], Awaitable[RAW_RESULT]], kind: str
+) -> RAW_RESULT:
+    with resync_error_handling():
+        results = await fn(kind)
+        return validate_result(results)
+
+
+async def resync_generator_wrapper(
+    fn: Callable[[str], ASYNC_GENERATOR_RESYNC_TYPE], kind: str
+) -> ASYNC_GENERATOR_RESYNC_TYPE:
+    generator = fn(kind)
+    errors = []
+    try:
+        while True:
+            try:
+                with resync_error_handling():
+                    result = await anext(generator)
+                    yield validate_result(result)
+            except OceanAbortException as error:
+                errors.append(error)
+    except StopAsyncIteration:
+        if errors:
+            raise ExceptionGroup(
+                "At least one of the resync generator iterations failed", errors
+            )
+
+
+def is_resource_supported(
+    kind: str, resync_event_mapping: dict[str | None, list[RESYNC_EVENT_LISTENER]]
+) -> bool:
+    return bool(resync_event_mapping[kind] or resync_event_mapping[None])
+
+
+def unsupported_kind_response(
+    kind: str, available_resync_kinds: list[str]
+) -> tuple[RESYNC_RESULT, list[Exception]]:
+    logger.error(f"Kind {kind} is not supported in this integration")
+    return [], [KindNotImplementedException(kind, available_resync_kinds)]
+
+class ProcessWrapper(multiprocessing.Process):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def join_async(self) -> None:
+        while self.exitcode is None:
+            await asyncio.sleep(2)
+        if self.exitcode != 0:
+            logger.error(f"Process {self.pid} failed with exit code {self.exitcode}")
+        else:
+            logger.info(f"Process {self.pid} finished with exit code {self.exitcode}")
+        return super().join()
