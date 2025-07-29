a788c87633d6cd938d70c6ea0992ce96014190f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:17 +0000

    Add package cvalaval/0.2/cvalaval-0.2.tar.gz

---
+++ packages/cvalaval/cvalaval-0.2.tar.gz/cvalaval-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cvalaval',
+    version='0.2',
+    packages=find_packages(),
+    description='Fast math computations and helpers',
+    author='Airwolftomato',
+    python_requires='>=3.7',
+)
