73a21bf9311a8e9c1c029741503f140b0edf3738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:38 +0000

    Add package decentralearn/2.1.2/decentralearn-2.1.2-py3-none-any.whl

---
+++ packages/decentralearn/decentralearn-2.1.2-py3-none-any.whl/decentralearn/dataset/__init__.py
@@ -0,0 +1,8 @@
+'''
+Dataset handling for DecentraLearn
+'''
+
+from .DatasetFactory import DatasetFactory
+from .DatasetSpliter import DatasetSpliter
+
+__all__ = ['DatasetFactory', 'DatasetSpliter'] 
