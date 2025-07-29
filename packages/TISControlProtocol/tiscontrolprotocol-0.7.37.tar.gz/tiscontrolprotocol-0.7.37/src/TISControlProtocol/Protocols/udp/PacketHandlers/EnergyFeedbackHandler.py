abbf898e80cba0f6fa4b55828462d9acca70a95b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:23 +0000

    Add package TISControlProtocol/0.7.37/tiscontrolprotocol-0.7.37.tar.gz

---
+++ packages/TISControlProtocol/tiscontrolprotocol-0.7.37.tar.gz/tiscontrolprotocol-0.7.37/src/TISControlProtocol/Protocols/udp/PacketHandlers/EnergyFeedbackHandler.py
@@ -0,0 +1,95 @@
+from homeassistant.core import HomeAssistant
+import struct
+import logging
+
+
+async def big_endian_to_num(str_list):
+    big_endian = "".join(str(x) for x in str_list)
+    value = struct.unpack(">f", bytes.fromhex(big_endian))[0]
+    return round(value, 1)
+
+
+async def handle_energy_feedback(hass: HomeAssistant, info: dict):
+    """
+    Handle the feedback from an energy sensor.
+    """
+    device_id = info["device_id"]
+    channel_num = int(info["additional_bytes"][0]) + 1
+    sub_operation = int(info["additional_bytes"][1])
+    logging.warning(f"info {info} ,, sub_operation {sub_operation}")
+
+    if sub_operation == 0xDA:
+        # [energy, monthly power, phase1, phase2, phase3]
+        energy = int(
+            (info["additional_bytes"][16] << 8) | (info["additional_bytes"][17])
+        )
+
+        event_data = {
+            "device_id": device_id,
+            "channel_num": channel_num,
+            "feedback_type": "monthly_energy_feedback",
+            "energy": energy,
+            "additional_bytes": info["additional_bytes"],
+        }
+
+        try:
+            hass.bus.async_fire(str(info["device_id"]), event_data)
+        except Exception as e:
+            logging.error(f"error in firing event for feedback: {e}")
+    elif sub_operation == 0x65:
+        logging.error(f"energy 0x65 packet: {info['additional_bytes']}")
+        try:
+            energy = {
+                "v1": await big_endian_to_num(info["additional_bytes"][3:7]),
+                "v2": await big_endian_to_num(info["additional_bytes"][7:11]),
+                "v3": await big_endian_to_num(info["additional_bytes"][11:15]),
+                "current_p1": await big_endian_to_num(info["additional_bytes"][15:19]),
+                "current_p2": await big_endian_to_num(info["additional_bytes"][19:23]),
+                "current_p3": await big_endian_to_num(info["additional_bytes"][23:27]),
+                "active_p1": await big_endian_to_num(info["additional_bytes"][27:31]),
+                "active_p2": await big_endian_to_num(info["additional_bytes"][31:35]),
+                "active_p3": await big_endian_to_num(info["additional_bytes"][35:39]),
+                "apparent1": await big_endian_to_num(info["additional_bytes"][39:43]),
+                "apparent2": await big_endian_to_num(info["additional_bytes"][43:47]),
+                "apparent3": await big_endian_to_num(info["additional_bytes"][47:51]),
+                "reactive1": await big_endian_to_num(info["additional_bytes"][51:55]),
+                "reactive2": await big_endian_to_num(info["additional_bytes"][55:59]),
+                "reactive3": await big_endian_to_num(info["additional_bytes"][59:63]),
+                "pf1": await big_endian_to_num(info["additional_bytes"][63:67]),
+                "pf2": await big_endian_to_num(info["additional_bytes"][67:71]),
+                "pf3": await big_endian_to_num(info["additional_bytes"][71:75]),
+                "pa1": await big_endian_to_num(info["additional_bytes"][75:79]),
+                "pa2": await big_endian_to_num(info["additional_bytes"][79:83]),
+                "pa3": await big_endian_to_num(info["additional_bytes"][83:87]),
+                "avg_live_to_neutral": await big_endian_to_num(
+                    info["additional_bytes"][87:91]
+                ),
+                "avg_current": await big_endian_to_num(info["additional_bytes"][91:95]),
+                "sum_current": await big_endian_to_num(info["additional_bytes"][95:99]),
+                "total_power": await big_endian_to_num(
+                    info["additional_bytes"][107:111]
+                ),
+                "total_volt_amps": await big_endian_to_num(
+                    info["additional_bytes"][115:119]
+                ),
+                "total_var": await big_endian_to_num(info["additional_bytes"][123:127]),
+                "total_pf": await big_endian_to_num(info["additional_bytes"][127:131]),
+                "total_pa": await big_endian_to_num(info["additional_bytes"][135:139]),
+                "frq": await big_endian_to_num(info["additional_bytes"][143:147]),
+            }
+
+            logging.error(f"energy data prepared {energy}")
+
+            event_data = {
+                "device_id": device_id,
+                "channel_num": channel_num,
+                "feedback_type": "energy_feedback",
+                "energy": energy,
+                "additional_bytes": info["additional_bytes"],
+            }
+            logging.error(f"event data got changed {event_data}")
+
+            hass.bus.async_fire(str(info["device_id"]), event_data)
+            logging.error(f"event got fired {info["additional_bytes"]}")
+        except Exception as e:
+            logging.error(f"error in firing event for feedback: {e}")
