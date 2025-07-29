71c1e8b77c58aab5c29473f8101744d33f61fd0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:34 +0000

    Add package minikerberos-bAD/0.4.10/minikerberos_bad-0.4.10-py3-none-any.whl

---
+++ packages/minikerberos-bAD/minikerberos_bad-0.4.10-py3-none-any.whl/minikerberos/_version.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.4.10"
+__banner__ = \
+"""
+# minikerberos %s 
+# Author: Tamas Jos @skelsec (skelsecprojects@gmail.com)
+# Current Maintainer: Baptiste Crépin (baptiste@cravaterouge.com)
+""" % __version__
