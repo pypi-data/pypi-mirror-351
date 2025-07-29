98d6605e3ecbcde4266905fd81094a72f241f351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:46 +0000

    Add package hathitrust-downloader/1.4.0/hathitrust_downloader-1.4.0.tar.gz

---
+++ packages/hathitrust-downloader/hathitrust_downloader-1.4.0.tar.gz/hathitrust_downloader-1.4.0/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+import os
+
+import hathitrustdownloader
+
+
+setup(
+    name='hathitrust_downloader',
+    version=os.environ.get('HATHITRUST_DOWNLOADER_VERSION', '0.0.0'),
+    description='Downloads multiple pages from Hathitrust from the CLI.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='http://github.com/addono/hathitrust-downloader',
+    author='Adriaan Knapen',
+    author_email='hi@aknapen.nl',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+    ],
+    keywords='hathitrust download downloader cli',
+    packages=find_packages(),
+    install_requires=[
+        'requests==2.32.3',
+        'tqdm==4.66.5',
+    ],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'hathitrust-downloader=hathitrustdownloader.cli:main',
+        ],
+    }
+)
+
