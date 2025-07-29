00bee7630f1917b4e601e05e18c9fae0dea958a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:15 +0000

    Add package evewiki/0.1.0/evewiki-0.1.0.tar.gz

---
+++ packages/evewiki/evewiki-0.1.0.tar.gz/evewiki-0.1.0/evewiki/auth_hooks.py
@@ -0,0 +1,35 @@
+from django.utils.translation import gettext_lazy as _
+
+from allianceauth import hooks
+from allianceauth.services.hooks import MenuItemHook, UrlHook
+
+from . import urls
+
+
+class EveWikiMenuItem(MenuItemHook):
+    """This class ensures only authorized users will see the menu entry"""
+
+    def __init__(self):
+        # setup menu entry for sidebar
+        MenuItemHook.__init__(
+            self,
+            _("wiki"),
+            "fas fa-tree fa-fw",
+            "evewiki:index",
+            navactive=["evewiki:"],
+        )
+
+    def render(self, request):
+        if request.user.has_perm("evewiki.basic_access"):
+            return MenuItemHook.render(self, request)
+        return ""
+
+
+@hooks.register("menu_item_hook")
+def register_menu():
+    return EveWikiMenuItem()
+
+
+@hooks.register("url_hook")
+def register_urls():
+    return UrlHook(urls, "evewiki", r"^evewiki/")
