cd92323229e450a1d58af86546ce1dcbc00d6964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:22 +0000

    Add package message-ix-models/2025.5.28/message_ix_models-2025.5.28.tar.gz

---
+++ packages/message-ix-models/message_ix_models-2025.5.28.tar.gz/message_ix_models-2025.5.28/message_ix_models/model/emissions.py
@@ -0,0 +1,161 @@
+import logging
+import re
+from typing import Optional
+
+import pandas as pd
+from genno import Quantity
+from genno import operator as g
+from iam_units import convert_gwp
+from message_ix import Scenario, make_df
+
+from message_ix_models import ScenarioInfo
+from message_ix_models.util import package_data_path
+
+from .structure import get_codes
+
+log = logging.getLogger(__name__)
+
+
+def get_emission_factors(units: Optional[str] = None) -> Quantity:
+    """Return carbon emission factors.
+
+    Values are from the file :file:`message_ix_models/data/ipcc/1996_v3_t1-2.csv`, in
+    turn from `IPCC <https://www.ipcc-nggip.iges.or.jp/public/gl/guidelin/ch1wb1.pdf>`_
+    (see Table 1-2 on page 1.6); these are the same that appear on the "Emissions from
+    energy" page of the MESSAGEix-GLOBIOM documentation.
+
+    The fuel dimension and names in the source are mapped to a :math:`c` ("commodity")
+    dimension and labels from :ref:`commodity.yaml <commodity-yaml>`, using the
+    ``ipcc-1996-name`` annotations appearing in the latter. A value for "methanol" that
+    appears in the MESSAGEix-GLOBIOM docs table but not in the source is appended.
+
+    Parameters
+    ----------
+    unit : str, optional
+        Expression for units of the returned quantity. Tested values include:
+
+        - “tC / TJ”, source units (default),
+        - “t CO2 / TJ”, and
+        - “t C / kWa”, internal units in MESSAGEix-GLOBIOM, for instance for
+          "relation_activity" entries for emissions relations.
+
+    Returns
+    -------
+    Quantity
+        with 1 dimension (:math:`c`).
+    """
+    # Prepare information about commodities
+    commodities = get_codes("commodity")
+    relabel = {}  # Mapping from IPCC names/IDs to message_ix_models commodity ID
+    select = []  # Select only the commodities needed
+    for c in commodities:
+        try:
+            ipcc_name = str(c.get_annotation(id="ipcc-1996-name").text)
+        except KeyError:
+            continue
+        else:
+            relabel[ipcc_name] = c.id
+            select.append(c.id)
+
+    # Load data from file; relabel; and select only the values needed
+    result = (
+        g.load_file(package_data_path("ipcc", "1996_v3_t1-2.csv"), dims={"fuel": "c"})
+        .pipe(g.relabel, dict(c=relabel))
+        .pipe(g.select, dict(c=select))
+    )
+
+    # Manually insert a value for methanol
+    result = g.concat(
+        result,
+        Quantity(pd.Series(17.4, pd.Index(["methanol"], name="c")), units=result.units),
+    )
+
+    result.attrs["species"] = "C"
+
+    if units is not None:
+        # Identify a GWP factor for target `units`, if any
+        to_units, to_species = split_species(units)
+        gwp_factor = convert_gwp(
+            "AR5GWP100", (1.0, str(result.units)), "C", to_species
+        ).magnitude
+    else:
+        gwp_factor, to_units = 1.0, result.units
+
+    # Multiply by the GWP factor; let genno/pint handle other conversion
+    return result.pipe(g.mul, Quantity(gwp_factor)).pipe(g.convert_units, to_units)
+
+
+def add_tax_emission(
+    scen: Scenario,
+    price: float,
+    conversion_factor: Optional[float] = None,
+    drate_parameter="drate",
+) -> None:
+    """Add a global CO₂ price to `scen`.
+
+    A carbon price is implemented on node=“World” by populating the
+    :ref:`MESSAGEix parameter <message-ix:section_parameter_emissions>`
+    ``tax_emission``, starting from the first model year and covering the entire model
+    horizon. The tax has an annual growth rate equal to the discount rate.
+
+    The other dimensions of ``tax_emission`` are filled with type_emission=“TCE” and
+    type_tec=“all”.
+
+    Parameters
+    ----------
+    scen : :class:`message_ix.Scenario`
+    price : float
+        Price in the first model year, in USD / tonne CO₂.
+    conversion_factor : float, optional
+        Factor for converting `price` into the model's internal emissions units,
+        currently USD / tonne carbon. Optional: a default value is retrieved from
+        :mod:`iam_units`.
+    drate_parameter : str; one of "drate" or "interestrate"
+        Name of the parameter to use for the growth rate of the carbon price.
+    """
+    years = ScenarioInfo(scen).Y
+    filters = dict(year=years)
+    # Default: since the mass of the species is in the denominator, take the inverse
+    conversion_factor = conversion_factor or 1.0 / convert_gwp(
+        "AR5GWP100", "1 t", "CO2", "C"
+    )
+
+    # Duration of periods
+    dp = scen.par("duration_period", filters=filters).set_index("year")["value"]
+
+    # Retrieve the discount rate
+    if drate_parameter == "interestrate":
+        # MESSAGE parameter with "year" dimension
+        r = scen.par(drate_parameter, filters=filters).set_index("year")["value"]
+    else:
+        # MACRO parameter with "node" dimension
+        drates = scen.par(drate_parameter).value.unique()
+        if len(drates) > 1:
+            log.warning(f"Using the first of multiple discount rates: drate={drates}")
+        r = pd.Series([drates[0]] * len(years), index=pd.Index(years, name="year"))
+
+    # Compute cumulative growth versus the first period
+    r_cumulative = (r + 1).pow(dp.shift(-1)).cumprod().shift(1, fill_value=1.0)
+
+    # Assemble the parameter data
+    name = "tax_emission"
+    data = make_df(
+        name,
+        value=(price * conversion_factor * r_cumulative),
+        type_year=r_cumulative.index,
+        node="World",
+        type_emission="TCE",
+        type_tec="all",
+        unit="USD/tC",
+    )
+
+    with scen.transact("Added carbon price"):
+        scen.add_par(name, data)
+
+
+def split_species(unit_expr: str) -> tuple[str, Optional[str]]:
+    """Split `unit_expr` to an expression without a unit mention, and maybe species."""
+    if match := re.fullmatch("(.*)(CO2|C)(.*)", unit_expr):
+        return f"{match.group(1)}{match.group(3)}", match.group(2)
+    else:
+        return unit_expr, None
---
+++ packages/message-ix-models/message_ix_models-2025.5.28.tar.gz/message_ix_models-2025.5.28/message_ix_models/tests/util/test_sdmx.py
@@ -0,0 +1,224 @@
+import logging
+import re
+
+import genno
+import pytest
+from genno import Computer, Key
+from sdmx.model.common import Code
+from sdmx.model.v21 import Annotation
+
+from message_ix_models.model.transport import (
+    build,
+    # Ensure .util.sdmx.DATAFLOW is populated. This seems needed only for Python ≤ 3.9
+    # TODO Remove once Python 3.9 is no longer supported
+    data,  # noqa: F401
+    testing,
+)
+from message_ix_models.util.sdmx import DATAFLOW, Dataflow, eval_anno, make_enum, read
+
+log = logging.getLogger(__name__)
+
+
+class TestDataflow:
+    """Test :class:`.Dataflow."""
+
+    @pytest.fixture
+    def any_df(self):
+        yield next(iter(DATAFLOW.values()))
+
+    # TODO Use a broader-scoped context to allow (scope="class")
+    @pytest.fixture
+    @pytest.mark.usefixtures("ssp_test_data")
+    def build_computer(self, test_context):
+        """A :class:`.Computer` from :func:`.configure_build`.
+
+        This in turn invokes :func:`.transport.build.add_exogenous_data`, which adds
+        each of :data:`.FILES` to a Computer.
+        """
+        c, _ = testing.configure_build(test_context, regions="R12", years="B")
+        yield c
+
+    def test_init(self, caplog) -> None:
+        # Message is logged for invalid units
+        Dataflow(module=__name__, name="test_init_0", path="test-init-0", units="foo")
+        assert "'foo' is not defined in the unit registry" in caplog.records[0].message
+
+        # Exception is raised for duplicate definition
+        with pytest.raises(
+            RuntimeError, match="Definition of .*DF_TEST_INIT_0.*duplicates"
+        ):
+            Dataflow(
+                module=__name__, name="test_init_0", path="test-init-0", units="foo"
+            )
+
+    def test_add_tasks(self, caplog, test_context) -> None:
+        c = Computer()
+
+        n, p = "test_add_tasks", "test-add-tasks"
+
+        # FileNotFoundError is raised when adding to Computer with no file
+        df0 = Dataflow(module=__name__, name=f"{n}0", path=f"{p}0", units="")
+        with pytest.raises(FileNotFoundError):
+            c.add("", df0, context=test_context)
+
+        # With required=False, no exception, but also no keys added
+        df1 = Dataflow(
+            module=__name__, name=f"{n}1", path=f"{p}1", units="", required=False
+        )
+        result = c.add("", df1, context=test_context)
+        assert () == result
+
+    @build.get_computer.minimum_version
+    @pytest.mark.usefixtures("iea_eweb_test_data", "ssp_user_data")
+    @pytest.mark.parametrize(
+        "file",
+        [f for f in DATAFLOW.values() if f.intent & Dataflow.FLAG.IN],
+        ids=lambda f: "-".join(f.path.parts),
+    )
+    def test_configure_build(
+        self, build_computer: "Computer", file: "Dataflow"
+    ) -> None:
+        """Input data can be read and has the expected dimensions."""
+        c = build_computer
+
+        # Task runs
+        result = c.get(file.key)
+
+        # Quantity is loaded
+        assert isinstance(result, genno.Quantity)
+
+        # Dimensions are as expected
+        assert set(Key(result).dims) == set(file.key.dims)
+
+    def test_generate_csv_template(self, any_df: "Dataflow") -> None:
+        with pytest.raises(NotImplementedError):
+            any_df.generate_csv_template()
+
+    def test_repr(self, any_df: "Dataflow") -> None:
+        urn = (
+            "urn:sdmx:org.sdmx.infomodel.datastructure.DataflowDefinition=IIASA_ECE:"
+            "DF_FREIGHT_ACTIVITY(2025.3.11)"
+        )
+        assert (
+            "<Dataflow wrapping "
+            "'DataflowDefinition=IIASA_ECE:DF_FREIGHT_ACTIVITY(2025.3.11)'>"
+            == repr(DATAFLOW[urn])
+        )
+
+    def test_required(self, any_df: "Dataflow") -> None:
+        """The :`ExogenousDataFiles.required` property has a :class:`bool` value."""
+        assert isinstance(any_df.required, bool)
+
+    def test_units(self, any_df: "Dataflow") -> None:
+        """The :`ExogenousDataFiles.units` property has a :class:`pint.Unit` value."""
+        import pint
+
+        assert isinstance(any_df.units, pint.Unit)
+
+
+def test_eval_anno(caplog, recwarn):
+    c = Code()
+
+    with pytest.warns(DeprecationWarning):
+        assert None is eval_anno(c, "foo")
+
+    c.annotations.append(Annotation(id="foo", text="bar baz"))
+
+    with (
+        caplog.at_level(logging.DEBUG, logger="message_ix_models"),
+        pytest.warns(DeprecationWarning),
+    ):
+        assert "bar baz" == eval_anno(c, "foo")
+
+    assert re.fullmatch(
+        r"Could not eval\('bar baz'\): .* \(<string>, line 1\)", caplog.messages[0]
+    )
+
+    c.annotations.append(Annotation(id="qux", text="3 + 4"))
+
+    with pytest.warns(DeprecationWarning):
+        assert 7 == eval_anno(c, id="qux")
+
+
+def test_make_enum0():
+    """:func:`.make_enum` works with :class:`~enum.Flag` and subclasses."""
+    from enum import Flag, IntFlag
+
+    E = make_enum("ICONICS:SSP(2017)", base=Flag)
+
+    # Values are bitwise flags
+    assert not isinstance(E["1"], int)
+
+    # Expected length
+    assert 2 ** (len(E) - 1) == list(E)[-1].value
+
+    # Flags can be combined
+    flags = E["1"] | E["2"]
+    assert E["1"] & flags
+    assert E["2"] & flags
+    assert not (E["3"] & flags)
+
+    # Similar, with IntFlag
+    E = make_enum("IIASA_ECE:AGENCIES(0.1)", base=IntFlag)
+
+    # Values are ints
+    assert isinstance(E["IIASA_ECE"], int)
+
+    # Expected length
+    assert 2 ** (len(E) - 1) == list(E)[-1].value
+
+
+_urn_prefix = "urn:sdmx:org.sdmx.infomodel.codelist"
+
+
+@pytest.mark.parametrize(
+    "urn, expected",
+    (
+        ("ICONICS:SSP(2017)", f"{_urn_prefix}.Code=ICONICS:SSP(2017).1"),
+        ("ICONICS:SSP(2024)", f"{_urn_prefix}.Code=ICONICS:SSP(2024).1"),
+        ("SSP(2017)", f"{_urn_prefix}.Code=ICONICS:SSP(2017).1"),
+        ("SSP(2024)", f"{_urn_prefix}.Code=ICONICS:SSP(2024).1"),
+        ("SSP", f"{_urn_prefix}.Code=ICONICS:SSP(2017).1"),
+        pytest.param(
+            "AGENCIES",
+            f"{_urn_prefix}.Agency=IIASA_ECE:AGENCIES(0.1).IEA",
+            marks=pytest.mark.xfail(raises=KeyError, reason="XML needs update"),
+        ),
+    ),
+)
+def test_make_enum1(urn, expected):
+    # make_enum() runs
+    E = make_enum(urn)
+
+    # A known URN retrieves an enumeration member
+    E.by_urn(expected)
+
+
+@pytest.mark.parametrize(
+    "urn, expected",
+    (
+        ("ICONICS:SSP(2017)", "Codelist=ICONICS:SSP(2017)"),
+        ("ICONICS:SSP(2024)", "Codelist=ICONICS:SSP(2024)"),
+        ("SSP(2017)", "Codelist=ICONICS:SSP(2017)"),
+        ("SSP(2024)", "Codelist=ICONICS:SSP(2024)"),
+        ("SSP", "Codelist=ICONICS:SSP(2017)"),
+        ("AGENCIES", "AgencyScheme=IIASA_ECE:AGENCIES(0.1)"),
+    ),
+)
+def test_read0(urn, expected):
+    obj = read(urn)
+    assert expected in obj.urn
+
+
+def test_read1():
+    SSPS = read("ssp")
+
+    # Identify an SSP by matching strings in its name
+    code0 = next(filter(lambda c: "2" in repr(c), iter(SSPS)))
+    code1 = next(filter(lambda c: "SSP2" in repr(c), iter(SSPS)))
+    code2 = next(filter(lambda c: "middle of the road" in repr(c).lower(), iter(SSPS)))
+
+    assert code0 is code1 is code2
+
+    with pytest.raises(FileNotFoundError):
+        read("foo")
