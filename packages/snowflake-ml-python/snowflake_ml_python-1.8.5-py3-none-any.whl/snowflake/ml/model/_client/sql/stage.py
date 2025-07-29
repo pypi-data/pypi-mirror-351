1f17466d4971f2d0619ac5e1c08ebed2ad828ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:13 +0000

    Add package snowflake-ml-python/1.8.5/snowflake_ml_python-1.8.5-py3-none-any.whl

---
+++ packages/snowflake-ml-python/snowflake_ml_python-1.8.5-py3-none-any.whl/snowflake/ml/model/_client/sql/stage.py
@@ -0,0 +1,20 @@
+from typing import Any, Optional
+
+from snowflake.ml._internal.utils import query_result_checker, sql_identifier
+from snowflake.ml.model._client.sql import _base
+
+
+class StageSQLClient(_base._BaseSQLClient):
+    def create_tmp_stage(
+        self,
+        *,
+        database_name: Optional[sql_identifier.SqlIdentifier],
+        schema_name: Optional[sql_identifier.SqlIdentifier],
+        stage_name: sql_identifier.SqlIdentifier,
+        statement_params: Optional[dict[str, Any]] = None,
+    ) -> None:
+        query_result_checker.SqlResultValidator(
+            self._session,
+            f"CREATE SCOPED TEMPORARY STAGE {self.fully_qualified_object_name(database_name, schema_name, stage_name)}",
+            statement_params=statement_params,
+        ).has_dimensions(expected_rows=1, expected_cols=1).validate()
