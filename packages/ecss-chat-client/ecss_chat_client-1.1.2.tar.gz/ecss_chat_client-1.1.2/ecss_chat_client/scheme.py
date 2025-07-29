ed1eeec4f85f92d36c71be84fc0618ed81e363e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:50 +0000

    Add package ecss-chat-client/1.1.2/ecss_chat_client-1.1.2.tar.gz

---
+++ packages/ecss-chat-client/ecss_chat_client-1.1.2.tar.gz/ecss_chat_client-1.1.2/ecss_chat_client/scheme.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from typing import List, Optional
+
+from pydantic import BaseModel, Field
+
+
+class IdMixin(BaseModel):
+    id: str = Field(alias='_id')
+
+
+class UpdatedAtMixin(BaseModel):
+    updatedAt: Optional[datetime] = Field(None, alias='_updatedAt')
+
+
+class TypeMixin(BaseModel):
+    type: str
+
+
+class UserModel(IdMixin, TypeMixin, UpdatedAtMixin):
+    active: bool
+    aliasId: str
+    createdAt: str
+    domain: str
+    name: str
+    notificationsDisabled: bool = False
+    preferences: List = []
+    roles: List[str]
+    username: str
+
+
+class AuthData(BaseModel):
+    userId: str
+    authToken: str
+    authTokenExpires: int
+    me: UserModel
+
+
+class AuthResponse(BaseModel):
+    status: str
+    data: AuthData
