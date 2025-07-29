eed2d6561a2a258bd7b5e150c7b052e79210fa7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:47 +0000

    Add package swh.loader.svn/2.3.2/swh_loader_svn-2.3.2.tar.gz

---
+++ packages/swh.loader.svn/swh_loader_svn-2.3.2.tar.gz/swh_loader_svn-2.3.2/swh/loader/svn/tests/test_task.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2019-2023  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import pytest
+
+from swh.scheduler.model import ListedOrigin
+
+from .conftest import NAMESPACE
+
+
+@pytest.fixture
+def svn_listed_origin(svn_lister):
+    return ListedOrigin(
+        lister_id=svn_lister.id, url="svn://example.org/repo", visit_type="svn"
+    )
+
+
+@pytest.mark.parametrize("extra_loader_arguments", [{}, {"visit_date": "now"}])
+def test_svn_loader_from_remote_dump_for_listed_origin(
+    loading_task_creation_for_listed_origin_test,
+    svn_lister,
+    svn_listed_origin,
+    extra_loader_arguments,
+):
+    svn_listed_origin.extra_loader_arguments = extra_loader_arguments
+
+    loading_task_creation_for_listed_origin_test(
+        loader_class_name=f"{NAMESPACE}.loader.SvnLoaderFromRemoteDump",
+        task_function_name=f"{NAMESPACE}.tasks.DumpMountAndLoadSvnRepository",
+        lister=svn_lister,
+        listed_origin=svn_listed_origin,
+    )
