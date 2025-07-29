b85babafb77962da531775f354ecb5dadfa0c898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:33 +0000

    Add package aistore/1.14.0/aistore-1.14.0.tar.gz

---
+++ packages/aistore/aistore-1.14.0.tar.gz/aistore-1.14.0/aistore/sdk/etl/etl_const.py
@@ -0,0 +1,39 @@
+# Defaults
+DEFAULT_ETL_COMM = "hpush"
+DEFAULT_ETL_TIMEOUT = "5m"
+DEFAULT_ETL_OBJ_TIMEOUT = "45s"
+DEFAULT_ETL_RUNTIME = "python3.13v2"
+
+# ETL comm types
+# ext/etl/api.go Hpush
+ETL_COMM_HPUSH = "hpush"
+# ext/etl/api.go Hpull
+ETL_COMM_HPULL = "hpull"
+# ext/etl/api.go WebSocket
+ETL_COMM_WS = "ws"
+# ext/etl/api.go HpushStdin
+ETL_COMM_IO = "io"
+
+# ETL lifecycle stages (see docs/etl.md#etl-pod-lifecycle)
+ETL_STAGE_INIT = "Initializing"
+ETL_STAGE_RUNNING = "Running"
+ETL_STAGE_STOPPED = "Stopped"
+
+ETL_COMM_CODE = [ETL_COMM_IO, ETL_COMM_HPUSH, ETL_COMM_HPULL]
+ETL_COMM_SPEC = [ETL_COMM_HPUSH, ETL_COMM_HPULL, ETL_COMM_WS]
+
+ETL_SUPPORTED_PYTHON_VERSIONS = ["3.9", "3.10", "3.11", "3.12", "3.13"]
+
+# templates for ETL
+
+CODE_TEMPLATE = """
+import pickle
+import base64
+import importlib
+
+for mod in {}:
+    importlib.import_module(mod)
+    
+transform = pickle.loads(base64.b64decode('{}'))
+{}
+"""
