82a495ab4375564a85ffc24c4c8039175e9290eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:25 +0000

    Add package nuclia/4.8.10/nuclia-4.8.10-py3-none-any.whl

---
+++ packages/nuclia/nuclia-4.8.10-py3-none-any.whl/nuclia/tests/test_kb/test_conversation.py
@@ -0,0 +1,32 @@
+import os
+
+from nucliadb_sdk.v2.exceptions import NotFoundError
+
+from nuclia.sdk.kb import NucliaKB
+from nuclia.sdk.resource import NucliaResource
+from nuclia.sdk.upload import NucliaUpload
+
+path = f"{os.path.dirname(__file__)}/../assets/conversation.json"
+
+
+def test_conversation(testing_config):
+    nkb = NucliaKB()
+    nresource = NucliaResource()
+    try:
+        res = nresource.get(slug="conversation1")
+        nresource.delete(rid=res.id)
+    except NotFoundError:
+        pass
+
+    res = nkb.list()
+    assert len(res.resources) == 2
+
+    nu = NucliaUpload()
+    nu.conversation(path=path, slug="conversation1", field="c1")
+
+    res = nresource.get(slug="conversation1", show=["values"])
+    assert res
+    assert res.data
+    assert res.data.conversations
+    assert res.data.conversations["c1"]
+    nresource.delete(rid=res.id)
