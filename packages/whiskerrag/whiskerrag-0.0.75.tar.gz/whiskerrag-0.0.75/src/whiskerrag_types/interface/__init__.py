6e8bbff343ada3eb7f5a92139673b2e76dade4f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:04 +0000

    Add package whiskerrag/0.0.75/whiskerrag-0.0.75.tar.gz

---
+++ packages/whiskerrag/whiskerrag-0.0.75.tar.gz/whiskerrag-0.0.75/src/whiskerrag_types/interface/__init__.py
@@ -0,0 +1,22 @@
+from .db_engine_plugin_interface import DBPluginInterface
+from .decomposer_interface import BaseDecomposer
+from .embed_interface import BaseEmbedding
+from .loader_interface import BaseLoader
+from .logger_interface import LoggerManagerInterface
+from .parser_interface import BaseParser, ParseResult
+from .retriever_interface import BaseRetriever
+from .settings_interface import SettingsInterface
+from .task_engine_plugin_interface import TaskEnginPluginInterface
+
+__all__ = [
+    "DBPluginInterface",
+    "BaseEmbedding",
+    "BaseParser",
+    "BaseRetriever",
+    "BaseLoader",
+    "LoggerManagerInterface",
+    "SettingsInterface",
+    "TaskEnginPluginInterface",
+    "BaseDecomposer",
+    "ParseResult",
+]
