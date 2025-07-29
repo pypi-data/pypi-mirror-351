99024a5f2387a5c0e7cda624cdd13703e60b5f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:27 +0000

    Add package sportsball/0.4.7/sportsball-0.4.7.tar.gz

---
+++ packages/sportsball/sportsball-0.4.7.tar.gz/sportsball-0.4.7/sportsball/data/nfl/aussportsbetting/nfl_aussportsbetting_league_model.py
@@ -0,0 +1,20 @@
+"""NFL aussportsbetting league model."""
+
+import requests_cache
+
+from ...aussportsbetting.aussportsbetting_league_model import \
+    AusSportsBettingLeagueModel
+from ...league import League
+
+
+class NFLAusSportsBettingLeagueModel(AusSportsBettingLeagueModel):
+    """NFL AusSportsBetting implementation of the league model."""
+
+    def __init__(
+        self, session: requests_cache.CachedSession, position: int | None = None
+    ) -> None:
+        super().__init__(League.NFL, session, position=position)
+
+    @classmethod
+    def name(cls) -> str:
+        return "nfl-aussportsbetting-league-model"
