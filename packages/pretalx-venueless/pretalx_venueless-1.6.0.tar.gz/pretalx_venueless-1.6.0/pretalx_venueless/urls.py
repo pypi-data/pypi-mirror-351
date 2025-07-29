ad03caabad8817e88483d0216fff12ca18cc3e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:40 +0000

    Add package pretalx-venueless/1.6.0/pretalx_venueless-1.6.0.tar.gz

---
+++ packages/pretalx-venueless/pretalx_venueless-1.6.0.tar.gz/pretalx_venueless-1.6.0/pretalx_venueless/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path(
+        "orga/event/<slug:event>/settings/p/venueless/",
+        views.Settings.as_view(),
+        name="settings",
+    ),
+    path(
+        "<slug:event>/p/venueless/check",
+        views.check,
+        name="check",
+    ),
+    path(
+        "<slug:event>/p/venueless/join",
+        views.SpeakerJoin.as_view(),
+        name="join",
+    ),
+]
