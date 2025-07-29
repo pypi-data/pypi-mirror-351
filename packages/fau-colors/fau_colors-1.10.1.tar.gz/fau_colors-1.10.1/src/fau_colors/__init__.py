e15cccbac8187bce2fa68040e750412520ec82bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:45 +0000

    Add package fau-colors/1.10.1/fau_colors-1.10.1.tar.gz

---
+++ packages/fau-colors/fau_colors-1.10.1.tar.gz/fau_colors-1.10.1/src/fau_colors/__init__.py
@@ -0,0 +1,26 @@
+__version__ = "1.10.1"
+
+from fau_colors._utils import export_as_gpl, export_as_tex
+from fau_colors.fonts import register_fausans_font
+from fau_colors.v2024 import (
+    cmaps,
+    cmaps_with_names,
+    colors,
+    colors_all,
+    colors_dark,
+    register_cmaps,
+    unregister_cmaps,
+)
+
+__all__ = [
+    "cmaps",
+    "cmaps_with_names",
+    "colors",
+    "colors_all",
+    "colors_dark",
+    "export_as_gpl",
+    "export_as_tex",
+    "register_cmaps",
+    "register_fausans_font",
+    "unregister_cmaps",
+]
---
+++ packages/fau-colors/fau_colors-1.10.1.tar.gz/fau_colors-1.10.1/src/fau_colors/v2019.py
@@ -0,0 +1,45 @@
+from collections import namedtuple
+from typing import Literal
+
+import seaborn as sns
+
+from fau_colors._utils import (
+    custom_blend_colormap,
+    get_register_func,
+    get_unregister_func,
+)
+
+__all__ = ["cmaps", "colors", "register_cmaps", "unregister_cmaps"]
+
+
+NAMED_COLORS = Literal["fau", "tech", "phil", "med", "nat", "wiso"]
+_LIGHTNESS_LEVELS = [0.125, 0.25, 0.375, 0.625, 1]
+
+
+_FacultyColors = namedtuple("FacultyColors", ["fau", "tech", "phil", "med", "nat", "wiso"])
+_CmapsAll = namedtuple("Cmaps", ["faculties", *_FacultyColors._fields])
+
+colors = _FacultyColors(
+    fau="#003865",
+    tech="#98a4ae",
+    phil="#c99313",
+    med="#00b1eb",
+    nat="#009b77",
+    wiso="#8d1429",
+)
+
+
+cmaps = _CmapsAll(
+    faculties=sns.color_palette(list(colors)),
+    **{
+        k: sns.color_palette(
+            custom_blend_colormap(["#FFFFFF", v], list(reversed(_LIGHTNESS_LEVELS))),
+            as_cmap=True,
+        )
+        for k, v in colors._asdict().items()
+    },
+)
+
+
+register_cmaps = get_register_func(cmaps)
+unregister_cmaps = get_unregister_func(cmaps)
