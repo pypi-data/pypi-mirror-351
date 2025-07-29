a26bdd7db5d61e324eed8199bfafc44ed7313800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:25 +0000

    Add package graphfaker/0.1.2/graphfaker-0.1.2.tar.gz

---
+++ packages/graphfaker/graphfaker-0.1.2.tar.gz/graphfaker-0.1.2/graphfaker/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for graphfaker."""
+
+__author__ = """Dennis Irorere"""
+__email__ = "denironyx@gmail.com"
+__version__ = "0.1.0"
+
+from .core import GraphFaker
+from .logger import configure_logging, logger
+
+__all__ = ["GraphFaker", "logger", "configure_logging", "add_file_logging"]
