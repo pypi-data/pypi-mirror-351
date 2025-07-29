2fc5427d16edae491848b6a8d5334c3392a34f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:24 +0000

    Add package peer2peerchat/0.1.9/peer2peerchat-0.1.9.tar.gz

---
+++ packages/peer2peerchat/peer2peerchat-0.1.9.tar.gz/peer2peerchat-0.1.9/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="peer2peerchat",
+    version="0.1.9",
+    author="No Name",
+    description="A simple peer-to-peer chat application with clipboard sharing functionality",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/abdullah134/p2pchat",
+    packages=["peer2peer"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Communications :: Chat",
+        "Topic :: Internet",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "pyperclip"
+    ],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "p2pchat=peer2peer:Peer",
+        ],
+    },
+)
