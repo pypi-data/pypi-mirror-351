00708deb6da0485710bf63c3776575a3f9947073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:35 +0000

    Add package iiot-openapi/0.0.4/iiot_openapi-0.0.4-py3-none-any.whl

---
+++ packages/iiot-openapi/iiot_openapi-0.0.4-py3-none-any.whl/iiot_openapi/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from iiot_openapi.api.model_api import ModelApi
+from iiot_openapi.api.record_api import RecordApi
+from iiot_openapi.api.table_api import TableApi
+from iiot_openapi.api.default_api import DefaultApi
+
