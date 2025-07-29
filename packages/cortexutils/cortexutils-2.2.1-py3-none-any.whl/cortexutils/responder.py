8f42d11350d684bbdd048e92c1790917997a6ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:07:42 +0000

    Add package cortexutils/2.2.1/cortexutils-2.2.1-py3-none-any.whl

---
+++ packages/cortexutils/cortexutils-2.2.1-py3-none-any.whl/cortexutils/responder.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from cortexutils.worker import Worker
+
+
+class Responder(Worker):
+
+    def __init__(self, job_directory=None, secret_phrases=None):
+        Worker.__init__(self, job_directory, secret_phrases)
+
+        # Not breaking compatibility
+        self.artifact = self._input
+
+    def get_data(self):
+        """Wrapper for getting data from input dict.
+
+        :return: Data (observable value) given through Cortex"""
+        return self.get_param("data", None, "Missing data field")
+
+    def report(self, full_report, ensure_ascii=False):
+        """Returns a json dict via stdout.
+
+        :param full_report: Responsder results as dict.
+        :param ensure_ascii: Force ascii output. Default: False"""
+
+        operation_list = []
+        try:
+            operation_list = self.operations(full_report)
+        except Exception:
+            pass  # nosec B110
+        super(Responder, self).report(
+            {"success": True, "full": full_report, "operations": operation_list},
+            ensure_ascii,
+        )
+
+    def run(self):
+        """Overwritten by responders"""
+        pass
