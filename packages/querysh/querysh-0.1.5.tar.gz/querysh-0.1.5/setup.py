b01bc8178a577fac93223f2b306f7b36b974cff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:13 +0000

    Add package querysh/0.1.5/querysh-0.1.5.tar.gz

---
+++ packages/querysh/querysh-0.1.5.tar.gz/querysh-0.1.5/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import re
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("querysh/__init__.py", "r", encoding="utf-8") as f:
+    version = re.search(r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read()).group(1)
+
+setup(
+    name="querysh",
+    version=version,
+    description="A local, offline shell interface powered by MLX-optimized Llama model",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "mlx-lm>=0.0.6",
+        "rich>=13.7.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "querysh=querysh.cli:main",
+        ],
+    },
+) 
