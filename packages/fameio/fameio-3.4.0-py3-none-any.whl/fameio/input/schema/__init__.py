01e40067266d925927db89f5ebc094b1a1fa6add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:58:22 +0000

    Add package fameio/3.4.0/fameio-3.4.0-py3-none-any.whl

---
+++ packages/fameio/fameio-3.4.0-py3-none-any.whl/fameio/input/schema/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2025 German Aerospace Center <fame@dlr.de>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from .agenttype import AgentType  # noqa: F401
+from .attribute import AttributeSpecs, AttributeType  # noqa: F401
+from .java_packages import JavaPackages  # noqa: F401
+from .schema import Schema  # noqa: F401
---
+++ packages/fameio/fameio-3.4.0-py3-none-any.whl/fameio/output/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2025 German Aerospace Center <fame@dlr.de>
+#
+# SPDX-License-Identifier: CC0-1.0
+
+
+class OutputError(Exception):
+    """An error that occurred during extracting an output file"""
