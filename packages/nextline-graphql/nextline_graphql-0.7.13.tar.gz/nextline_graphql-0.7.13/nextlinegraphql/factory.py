69627187f0ea1724591ac46c5f75771e6e129596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:05 +0000

    Add package nextline-graphql/0.7.13/nextline_graphql-0.7.13.tar.gz

---
+++ packages/nextline-graphql/nextline_graphql-0.7.13.tar.gz/nextline_graphql-0.7.13/nextlinegraphql/factory.py
@@ -0,0 +1,69 @@
+import contextlib
+import logging.config
+from collections.abc import AsyncIterator
+from logging import getLogger
+from typing import Any
+
+from rich import print
+from starlette.applications import Starlette
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+
+from .config import load_settings
+from .hook import load_plugins
+
+
+def create_app(external_plugins: bool = True) -> Starlette:
+    '''App factory for Uvicorn
+
+    Use the factory option to run.
+
+    $ uvicorn --factory nextlinegraphql:create_app
+
+    Uvicorn Doc: https://www.uvicorn.org/#application-factories
+    '''
+
+    hook = load_plugins(external=external_plugins)
+    config = load_settings(hook)
+    print('Settings:', config.as_dict())
+
+    configure_logging(config.logging)
+    logger = getLogger(__name__)
+    logger.info('Logging configured')
+
+    hook.hook.configure(settings=config, hook=hook)
+
+    plugin_names = [n for n, p in hook.list_name_plugin() if p]
+    logger.info(f'Pluggy project name: {hook.project_name!r}')
+    logger.info(f'Loaded plugins: {plugin_names}')
+
+    @contextlib.asynccontextmanager
+    async def lifespan(app: Starlette) -> AsyncIterator[None]:
+        context = dict[Any, Any]()
+        await hook.ahook.update_lifespan_context(app=app, hook=hook, context=context)
+        async with hook.awith.lifespan(
+            app=app, hook=hook, context=context
+        ):  # pragma: no branch
+            yield
+
+    middleware = [
+        Middleware(
+            CORSMiddleware,
+            allow_origins=config.cors['allow_origins'],
+            allow_methods=['GET', 'POST', 'OPTIONS'],
+            allow_headers=config.cors['allow_headers'],
+            allow_credentials=config.cors['allow_credentials'],
+        )
+    ]
+
+    app = Starlette(debug=True, lifespan=lifespan, middleware=middleware)
+
+    return app
+
+
+def configure_logging(config: dict) -> None:
+    logging.config.dictConfig(config)
+
+    # https://pypi.org/project/logging_tree/
+    # import logging_tree
+    # logging_tree.printout()
