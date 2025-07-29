3fc86ab17cc17e868ecacb3a623851880ac4c7cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:12 +0000

    Add package quantify-core/0.8.2/quantify_core-0.8.2.tar.gz

---
+++ packages/quantify-core/quantify_core-0.8.2.tar.gz/quantify_core-0.8.2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# pylint: disable=import-outside-toplevel
+from setuptools import setup
+
+
+def get_version_and_cmdclass(pkg_path):
+    """Load version.py module without importing the whole package.
+
+    Template code from miniver
+    """
+    import os
+    from importlib.util import module_from_spec, spec_from_file_location
+
+    spec = spec_from_file_location("version", os.path.join(pkg_path, "_version.py"))
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module.__version__, module.get_cmdclass(pkg_path)
+
+
+version, cmdclass = get_version_and_cmdclass(r"quantify_core")
+
+setup(
+    version=version,
+    cmdclass=cmdclass,
+)
