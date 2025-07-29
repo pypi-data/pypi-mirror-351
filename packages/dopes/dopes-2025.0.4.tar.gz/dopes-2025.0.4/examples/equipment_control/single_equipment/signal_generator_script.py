5b9fa5c7db3a72846b599bde943c8e1dd44f6e53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:17 +0000

    Add package dopes/2025.0.4/dopes-2025.0.4.tar.gz

---
+++ packages/dopes/dopes-2025.0.4.tar.gz/dopes-2025.0.4/examples/equipment_control/single_equipment/signal_generator_script.py
@@ -0,0 +1,49 @@
+# =============================================================================
+# 1. Import classes and modules
+# =============================================================================
+
+# =============================================================================
+# # If local installation of dopes instead of using PyPI (https://pypi.org/project/dopes/)
+# import sys
+# sys.path.insert(1, 'D:/Roisin/Documents/dopes/dopes')
+# =============================================================================
+
+import dopes.equipment_control.equipment as eq
+import dopes.equipment_control.signal_generator as signal_generator
+import time
+
+# =============================================================================
+# 2. List  available connections (chopes use pyvisa package for communicate with most equipments)
+# =============================================================================
+rm=eq.resource_manager()
+list_connections= eq.available_connections()
+print("Available connections: %s"%str(list_connections))
+
+# =============================================================================
+# 3. Connection to the equipment
+# =============================================================================
+mygenerator=signal_generator.signal_generator("USB0::0x0699::0x0349::C012340::INSTR",timeout=5e3)
+
+# =============================================================================
+# 4. Measurement parameters
+# =============================================================================
+waveform="sin"
+freq=1e3 
+amplitude=1 
+offset=0.5
+
+# =============================================================================
+# 5. Initialization of the equipment
+# =============================================================================
+mygenerator.initialize(waveform=waveform, freq=freq, amp=amplitude, offset=offset)
+
+# =============================================================================
+# 6. Measurement script
+# =============================================================================
+mygenerator.set_output("ON")
+time.sleep(10)
+
+# =============================================================================
+# 7. Close connection
+# =============================================================================
+mygenerator.close_connection()
