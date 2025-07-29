05d4060812d154050c85d77e2eac25e2f48c7373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:43 +0000

    Add package NeditGD/0.3.0/neditgd-0.3.0.tar.gz

---
+++ packages/NeditGD/neditgd-0.3.0.tar.gz/neditgd-0.3.0/NeditGD/__init__.py
@@ -0,0 +1,7 @@
+from NeditGD.object_gd import Object, get_pos
+from NeditGD.editor_gd import Editor
+from NeditGD.Dictionaries.PropertyHSV import HSV
+
+
+__version__ = "0.3.0"
+__author__ = 'Nemo2510 (Boris Filin)'
