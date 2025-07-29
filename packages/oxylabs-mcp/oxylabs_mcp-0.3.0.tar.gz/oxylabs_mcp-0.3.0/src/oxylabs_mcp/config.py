367eade085a6941a62404393a7a1f3cbdf088705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:07 +0000

    Add package oxylabs-mcp/0.3.0/oxylabs_mcp-0.3.0.tar.gz

---
+++ packages/oxylabs-mcp/oxylabs_mcp-0.3.0.tar.gz/oxylabs_mcp-0.3.0/src/oxylabs_mcp/config.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    """Project settings."""
+
+    OXYLABS_SCRAPER_URL: str = "https://realtime.oxylabs.io/v1/queries"
+    OXYLABS_REQUEST_TIMEOUT_S: int = 100
+
+
+settings = Settings()
