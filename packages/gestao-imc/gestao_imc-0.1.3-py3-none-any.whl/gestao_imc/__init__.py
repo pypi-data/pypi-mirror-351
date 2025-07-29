1e0be917b0149948910d0d1e6927787ab77424a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:39 +0000

    Add package gestao-imc/0.1.3/gestao_imc-0.1.3-py3-none-any.whl

---
+++ packages/gestao-imc/gestao_imc-0.1.3-py3-none-any.whl/gestao_imc/__init__.py
@@ -0,0 +1,13 @@
+from .funcoes import (
+    add_pessoa,
+    calcular_imc,
+    add_dados,
+    listar_pessoas,
+    listar_por_imc_decrescente,
+    listar_pessoas_com_imc_critico,
+    tabela_altura,
+    tabela_peso,
+    historico_imc,
+    historico_imc_atemporal,
+    testar_integridade,
+)
