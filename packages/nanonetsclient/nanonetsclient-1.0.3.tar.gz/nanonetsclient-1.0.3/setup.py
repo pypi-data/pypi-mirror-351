6cd6a07a7d3931bbebe09cf5df80fffae09d420a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:21 +0000

    Add package nanonetsclient/1.0.3/nanonetsclient-1.0.3.tar.gz

---
+++ packages/nanonetsclient/nanonetsclient-1.0.3.tar.gz/nanonetsclient-1.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nanonetsclient",
+    version="1.0.3",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.0",
+        "python-dotenv>=0.19.0",
+    ],
+    python_requires=">=3.7",
+    author="Nanonets",
+    author_email="support@nanonets.com",
+    description="Nanonets API SDK for Python",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://nanonets.com/?utm_source=python-sdk&utm_medium=sdk&utm_campaign=sdk-install&ph_source=python-sdk&ph_medium=sdk&ph_campaign=sdk-install",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+) 
