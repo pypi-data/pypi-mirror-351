3b40895e1c1e4e610e053ef17567bee89fcabd32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:38 +0000

    Add package impulcifer-py313/1.3.3/impulcifer_py313-1.3.3-py3-none-any.whl

---
+++ packages/impulcifer-py313/impulcifer_py313-1.3.3-py3-none-any.whl/impulcifer_cli.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+Impulcifer 명령줄 진입점
+"""
+
+import sys
+from impulcifer import main, create_cli
+
+def entry_point():
+    """명령줄에서 실행 시 진입점 함수"""
+    args = create_cli()
+    main(**args)
+
+if __name__ == "__main__":
+    entry_point() 
