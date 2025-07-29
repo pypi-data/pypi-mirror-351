295256291d4d84aedd233d8585a66313131d9fbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:10 +0000

    Add package ok-script/0.0.560/ok_script-0.0.560-cp312-cp312-win_amd64.whl

---
+++ packages/ok-script/ok_script-0.0.560-cp312-cp312-win_amd64.whl/ok/__init__.py
@@ -0,0 +1,9 @@
+#
+# import importlib
+#
+# submodule = importlib.import_module('ok.ok')
+# # Import all non-private names from __init__.py
+# for name, obj in submodule.__dict__.items():
+#     if not name.startswith("_"):
+#         globals()[name] = obj
+#         # print(f"{name} loaded")
