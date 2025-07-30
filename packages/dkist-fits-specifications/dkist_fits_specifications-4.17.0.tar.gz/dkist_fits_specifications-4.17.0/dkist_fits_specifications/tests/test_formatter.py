2de3021bf0bae0c61f081fb2c0f57a9fc755731d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:08 +0000

    Add package dkist-fits-specifications/4.17.0/dkist_fits_specifications-4.17.0.tar.gz

---
+++ packages/dkist-fits-specifications/dkist_fits_specifications-4.17.0.tar.gz/dkist_fits_specifications-4.17.0/dkist_fits_specifications/tests/test_formatter.py
@@ -0,0 +1,57 @@
+from random import randint
+
+import pytest
+from astropy.io import fits
+
+from dkist_fits_specifications.spec214 import load_processed_spec214
+from dkist_fits_specifications.utils.formatter import reformat_spec214_header
+
+
+@pytest.fixture
+def header_with_all_sections() -> fits.Header:
+    all_214_schemas = load_processed_spec214()
+    header_dict = dict()
+    for section in all_214_schemas.values():
+        # Value doesn't matter at all, so just make it a random int
+        header_dict.update({k: randint(1, 10) for k in section.keys()})
+
+    # Manual intervention needed on these keys so header processors will work correctly
+    del header_dict["END"]
+    header_dict["DAAXES"] = 3
+    header_dict["DEAXES"] = 2
+    header_dict["DNAXIS"] = 5
+    header_dict["NPROPOS"] = 10
+    header_dict["NEXPERS"] = 11
+    header_dict["NAXIS"] = 3
+    header_dict["ZVAL1"] = 1
+    header_dict["TFIELDS"] = 2
+    header_dict["NSPECLNS"] = 13
+    header = fits.Header(header_dict)
+
+    header.add_history("This is a test history entry.")
+
+    # Add a comment. Choosing "BUNIT" because it doesn't otherwise have a comment
+    header.comments["BUNIT"] = "Wibble"
+    # And overwrite a comment for a key that would get a default comment
+    header.comments["TELTRACK"] = "NOT the default comment"
+
+    return header
+
+
+def test_formatter(header_with_all_sections):
+    """
+    Given: A 214 header with all possible schema sections
+    When: Formatting the header
+    Then: The dang thing runs and comments are preserved
+    """
+    output_header = reformat_spec214_header(header_with_all_sections)
+    assert isinstance(output_header, fits.Header)
+    assert output_header.comments["BUNIT"] == "Wibble"
+    assert output_header.comments["TELTRACK"] == "NOT the default comment"
+
+    # Make sure a default comment was set when no comments existed in input header
+    assert output_header.comments["TAZIMUTH"] != ""
+
+    # Make sure multiple history entries are preserved
+    assert "This is a test history entry." in str(output_header["HISTORY"])
+    assert str(output_header["HISTORY"]) != "This is a test history entry."
