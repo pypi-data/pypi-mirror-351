7356404b66e677e182c9f3233a0c313112319f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:11 +0000

    Add package dro/0.3.2/dro-0.3.2.tar.gz

---
+++ packages/dro/dro-0.3.2.tar.gz/dro-0.3.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup,find_packages
+import os 
+
+here = os.path.abspath(os.path.dirname(__file__))
+md_path = os.path.join(here, 'dro/README.md')
+
+setup(
+    name='dro',
+    version='0.3.2',    
+    license='MIT License',
+    description='A package of distributionally robust optimization (DRO) methods. Implemented via cvxpy and PyTorch',
+    long_description=open(md_path, encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/namkoong-lab/dro',
+    author='DRO developers.',
+    author_email='liujiashuo77@gmail.com, tw2837@columbia.edu',
+    packages=find_packages(),
+    install_requires=['pandas',
+                      'numpy>=1.20',                     
+                      'scikit-learn',
+                      'torch', 
+                      'scipy',
+                      'cvxpy', 
+                      'torchvision', 
+                      'ucimlrepo', 
+                      'matplotlib',
+                      'torchattacks',
+                      'mosek>=10.2.17',
+                      'xgboost',
+                      'lightgbm',
+
+                      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+        'Operating System :: POSIX :: Linux',
+    ],
+    python_requires=">=3.7",
+)
