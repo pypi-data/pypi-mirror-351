1cba352d54c234998053b44ae27efdadf498657f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:25 +0000

    Add package abs-integration-core/0.1.5/abs_integration_core-0.1.5.tar.gz

---
+++ packages/abs-integration-core/abs_integration_core-0.1.5.tar.gz/abs_integration_core-0.1.5/abs_integration_core/models/integration_model.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, String, DateTime, Text
+
+from abs_repository_core.models import BaseModel
+
+
+class Integration(BaseModel):
+    """Integration model"""
+    __tablename__ = "gov_integrations"
+
+    provider_name = Column(String(255), nullable=False)
+    access_token = Column(Text, nullable=False)
+    refresh_token = Column(Text, nullable=False)
+    expires_at = Column(DateTime, nullable=False)
