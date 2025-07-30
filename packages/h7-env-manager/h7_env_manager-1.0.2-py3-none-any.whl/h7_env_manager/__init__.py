09546171d11b6a2cbd6bae16ca10055ef34f5473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:03 +0000

    Add package h7-env-manager/1.0.2/h7_env_manager-1.0.2-py3-none-any.whl

---
+++ packages/h7-env-manager/h7_env_manager-1.0.2-py3-none-any.whl/h7_env_manager/__init__.py
@@ -0,0 +1,10 @@
+"""Documentation for the h7_env_manager package.
+
+This package provides utilities for managing environment variables in a project.
+"""
+
+# Package version
+__version__ = "1.0.2"
+
+# Import main class to make it available at package level
+from .env_manager import EnvManager as EnvManager
