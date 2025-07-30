dc50cb3ddd7c6886db886a3e9dda5f5a06b6c336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:10 +0000

    Add package lynkr/0.1.10/lynkr-0.1.10.tar.gz

---
+++ packages/lynkr/lynkr-0.1.10.tar.gz/lynkr-0.1.10/src/lynkr/__init__.py
@@ -0,0 +1,11 @@
+"""
+Lynkr SDK - Official Python client library for the Lynkr Service.
+
+This package provides a convenient interface to the Lynkr Service API.
+"""
+
+__version__ = "0.1.0"
+
+from .client import LynkrClient
+
+__all__ = ["LynkrClient"]
