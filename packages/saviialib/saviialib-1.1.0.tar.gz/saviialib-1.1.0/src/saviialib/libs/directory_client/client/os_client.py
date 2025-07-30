796e21df233827083ed0965f9c81bbbdda66ab91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:52 +0000

    Add package saviialib/1.1.0/saviialib-1.1.0.tar.gz

---
+++ packages/saviialib/saviialib-1.1.0.tar.gz/saviialib-1.1.0/src/saviialib/libs/directory_client/client/os_client.py
@@ -0,0 +1,23 @@
+from saviialib.libs.directory_client.directory_client_contract import (
+    DirectoryClientContract,
+)
+import os
+import asyncio
+
+
+class OsClient(DirectoryClientContract):
+    @staticmethod
+    def join_paths(*paths: str) -> str:
+        return os.path.join(*paths)
+
+    @staticmethod
+    async def path_exists(path: str) -> bool:
+        return await asyncio.to_thread(os.path.exists, path)
+
+    @staticmethod
+    async def listdir(path: str) -> list:
+        return await asyncio.to_thread(os.listdir, path)
+
+    @staticmethod
+    async def isdir(path: str) -> list:
+        return await asyncio.to_thread(os.path.isdir, path)
