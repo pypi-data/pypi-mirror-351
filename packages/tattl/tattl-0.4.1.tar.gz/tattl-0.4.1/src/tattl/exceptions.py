cb2cb9a65680746480d7cdb7d33972a4ff5cc5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:51 +0000

    Add package tattl/0.4.1/tattl-0.4.1.tar.gz

---
+++ packages/tattl/tattl-0.4.1.tar.gz/tattl-0.4.1/src/tattl/exceptions.py
@@ -0,0 +1,16 @@
+class TATTLException(Exception):
+    """Raised when the data given does not conform to the ``dataclass``.
+
+    This is the base exception for all TATTL exceptions."""
+
+
+class ValidationException(TATTLException):
+    """Indicates that the type of a field in the data differs from the type of the ``dataclass``
+    field with the same name.
+    """
+
+    pass
+
+
+class MissingFieldException(TATTLException):
+    """Indicates that the data is missing a required field in the ``dataclass``."""
---
+++ packages/tattl/tattl-0.4.1.tar.gz/tattl-0.4.1/tests/test_defaults_override.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass, field
+
+
+def my_dict_factory():
+    return {"spam": 1.23, "ham": 4.56, "eggs": 7.89}
+
+
+def my_list_factory():
+    return [1, 1, 2, 3, 5, 8]
+
+
+@dataclass
+class Structure:
+    foo: str = "AAA"
+    bar: int = 123
+    baz: bool = field(default=True)
+    qux: list[str] = field(default_factory=list)
+    cor: dict[str, float] = field(default_factory=my_dict_factory)
+    gra: list[int] = field(default_factory=my_list_factory)
+
+
+EXAMPLE_TOML = """
+foo = "BBB"
+bar = 234
+baz = false
+qux = [ "Hello", "World" ]
+cor = { snork = 2.34, blarg = 5.67, wibble = 8.90 }
+gra = [ 1, 121, 12321, 1234321 ]
+"""
+
+
+def test_example():
+    import tattl
+    import tomllib
+
+    data = tattl.unpack_dict(tomllib.loads(EXAMPLE_TOML), Structure)
+    print(data)
+    assert data == Structure(
+        foo="BBB",
+        bar=234,
+        baz=False,
+        qux=["Hello", "World"],
+        cor={"snork": 2.34, "blarg": 5.67, "wibble": 8.90},
+        gra=[1, 121, 12321, 1234321],
+    )
