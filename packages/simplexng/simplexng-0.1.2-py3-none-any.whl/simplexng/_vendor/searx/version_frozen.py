14fcd6a9d1b12bbeee3571f6ed9c24597f0c6cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:24 +0000

    Add package simplexng/0.1.2/simplexng-0.1.2-py3-none-any.whl

---
+++ packages/simplexng/simplexng-0.1.2-py3-none-any.whl/simplexng/_vendor/searx/version_frozen.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# pylint: disable=missing-module-docstring
+# this file is generated automatically by searx/version.py
+
+VERSION_STRING = "2025.5.28+2288f07"
+VERSION_TAG = "2025.5.28+2288f07"
+DOCKER_TAG = "2025.5.28-2288f07"
+GIT_URL = "https://github.com/jlevy/simplexng"
+GIT_BRANCH = "v0.1.2"
