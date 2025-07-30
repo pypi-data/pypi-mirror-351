7e662d81f006d9a9da94de133a430b00c8b139c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:04 +0000

    Add package digitalhub-runtime-container/0.11.0/digitalhub_runtime_container-0.11.0.tar.gz

---
+++ packages/digitalhub-runtime-container/digitalhub_runtime_container-0.11.0.tar.gz/digitalhub_runtime_container-0.11.0/digitalhub_runtime_container/entities/function/container/status.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from digitalhub.entities.function._base.status import FunctionStatus
+
+
+class FunctionStatusContainer(FunctionStatus):
+    """
+    FunctionStatusContainer status.
+    """
