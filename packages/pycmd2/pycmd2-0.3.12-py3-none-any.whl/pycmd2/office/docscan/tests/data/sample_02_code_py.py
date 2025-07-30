636104afdcc31939588640ba8fc2b23c487fa920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:40 +0000

    Add package pycmd2/0.3.12/pycmd2-0.3.12-py3-none-any.whl

---
+++ packages/pycmd2/pycmd2-0.3.12-py3-none-any.whl/pycmd2/office/docscan/tests/data/sample_02_code_py.py
@@ -0,0 +1,17 @@
+from PySide2.QtCore import QStringListModel
+from PySide2.QtWidgets import QApplication, QComboBox, QListView, QSplitter
+
+app = QApplication()
+cities = "北京 上海 广州 深圳 武汉 成都 西安 长沙".split()
+model = QStringListModel([f"城市({i}):{e}" for i, e in enumerate(cities)])
+
+splitter = QSplitter()
+view1 = QListView(splitter)
+view1.setModel(model)
+
+# QComboBox 也可以作为 ListView 进行显示
+view2 = QComboBox(splitter)
+view2.setModel(model)
+
+splitter.show()
+app.exec_()
