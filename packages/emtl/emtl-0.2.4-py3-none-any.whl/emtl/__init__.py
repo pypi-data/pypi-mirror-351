83f41f95d692cf0f30fb96bba71b4e08f32430d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:46:20 +0000

    Add package emtl/0.2.4/emtl-0.2.4-py3-none-any.whl

---
+++ packages/emtl/emtl-0.2.4-py3-none-any.whl/emtl/__init__.py
@@ -0,0 +1,23 @@
+__version__ = "0.2.4"
+
+from .core import cancel_order
+from .core import create_order
+from .core import login
+from .core import query_asset_and_position
+from .core import query_funds_flow
+from .core import query_history_orders
+from .core import query_history_trades
+from .core import query_orders
+from .core import query_trades
+
+__all__ = [
+    "login",
+    "query_asset_and_position",
+    "query_orders",
+    "query_history_orders",
+    "query_trades",
+    "query_history_trades",
+    "query_funds_flow",
+    "create_order",
+    "cancel_order",
+]
