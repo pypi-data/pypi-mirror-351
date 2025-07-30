c9eb866ad76981f20e156b38b075836d9387bdcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:48 +0000

    Add package geohashr/1.5.0/geohashr-1.5.0-cp313-cp313-win_amd64.whl

---
+++ packages/geohashr/geohashr-1.5.0-cp313-cp313-win_amd64.whl/geohashr/__init__.py
@@ -0,0 +1,12 @@
+from ._geohashr import (
+    DecodeError,
+    EncodeError,
+    ParamError,
+    __version__,
+    bbox,
+    decode,
+    decode_exact,
+    encode,
+    neighbor,
+    neighbors,
+)
