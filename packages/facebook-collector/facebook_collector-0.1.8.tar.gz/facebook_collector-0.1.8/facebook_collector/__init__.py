392aad9fa37a38c1c1f4ab1e15491f8c572bd9aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:24 +0000

    Add package facebook-collector/0.1.8/facebook_collector-0.1.8.tar.gz

---
+++ packages/facebook-collector/facebook_collector-0.1.8.tar.gz/facebook_collector-0.1.8/facebook_collector/__init__.py
@@ -0,0 +1,15 @@
+from .hashtag import FacebookHashtagCollector
+from .keywords import FacebookKeywordCollector
+from .post_comment import FacebookPostCommentCollector
+from .brand import FacebookBrandCollector
+from .graphql_handler import FacebookGraphQLCollector
+
+__all__ = [
+    'FacebookGraphQLCollector',
+    'FacebookHashtagCollector',
+    'FacebookKeywordCollector',
+    'FacebookBrandCollector',
+    'FacebookPostCommentCollector'
+]
+
+__version__ = "0.1.8"
