00ba9e8c5df70d2553eb03db5aaaad61430f7c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:16 +0000

    Add package fabricatio-anki/0.1.0.dev11/fabricatio_anki-0.1.0.dev11-cp312-cp312-win_amd64.whl

---
+++ packages/fabricatio-anki/fabricatio_anki-0.1.0.dev11-cp312-cp312-win_amd64.whl/fabricatio_anki/capabilities/create_deck.py
@@ -0,0 +1,10 @@
+"""Provide capabilities for creating a deck of cards."""
+
+from fabricatio_core.capabilities.propose import Propose
+
+
+class CreateDeck(Propose):
+    """Create a deck of cards."""
+
+    ...
+
