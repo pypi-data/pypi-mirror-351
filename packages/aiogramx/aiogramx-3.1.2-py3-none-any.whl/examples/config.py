b828b97b565f33c8b3fe5c82b06d7b7e7a416a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:57 +0000

    Add package aiogramx/3.1.2/aiogramx-3.1.2-py3-none-any.whl

---
+++ packages/aiogramx/aiogramx-3.1.2-py3-none-any.whl/examples/config.py
@@ -0,0 +1,4 @@
+POSTGRES_URL = "postgres://jzr:12345678@localhost:5432/jzrxbot"
+# demo bot
+# BOT_TOKEN = "368888696:AAGw1J2OmMOx9FwRHwaww8GvPYrheURemCQ"
+BOT_TOKEN = "688731229:AAE3xb-1w9wR6fCoIdG5zU2jUidGXFigfAk"
