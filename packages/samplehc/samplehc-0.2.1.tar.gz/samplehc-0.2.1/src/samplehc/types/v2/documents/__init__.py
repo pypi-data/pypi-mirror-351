9acef4f73e4f8269219132bf01aa85c35fa5d032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:23 +0000

    Add package samplehc/0.2.1/samplehc-0.2.1.tar.gz

---
+++ packages/samplehc/samplehc-0.2.1.tar.gz/samplehc-0.2.1/src/samplehc/types/v2/documents/__init__.py
@@ -0,0 +1,8 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .legacy_reason_params import LegacyReasonParams as LegacyReasonParams
+from .legacy_extract_params import LegacyExtractParams as LegacyExtractParams
+from .legacy_reason_response import LegacyReasonResponse as LegacyReasonResponse
+from .legacy_extract_response import LegacyExtractResponse as LegacyExtractResponse
