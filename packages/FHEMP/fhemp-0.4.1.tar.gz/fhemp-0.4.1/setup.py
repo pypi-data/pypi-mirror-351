59618cd5a8007cb77c8484fa29b1ff203e546922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:11:44 +0000

    Add package FHEMP/0.4.1/fhemp-0.4.1.tar.gz

---
+++ packages/FHEMP/fhemp-0.4.1.tar.gz/fhemp-0.4.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FHEMP',
+    version='0.4.1',
+    description='Гомоморфное шифрование на основе матричных полиномов',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='EvZait',
+    # author_email='evzait03@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+    'numpy',
+    ],
+    python_requires='>=3.8',
+)
+
+entry_points={
+    'console_scripts': [
+        'FHEMP=FHEMP.cli:main',
+    ]
+},
+
