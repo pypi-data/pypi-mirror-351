ad0431d7cb776269c1ecf6d7b5cd9458eaff5cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:24:33 +0000

    Add package ws-bom-robot-app/0.0.58/ws_bom_robot_app-0.0.58.tar.gz

---
+++ packages/ws-bom-robot-app/ws_bom_robot_app-0.0.58.tar.gz/ws_bom_robot_app-0.0.58/ws_bom_robot_app/llm/tools/models/main.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+
+class NoopInput(BaseModel):
+  pass
+class DocumentRetrieverInput(BaseModel):
+  query: str = Field(description="The search query string")
+class ImageGeneratorInput(BaseModel):
+  query: str = Field(description="description of the image to generate.")
+  language: str = Field(description="Language of the query. Default is 'it'", default="it")
+class LlmChainInput(BaseModel):
+  input: str = Field(description="Input to the LLM chain")
