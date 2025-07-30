f945af29d9b36653475ab4cfa21bb193b0e27ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:52 +0000

    Add package uznltk/0.0.10/uznltk-0.0.10.tar.gz

---
+++ packages/uznltk/uznltk-0.0.10.tar.gz/uznltk-0.0.10/uznltk/_lemmatizer.py
@@ -0,0 +1,9 @@
+from UzMorphAnalyser import UzMorphAnalyser
+
+# Avval obyekt yaratish kerak
+analyser = UzMorphAnalyser()
+def lemmatize(word):
+    # So‘zni lemmatizatsiya qilish
+    a=analyser.lemmatize(word)
+    return a
+
