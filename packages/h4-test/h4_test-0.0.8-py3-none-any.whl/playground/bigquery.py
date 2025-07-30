c453878484f6fc227855e93b58bb0ecc9e5873d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:12 +0000

    Add package h4-test/0.0.8/h4_test-0.0.8-py3-none-any.whl

---
+++ packages/h4-test/h4_test-0.0.8-py3-none-any.whl/playground/bigquery.py
@@ -0,0 +1,36 @@
+from google.cloud import bigquery
+
+_bigquery_client = None
+
+def _initialize_bigquery_client():
+    global _bigquery_client
+    
+    if _bigquery_client is None:
+        _bigquery_client = bigquery.Client()
+    return _bigquery_client
+
+def get_from_bigquery(query):
+    client = _initialize_bigquery_client()
+    
+    try:
+        query_job = client.query(query)
+        return query_job.result().to_dataframe(), None
+    except Exception as e:
+        return pd.DataFrame(), e
+
+def load_from_dataframe(df, table, write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE):
+    if df.empty:
+        return False, 'df.empty'
+    
+    client = _initialize_bigquery_client()
+    job_config = bigquery.LoadJobConfig(
+        write_disposition=write_disposition,
+        create_disposition=bigquery.CreateDisposition.CREATE_IF_NEEDED
+    )
+
+    try:
+        job = client.load_table_from_dataframe(df, table, job_config=job_config)
+        job.result()
+        return True, f'uploaded {len(df)} rows to {table_id}'
+    except Exception as e:
+        return False, e
