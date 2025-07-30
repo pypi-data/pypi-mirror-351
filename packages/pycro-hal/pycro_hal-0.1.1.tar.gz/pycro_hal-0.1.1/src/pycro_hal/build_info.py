c5033a792d43bcacccd471a3c33dfad01c3e2287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:08:15 +0000

    Add package pycro-hal/0.1.1/pycro_hal-0.1.1.tar.gz

---
+++ packages/pycro-hal/pycro_hal-0.1.1.tar.gz/pycro_hal-0.1.1/src/pycro_hal/build_info.py
@@ -0,0 +1,10 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.12.3'
+    os_name = 'posix:ubuntu'
+    version = '0.1.1'
+    git_sha = 'b8e57ea2867775aca6a946cae9525dd16b4d1de4'
+    git_branch = 'master'
+    git_uncommitted = [
+    ]
+    git_unpushed = [
+    ]
