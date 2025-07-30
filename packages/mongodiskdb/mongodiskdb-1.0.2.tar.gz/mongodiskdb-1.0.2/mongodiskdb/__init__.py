90a809561565e7fe636615dd0ef75e7aacdeae49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:24:42 +0000

    Add package mongodiskdb/1.0.2/mongodiskdb-1.0.2.tar.gz

---
+++ packages/mongodiskdb/mongodiskdb-1.0.2.tar.gz/mongodiskdb-1.0.2/mongodiskdb/__init__.py
@@ -0,0 +1,8 @@
+"""MongoDiskDB - A MongoDB-style disk-based database implementation"""
+
+from .db import DiskDB
+from .collection import Collection
+from .exceptions import NoCollectionError
+
+__version__ = '0.1.0'
+__all__ = ['DiskDB', 'Collection', 'NoCollectionError']
