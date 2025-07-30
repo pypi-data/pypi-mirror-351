debd6dba25543f5c63223e01cd5ba701d5124f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:59 +0000

    Add package quicksand/2.1.1/quicksand-2.1.1.tar.gz

---
+++ packages/quicksand/quicksand-2.1.1.tar.gz/quicksand-2.1.1/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="quicksand",
+    version="2.1.1",
+    author="tylabs.com",
+    author_email="nospam@tylabs.com",
+    description="QuickSand is a module to scan streams inside documents with Yara",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tylabs/quicksand",
+    download_url="https://github.com/tylabs/quicksand/archive/refs/tags/2.1.1.tar.gz",
+    include_package_data=True,
+    keywords = ['document', 'malware', 'forensics', 'yara', 'parser'],
+    project_urls={
+        "Bug Tracker": "https://github.com/tylabs/quicksand/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=['quicksand'],
+    scripts=['bin/quicksand'],
+    python_requires=">=3.6",
+    install_requires=[
+        'pdfreader',
+        'oletools',
+        'cryptography',
+        'zipfile38',
+        'msoffcrypto-tool',
+        'olefile',
+        'yara-python>=4.3.0',
+    ],
+)
