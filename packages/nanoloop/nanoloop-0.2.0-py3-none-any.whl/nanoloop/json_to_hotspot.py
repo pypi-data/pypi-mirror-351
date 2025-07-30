4acde8c7e511d8d5bfe4e2e64cb4d23bcd6c4df1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:57 +0000

    Add package nanoloop/0.2.0/nanoloop-0.2.0-py3-none-any.whl

---
+++ packages/nanoloop/nanoloop-0.2.0-py3-none-any.whl/nanoloop/json_to_hotspot.py
@@ -0,0 +1,60 @@
+# Parse json looking for "mutation hotspot" in each read, output a BED-like file
+
+import json
+import gzip
+import multiprocessing
+from multiprocessing import Process, Queue
+import os
+import tempfile
+from itertools import islice
+from .utils import process_json_to_hotspot, chunk_json_file
+
+
+def writer_process(queue, output_path):
+  """Dedicated process to write filtered results to gzipped NDJSON file"""
+  with gzip.open(output_path, "wt") as gz:
+    while True:
+      item = queue.get()
+      if item is None:  # Sentinel to end
+        break
+      if isinstance(item, Exception):
+        raise item
+      # Write filtered records
+      for record in item:
+        # convert json to list 
+        record = list(record)
+        try:
+          record = record[0]
+        except:
+          continue
+        line = '\t'.join(map(str, [record[2], record[0], record[1], record[3]]))
+        gz.write(line + "\n")
+
+def run_json_to_hotspot(args):
+  """
+  Parse NDJSON records looking for "mutation hotspot" in each read, output a BED-like file
+  """
+  os.makedirs(os.path.dirname(os.path.abspath(args.output)), exist_ok = True)
+  
+  result_queue = Queue()
+  writer = Process(target = writer_process, args = (result_queue, args.output))
+  writer.start()
+  
+  # Process input file in chunks
+  active_processes = []
+  
+  with gzip.open(args.json, 'rt') as f:
+    for chunk in chunk_json_file(f):
+      while len(active_processes) >= args.ncpus:
+        p = active_processes.pop(0)
+        p.join()
+      
+      p = Process(target = process_json_to_hotspot, args = (chunk, args.mutation_type, args.window_size, args.window_step, args.mutation_frac_cutoff, result_queue))
+      active_processes.append(p)
+      p.start()
+    
+    for p in active_processes:
+      p.join()
+  
+  result_queue.put(None)
+  writer.join()
