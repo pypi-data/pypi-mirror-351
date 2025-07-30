89e964534e38ae266aab526fe2bbba101aa81cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:42 +0000

    Add package distinction/0.0.1.7/distinction-0.0.1.7.tar.gz

---
+++ packages/distinction/distinction-0.0.1.7.tar.gz/distinction-0.0.1.7/src/distinction/__init__.py
@@ -0,0 +1,9 @@
+'distinction: a simple, explainable and tuneable binary text classifier using sentence embeddings and distance calculations'
+
+__version__ = '0.0.1'
+__version_info__ = 'distinction v' + __version__
+__author__ = 'Erik Broman <mikroberna@gmail.com>'
+__status__ = 'Alpha'
+__copyright__ = 'Copyright (c) 2025- ' + __author__
+
+from .classifier import *
