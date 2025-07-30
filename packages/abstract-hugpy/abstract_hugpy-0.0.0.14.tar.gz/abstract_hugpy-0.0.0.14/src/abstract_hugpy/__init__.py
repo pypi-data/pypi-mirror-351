34b43b9147ce6a9a896c648159b3b5eaa2e6b73d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:41 +0000

    Add package abstract-hugpy/0.0.0.14/abstract_hugpy-0.0.0.14.tar.gz

---
+++ packages/abstract-hugpy/abstract_hugpy-0.0.0.14.tar.gz/abstract_hugpy-0.0.0.14/src/abstract_hugpy/__init__.py
@@ -0,0 +1,10 @@
+from hugging_face_models.config import *
+from .routes import *
+def import_video_utils():
+    from .video_utils import *
+def import_abstract_hugpy():
+    from .abstract_hugpy import *
+
+
+
+
