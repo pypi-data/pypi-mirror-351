c89062ca7a04a836b89fdce097ca700406e1a6b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:49 +0000

    Add package dynamic-config-qq/1.2.0/dynamic_config_qq-1.2.0-py2-none-any.whl

---
+++ packages/dynamic-config-qq/dynamic_config_qq-1.2.0-py2-none-any.whl/dynamic_config_qq/exceptions.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+class ConfigError(Exception):
+    """配置操作基础异常"""
+
+
+class ConfigExistsError(ConfigError):
+    """配置已存在异常"""
+
+
+class ConfigNotFoundError(ConfigError):
+    """配置不存在异常"""
+
+class ConfigValueNotStringError(ConfigError):
+    """配置值不支持的类型异常"""
