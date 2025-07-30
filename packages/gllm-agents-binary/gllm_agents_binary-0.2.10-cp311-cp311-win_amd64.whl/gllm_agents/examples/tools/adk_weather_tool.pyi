b546a0de4c2bbd26dbad04bba573d19399b911ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:41 +0000

    Add package gllm-agents-binary/0.2.10/gllm_agents_binary-0.2.10-cp311-cp311-win_amd64.whl

---
+++ packages/gllm-agents-binary/gllm_agents_binary-0.2.10-cp311-cp311-win_amd64.whl/gllm_agents/examples/tools/adk_weather_tool.pyi
@@ -0,0 +1,10 @@
+def weather_tool(location: str) -> dict:
+    """Execute the weather lookup.
+
+    Args:
+        location: The location to get weather for
+        tool_context: The tool context
+
+    Returns:
+        A dictionary with the weather information
+    """
