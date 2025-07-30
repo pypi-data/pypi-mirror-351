d4179021a82717df88255c5e453d105f14a3518a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:52 +0000

    Add package cytaxii2/1.0.1/cytaxii2-1.0.1.tar.gz

---
+++ packages/cytaxii2/cytaxii2-1.0.1.tar.gz/cytaxii2-1.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='cytaxii2',
+    version='1.0.1',
+    description='This python package is created by Cyware Labs, as a TAXII 2 Client. This can be used easily by developers to connect to a TAXII 2 Server and perform actions',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cyware-labs/cytaxii2",
+    author='Cyware Labs',
+    author_email='contact@cyware.com',
+    license='MIT License',
+    packages=['cytaxii2'],
+    install_requires=['requests'],
+
+    classifiers=[
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
