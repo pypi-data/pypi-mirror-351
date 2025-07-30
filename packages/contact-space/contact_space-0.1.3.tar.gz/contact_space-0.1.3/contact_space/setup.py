e246b8e0bd6653a000b7ed4ce5aa696c5caebf4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:14 +0000

    Add package contact-space/0.1.3/contact_space-0.1.3.tar.gz

---
+++ packages/contact-space/contact_space-0.1.3.tar.gz/contact_space-0.1.3/contact_space/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+   name="contact_space",
+   version="0.1.03",
+   description="SDK to interact with the ContactSpace REST API",
+   author="George Burns",
+   author_email="mrgeorgegburns@gmail.com",
+   long_description="README.md",
+   long_description_content_type="text/markdown",
+   packages=find_packages(),
+   python_requires=">=3.7"
+)
