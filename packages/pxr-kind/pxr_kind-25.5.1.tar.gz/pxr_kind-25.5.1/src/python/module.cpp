732a57b23244430b6e7b621cacef99c27d1b99a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:32 +0000

    Add package pxr-kind/25.5.1/pxr_kind-25.5.1.tar.gz

---
+++ packages/pxr-kind/pxr_kind-25.5.1.tar.gz/pxr_kind-25.5.1/src/python/module.cpp
@@ -0,0 +1,16 @@
+// Copyright 2016 Pixar
+//
+// Licensed under the terms set forth in the LICENSE.txt file available at
+// https://openusd.org/license.
+//
+// Modified by Jeremy Retailleau.
+
+#include <pxr/tf/pyModule.h>
+
+using namespace pxr;
+
+TF_WRAP_MODULE
+{
+    TF_WRAP( Registry );
+    TF_WRAP( Tokens );
+}
