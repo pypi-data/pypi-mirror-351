1844b0f0c0b72e115237826dbd2ced2105de5037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:46 +0000

    Add package chromadb/1.0.12/chromadb-1.0.12.tar.gz

---
+++ packages/chromadb/chromadb-1.0.12.tar.gz/chromadb-1.0.12/rust/index/src/lib.rs
@@ -0,0 +1,30 @@
+pub mod config;
+pub mod fulltext;
+mod hnsw;
+pub mod hnsw_provider;
+pub mod metadata;
+pub mod spann;
+mod types;
+pub mod utils;
+
+// Re-export types
+
+use chroma_cache::new_non_persistent_cache_for_test;
+use chroma_storage::test_storage;
+pub use hnsw::*;
+use hnsw_provider::HnswIndexProvider;
+#[allow(unused_imports)]
+pub use spann::*;
+use tempfile::tempdir;
+pub use types::*;
+
+pub fn test_hnsw_index_provider() -> HnswIndexProvider {
+    HnswIndexProvider::new(
+        test_storage(),
+        tempdir()
+            .expect("Should be able to create a temporary directory")
+            .into_path(),
+        new_non_persistent_cache_for_test(),
+        16,
+    )
+}
