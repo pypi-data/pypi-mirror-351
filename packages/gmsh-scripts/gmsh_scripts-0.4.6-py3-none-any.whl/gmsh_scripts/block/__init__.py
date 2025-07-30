101cd3535c2774551715a408e8243578ecf304fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:56 +0000

    Add package gmsh-scripts/0.4.6/gmsh_scripts-0.4.6-py3-none-any.whl

---
+++ packages/gmsh-scripts/gmsh_scripts-0.4.6-py3-none-any.whl/gmsh_scripts/block/__init__.py
@@ -0,0 +1,6 @@
+from gmsh_scripts.block.block import Block
+from gmsh_scripts.block.layer import Layer
+from gmsh_scripts.block.matrix import Matrix
+from gmsh_scripts.block.polyhedron import Polyhedron
+
+__all__ = ['Block', 'Layer', 'Matrix', 'Polyhedron']
