7671a0ea492a17e2356b9d90830d113e114bcecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:30 +0000

    Add package smefit/3.1.0/smefit-3.1.0.tar.gz

---
+++ packages/smefit/smefit-3.1.0.tar.gz/smefit-3.1.0/README.md
@@ -0,0 +1,101 @@
+
+<p align="center">
+  <a href="https://lhcfitnikhef.github.io/smefit_release/"><img alt="SMEFiT" src=https://github.com/LHCfitNikhef/smefit_release/blob/main/docs/_assets/logo.png width=300>
+</a>
+</p>
+
+<p align="center">
+  <a href="https://github.com/lhcfitnikhef/smefit_release/actions/workflows/unittests.yml"><img alt="Tests" src="https://github.com/lhcfitnikhef/smefit_release/actions/workflows/unittests.yml/badge.svg" /></a>
+  <a href="https://www.codefactor.io/repository/github/lhcfitnikhef/smefit_release"><img src="https://www.codefactor.io/repository/github/lhcfitnikhef/smefit_release/badge" alt="CodeFactor" /></a>
+</p>
+
+[SMEFiT](https://lhcfitnikhef.github.io/smefit_release/index.html) is a python program for Standard Model Effective Field Theory fits
+## Installation
+
+To install the smefit release on PYPI you can do:
+
+```bash
+pip install smefit
+```
+
+### Installation for developers
+
+If you are interested in developing smefit or having the latest smefit code not yet released, you should clone the smefit repository and then install in editable mode:
+
+```bash
+cd smefit_release
+pip install -e .
+```
+
+If one is interested in having smefit installed in a conda environment, this can be done by creating the environment (for example with python 3.12), activating it and then installing inside the environment.
+
+```bash
+conda create python=3.12 -n smefit-dev
+conda activate smefit-dev
+pip install -e .
+```
+
+## Running
+The fitting code provide two fitting strategies.
+To run the code with `Nested Sampling` you can do:
+
+```bash
+smefit NS <path_to_runcard>
+```
+
+To run the code suing the analytic method (valid only for linear fits) you can do:
+
+```bash
+smefit A <path_to_runcard>
+```
+
+An runcard example is provided in `runcards/test_runcard.yaml`.
+You can also do `smefit -h` for more help.
+
+### Running in parallel
+
+To run smefit with `Nested Sampling` in parallel you can do:
+
+```bash
+mpiexec -n number_of_cores smefit NS <path_to_runcard>
+```
+
+## Documentation
+If you want to build the documentation do:
+```bash
+cd docs
+make html
+```
+## Unit tests
+To run the unit test you need to install:
+```bash
+pip install pytest pytest-env pytest-cov
+```
+And then simply run:
+```bash
+pytest
+```
+
+## Reports
+To run reports and produce PDF and HTML output you need to have [pandoc](https://pandoc.org/) and [pdflatex](https://www.math.rug.nl/~trentelman/jacob/pdflatex/pdflatex.html) installed.
+The first one is available in conda the latter can be sourced in:
+
+```bash
+souce /cvmfs/sft.cern.ch/lcg/external/texlive/2020/bin/x86_64-linux/pdflatex
+```
+
+## Citation policy
+Please cite our paper when using the code:
+
+```
+@article{Giani:2023gfq,
+    author = "Giani, Tommaso and Magni, Giacomo and Rojo, Juan",
+    title = "{SMEFiT: a flexible toolbox for global interpretations of particle physics data with effective field theories}",
+    eprint = "2302.06660",
+    archivePrefix = "arXiv",
+    primaryClass = "hep-ph",
+    reportNumber = "Nikhef-2022-023",
+    month = "2",
+    year = "2023"
+}
+```
---
+++ packages/smefit/smefit-3.1.0.tar.gz/smefit-3.1.0/src/smefit/cli/__init__.py
@@ -0,0 +1,243 @@
+# -*- coding: utf-8 -*-
+import pathlib
+import sys
+
+import click
+
+from .. import log
+from ..analyze import run_report
+from ..log import print_banner, setup_console
+from ..postfit import Postfit
+from ..prefit import Prefit
+from ..projections import Projection
+from ..runner import Runner
+from .base import base_command
+
+try:
+    from mpi4py import MPI
+
+    run_parallel = True
+except ModuleNotFoundError:
+    run_parallel = False
+
+fit_card = click.argument(
+    "fit_card",
+    type=click.Path(path_type=pathlib.Path, exists=True),
+)
+
+n_replica = click.option(
+    "-n",
+    "--n_replica",
+    type=int,
+    default=None,
+    required=True,
+    help="Number of the replica",
+)
+
+log_file = click.option(
+    "-l",
+    "--log_file",
+    type=click.Path(path_type=pathlib.Path),
+    default=None,
+    required=False,
+    help="path to log file",
+)
+
+rotate_to_pca = click.option(
+    "--rotate_to_pca",
+    type=bool,
+    is_flag=True,
+    default=False,
+    help="Run the fit in the PCA basis",
+)
+
+
+@base_command.command("NS")
+@fit_card
+@log_file
+@rotate_to_pca
+def nested_sampling(
+    fit_card: pathlib.Path, log_file: pathlib.Path, rotate_to_pca: bool
+):
+    """Run a fit with |NS| (Ultra Nest).
+
+    Usage: smefit NS [OPTIONS] path_to_runcard
+    """
+    rank = 0
+    if run_parallel:
+        comm = MPI.COMM_WORLD
+        rank = comm.Get_rank()
+
+    if rank == 0:
+        setup_console(log_file)
+        print_banner()
+        runner = Runner.from_file(fit_card.absolute())
+        if rotate_to_pca:
+            runner.rotate_to_pca()
+        log.console.log("Running : Nested Sampling Fit ")
+    else:
+        runner = None
+
+    if run_parallel:
+        runner = comm.bcast(runner, root=0)
+    runner.run_analysis("NS")
+
+
+@base_command.command("A")
+@fit_card
+@log_file
+@rotate_to_pca
+def analytic_linear(
+    fit_card: pathlib.Path, log_file: pathlib.Path, rotate_to_pca: bool
+):
+    """Get the analytic solution of the linear problem.
+
+    Usage: smefit A [OPTIONS] path_to_runcard
+    """
+    setup_console(log_file)
+    print_banner()
+    runner = Runner.from_file(fit_card.absolute())
+    if rotate_to_pca:
+        runner.rotate_to_pca()
+    log.console.log("Running : analytic solution.")
+    runner.run_analysis("A")
+
+
+@base_command.command("MC")
+@fit_card
+@n_replica
+@log_file
+@rotate_to_pca
+def monte_carlo_fit(
+    fit_card: pathlib.Path, n_replica: int, log_file: pathlib.Path, rotate_to_pca: bool
+):
+    """Run a fit with |MC|.
+
+    Usage: smefit MC [OPTIONS] path_to_runcard
+    """
+    setup_console(log_file)
+    print_banner()
+    runner = Runner.from_file(fit_card.absolute(), n_replica)
+    if rotate_to_pca:
+        runner.rotate_to_pca()
+    log.console.log("Running : MonteCarlo Fit")
+    runner.run_analysis("MC")
+
+
+@base_command.command("PREFIT")
+@fit_card
+def pre_fit(fit_card: pathlib.Path):
+    """Run prefit: computes the SM chi2 as a check before fitting.
+
+    Usage: smefit PREFIT [OPTIONS] path_to_runcard
+    """
+    runner = Runner.from_file(fit_card.absolute())
+    prefit = Prefit(runner.run_card)
+    prefit.chi2_sm()
+
+
+@base_command.command("POSTFIT")
+@click.argument(
+    "result_folder",
+    type=click.Path(path_type=pathlib.Path, exists=True),
+)
+@click.option(
+    "-n",
+    "--n_replica",
+    type=int,
+    default=None,
+    required=True,
+    help="total number of replica to save",
+)
+@click.option(
+    "-c",
+    "--clean_rep",
+    is_flag=True,
+    default=False,
+    help="remove the replica file",
+)
+def post_fit(result_folder: pathlib.Path, n_replica: int, clean_rep: bool):
+    """Run postfit selection over |MC| replicas.
+
+    Usage: smefit POSTFIT [OPTIONS] path_to_result_folder
+    """
+    postfit = Postfit.from_file(result_folder.absolute())
+    postfit.save(n_replica)
+    if clean_rep:
+        postfit.clean()
+
+
+@base_command.command("SCAN")
+@fit_card
+@click.option(
+    "-n",
+    "--n_replica",
+    type=int,
+    default=0,
+    required=False,
+    help="number of replicas used during the scan, default (0) will use only experimental data",
+)
+@click.option(
+    "-b",
+    "--bounds",
+    type=bool,
+    is_flag=True,
+    default=False,
+    help="compute also the chi2 bounds",
+)
+@click.option(
+    "-s",
+    "--scan_points",
+    type=int,
+    default=100,
+    required=False,
+    help="number of points to scan",
+)
+def scan(fit_card: pathlib.Path, n_replica: int, bounds: bool, scan_points: int):
+    r"""Plot individual :math:`\chi^2` profiles for all the free parameters.
+
+    Usage: smefit SCAN [OPTIONS] path_to_runcard
+    """
+    runner = Runner.from_file(fit_card.absolute())
+    runner.chi2_scan(n_replica, bounds, scan_points)
+
+
+@base_command.command("R")
+@click.argument(
+    "report_card",
+    type=click.Path(path_type=pathlib.Path, exists=True),
+)
+def report(report_card: pathlib.Path):
+    """Run a fit report.
+
+    Usage: smefit R path_to_runcard
+    """
+    run_report(report_card.absolute())
+
+
+@base_command.command("PROJ")
+@click.argument(
+    "projection_card",
+    type=click.Path(path_type=pathlib.Path, exists=True),
+)
+@click.option(
+    "--lumi",
+    type=float,
+    default=None,
+    required=False,
+    help="Adjusts the statistical uncertainties according to the specified luminosity. "
+    "If not specified, the original uncertainties are kept "
+    "and the central values are fluctuates according to the specified noise level.",
+)
+@click.option(
+    "--noise",
+    type=click.Choice(["L0", "L1"], case_sensitive=True),
+    default="L0",
+    required=False,
+    help="Noise level for the projection, choose between L0 or L1. Assumes L0 by default.",
+)
+def projection(projection_card: pathlib.Path, lumi: float, noise: str):
+    r"""Compute projection for specified dataset"""
+
+    projection_setup = Projection.from_config(projection_card)
+    projection_setup.build_projection(lumi, noise)
