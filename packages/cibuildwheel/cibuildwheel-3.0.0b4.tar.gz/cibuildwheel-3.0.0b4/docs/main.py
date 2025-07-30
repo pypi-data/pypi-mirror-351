dcaa4e9994f10c45ab25a3d165f857b89e95109f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:38 +0000

    Add package cibuildwheel/3.0.0b4/cibuildwheel-3.0.0b4.tar.gz

---
+++ packages/cibuildwheel/cibuildwheel-3.0.0b4.tar.gz/cibuildwheel-3.0.0b4/docs/main.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+import sysconfig
+from typing import Any
+
+
+def define_env(env: Any) -> None:
+    "Hook function for mkdocs-macros"
+
+    @env.macro  # type: ignore[misc]
+    def subprocess_run(*args: str) -> str:
+        "Run a subprocess and return the stdout"
+        env = os.environ.copy()
+        scripts = sysconfig.get_path("scripts")
+        env["PATH"] = f"{scripts}{os.pathsep}{env.get('PATH', '')}"
+        return subprocess.run(args, check=True, capture_output=True, text=True, env=env).stdout
