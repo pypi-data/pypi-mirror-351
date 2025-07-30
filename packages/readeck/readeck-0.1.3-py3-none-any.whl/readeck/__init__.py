b81311557e01333a8116313a8bbf03bbcb9adfd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:10:14 +0000

    Add package readeck/0.1.3/readeck-0.1.3-py3-none-any.whl

---
+++ packages/readeck/readeck-0.1.3-py3-none-any.whl/readeck/__init__.py
@@ -0,0 +1,57 @@
+"""Readeck Python Client Library.
+
+A Python client library for the Readeck API, providing easy access to
+Readeck's bookmark and reading features.
+"""
+
+__version__ = "0.1.3"
+__author__ = "Yash Vanzara"
+__email__ = "yashvanzara@gmail.com"
+
+from .client import ReadeckClient
+from .exceptions import (
+    ReadeckAuthError,
+    ReadeckError,
+    ReadeckNotFoundError,
+    ReadeckServerError,
+    ReadeckValidationError,
+)
+from .models import (
+    Bookmark,
+    BookmarkCreateRequest,
+    BookmarkCreateResponse,
+    BookmarkCreateResult,
+    BookmarkLink,
+    BookmarkListParams,
+    BookmarkResource,
+    BookmarkResources,
+    EmailSettings,
+    Provider,
+    ReaderSettings,
+    User,
+    UserProfile,
+    UserSettings,
+)
+
+__all__ = [
+    "ReadeckClient",
+    "ReadeckError",
+    "ReadeckAuthError",
+    "ReadeckNotFoundError",
+    "ReadeckServerError",
+    "ReadeckValidationError",
+    "UserProfile",
+    "User",
+    "Provider",
+    "UserSettings",
+    "ReaderSettings",
+    "EmailSettings",
+    "Bookmark",
+    "BookmarkCreateRequest",
+    "BookmarkCreateResponse",
+    "BookmarkCreateResult",
+    "BookmarkLink",
+    "BookmarkListParams",
+    "BookmarkResource",
+    "BookmarkResources",
+]
