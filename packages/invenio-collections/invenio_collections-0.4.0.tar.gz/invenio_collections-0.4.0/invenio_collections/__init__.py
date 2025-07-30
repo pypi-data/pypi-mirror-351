c3d4ccce1d0fd58580084ca0de491af01a973529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:52 +0000

    Add package invenio-collections/0.4.0/invenio_collections-0.4.0.tar.gz

---
+++ packages/invenio-collections/invenio_collections-0.4.0.tar.gz/invenio_collections-0.4.0/invenio_collections/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 CERN.
+# Copyright (C) 2025 Ubiquity Press.
+#
+# Invenio-Collections is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio module for organizing metadata into collections."""
+
+from .ext import InvenioCollections
+
+__version__ = "0.4.0"
+
+__all__ = ("__version__", "InvenioCollections")
