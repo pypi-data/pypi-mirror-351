599c9fb03aeba66208e6faad20c3c0cef7d6bb3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:17 +0000

    Add package convert-stream/1.2/convert_stream-1.2.tar.gz

---
+++ packages/convert-stream/convert_stream-1.2.tar.gz/convert_stream-1.2/convert_stream/__main__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+    Configuração instalação:
+ $ pip3 install -r requirements.txt
+ 
+ 
+------------------[versão 1.1]------------------
++ Incluir funcionalidade para adicionar imagens ao PdfStream()
++ Incluir os métodos __eq__() e __hash__() em PageDocumentPdf()
++ Incluir o método set_rotation() em ImageObject()
+ 
+"""
+import sys, os
+this_file = os.path.abspath(os.path.relpath(__file__))
+dir_of_project = os.path.dirname(this_file)
+sys.path.insert(0, dir_of_project)
+
+from convert_stream import (
+    version, __module_name__, __modify_date__, print_title
+)
+
+
+def test():
+    from convert_stream import PageDocumentPdf, PdfStream, DocumentPdf
+    from soup_files import File, Directory, UserFileSystem
+
+
+def main():
+    print_title(f'{__module_name__} versão {version} - {__modify_date__}', char='-')
+    test()
+
+if __name__ == '__main__':
+    main()
