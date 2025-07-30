5eac54b803cac25da9f0da8aabdef5cb3182af52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:05 +0000

    Add package prompt-galary/0.1.0/prompt_galary-0.1.0.tar.gz

---
+++ packages/prompt-galary/prompt_galary-0.1.0.tar.gz/prompt_galary-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(
+    name='prompt_galary',  # Must be unique on PyPI
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=["langchain_groq","pydantic"],  # or list of dependencies
+    author='Jay Telgote',
+    author_email='ijaytelgote@gmail.com',
+    description='a prompt galary for llm',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ijaytelgote/prompt_galary',
+    license='MIT',
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
