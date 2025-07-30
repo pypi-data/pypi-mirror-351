18292e2d0d31c5043baf33d08c909252bd44657e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:13 +0000

    Add package type-less/0.1.15/type_less-0.1.15.tar.gz

---
+++ packages/type-less/type_less-0.1.15.tar.gz/type_less-0.1.15/tests/test_replace.py
@@ -0,0 +1,23 @@
+from .matching import validate_is_equivalent_type
+from type_less.replace import replace_type_hint_deep, replace_type_hint_map_deep
+from typing import TypedDict
+
+def test_replace_flat():
+    test_type = list[str]
+    replaced_type = replace_type_hint_deep(test_type, str, int)
+    assert replaced_type == list[int]
+
+def test_replace_nested():
+    test_type = list[dict[str, int]]
+    replaced_type = replace_type_hint_deep(test_type, str, int)
+    assert replaced_type == list[dict[int, int]]
+
+def test_replace_map():
+    test_type = list[dict[str, int]]
+    replaced_type = replace_type_hint_map_deep(test_type, {str: int, int: str})
+    assert replaced_type == list[dict[int, str]]
+
+def test_replace_typeddict():
+    test_type = TypedDict("Test", {"name": str, "age": int})
+    replaced_type = replace_type_hint_deep(test_type, str, int)
+    assert validate_is_equivalent_type(replaced_type, TypedDict("Test", {"name": int, "age": int}))
