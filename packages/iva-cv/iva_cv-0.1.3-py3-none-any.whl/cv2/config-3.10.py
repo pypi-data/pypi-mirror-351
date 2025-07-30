4901e7cb077fcf1fffe21c98e0f55d8e494cb598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:08:04 +0000

    Add package iva-cv/0.1.3/iva_cv-0.1.3-py3-none-any.whl

---
+++ packages/iva-cv/iva_cv-0.1.3-py3-none-any.whl/cv2/config-3.10.py
@@ -0,0 +1,3 @@
+PYTHON_EXTENSIONS_PATHS = [
+    os.path.join('/usr/local/lib/python3.10/dist-packages/cv2', 'python-3.10')
+] + PYTHON_EXTENSIONS_PATHS
---
+++ packages/iva-cv/iva_cv-0.1.3-py3-none-any.whl/cv2/config.py
@@ -0,0 +1,5 @@
+import os
+
+BINARIES_PATHS = [
+    os.path.join('/home/iva/office/package_development/opencv_custom/opencv_build/install', 'lib')
+] + BINARIES_PATHS
