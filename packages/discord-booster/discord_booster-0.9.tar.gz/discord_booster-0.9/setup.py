2d08ee34d649e14696af58711f7bd844c782682b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:04 +0000

    Add package discord-booster/0.9/discord_booster-0.9.tar.gz

---
+++ packages/discord-booster/discord_booster-0.9.tar.gz/discord_booster-0.9/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="discord-booster",
+    version="0.9",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "colorama"
+    ],
+    entry_points={
+        'console_scripts': [
+            'discord_booster=discord_booster:main',
+        ],
+    },
+)
