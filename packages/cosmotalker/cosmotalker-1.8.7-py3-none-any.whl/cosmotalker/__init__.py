d2726d17eae4ea842b8be1a9e63143323d3f65f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:23:11 +0000

    Add package cosmotalker/1.8.7/cosmotalker-1.8.7-py3-none-any.whl

---
+++ packages/cosmotalker/cosmotalker-1.8.7-py3-none-any.whl/cosmotalker/__init__.py
@@ -0,0 +1,11 @@
+from .get import get
+from .spacex import spacex
+from .search import search
+from .apod import apod
+from .celestrak import celestrak
+from .feedback import feedback
+from .chat import chat
+from .wiki import wiki
+from .img import img
+from .img_db import image_source
+
