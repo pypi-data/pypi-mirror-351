c70c1737d32e1136ef553217334d27fb9ac5a43f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:11:34 +0000

    Add package gita-py/0.1.0/gita_py-0.1.0.tar.gz

---
+++ packages/gita-py/gita_py-0.1.0.tar.gz/gita_py-0.1.0/gita/data.py
@@ -0,0 +1,148 @@
+# summaries
+summaries = {
+    1: (
+        "Chapter 1 of Bhagavad Gita sets the scene where the two armies of Kauravas "
+        "and Pandavas are juxtaposed in the field of Kurukshetra. Sanjay is granted divine vision "
+        "by sage Ved Vyas to see the whole saga so that he can narrate it to Dhritrashtra, "
+        "father of Duryodhan. Sanjay describes the expanse of the armies of both sides to Dhritrashtra "
+        "and the valor of the mighty warriors on each side. Sanjay also mentions that victory is certain "
+        "for the side where Shri Krishna is. Arjuna is mounted in a chariot controlled by Shri Krishna, "
+        "the Lord of three lokas. Arjun is disheartened to see all his relatives standing in the opposite army "
+        "as his adversaries, who until yesterday were merely friends. Due to his mental dilemma, Arjun first requests "
+        "Shri Krishna to place his chariot between the two armies so that he can see all those who have stood against him. "
+        "Dominated by 'Satgun' (the quality of love and affection), Arjuna drops his weapon and retreats to the back of the chariot, "
+        "citing reasons that he cannot all of a sudden kill those whom he had until now considered his own. "
+        "He even expresses a desire that if Duryodhan kills him right now, then the whole carnage would be averted. "
+        "Arjun thus seeks the refuge of Shri Krishna."
+    ),
+    2: (
+        "In this chapter, Arjun reiterates to Shree Krishna that he is unable to cope with his current situation,"
+        "where he has to kill his elders and teachers. He refuses to take part in such a battle and requests Shree"
+        "Krishna to be his spiritual teacher and guide him on the proper path of action. Then the Supreme Lord"
+        "starts imparting divine knowledge to Arjun. He begins with the immortal-nature of the soul, which is"
+        "eternal and imperishable. Death only destroys the physical body, but the soul continues its journey."
+        "Just as a person discards his old clothes and adorns new ones, the soul "
+        "keeps changing bodies from one lifetime to another."
+        "The Lord then reminds Arjun that his social responsibility as a warrior is to fight for upholding righteousness."
+        "He explains that performing one’s social duty is a virtuous act that can take him to the celestial abodes," 
+        "while dereliction only leads to infamy and humiliation."
+        "At first, Shree Krishna tries to motivate Arjun at a mundane level. Then he moves deeper and "
+        "starts explaining to Arjun the Science of Work."
+        "He asks Arjun to perform his deeds without any attachment to their fruits."
+        "This science of working without desire for rewards is called the yog of the intellect or buddhi-yog."
+        "He further advises that the intellect should be used to control the desire for rewards from work."
+        "By working with such intent, the bondage-creating karmas get transformed into bondage-breaking "
+        "karmas and a state beyond sorrows can be attained."
+        "Arjun is curious to know more about those who are situated in divine consciousness. Shree Krishna, therefore, describes how"
+        "persons who have attained transcendence are free from attachment, fear, and anger."
+        "They are undisturbed and equipoised in every situation. With their senses subdued, they keep their minds always absorbed in God."
+        "He also explains the progression of afflictions of the mind—such as greed, anger, lust, etc. and advises how these can be overcome."
+
+    ),
+    3: (
+        "Shree Krishna expounds on karm yog or the yog of action in this chapter. He explains to Arjun that nobody "
+        "can remain without action, even for a moment. Bound by their inherent modes of nature, all beings"
+        "are always engaged in some work. Superior are those who practice karm yog and continue to"
+        "work diligently to fulfill their responsibilities externally, but internally they are unattached to them. However, the hypocrites display"
+        "external renunciation but internally dwell upon the objects of their senses."
+        "Shree Krishna says that all living beings are an integral part of God’s creation and have roles and responsibilities to fulfill."
+        "Vedas prescribe performing yajña to please the celestial gods, who in-return bestow material gains."
+        "These yajñas cause rains to fall, which aids in the production of food grains for the sustenance of life on earth."
+        "Those who live only for the delight of their senses and do not accept their responsibility in this cycle— are sinful and living in vain."
+        "However, Shree Krishna says that when works or prescribed duties are performed as an obligation to God, they are also considered yajña (sacrifice)."
+        "He then explains that unlike the rest of humankind, the enlightened souls are not obliged to fulfill their bodily responsibilities."
+        "They are situated in knowledge of the self and execute higher responsibilities of the soul. However, they do not abandon their social duties,"
+        "as it may create disharmony in the minds of the common people who look up to them for inspiration. Therefore, the wise continue to work without any"
+        "personal motive only to set good examples for others to follow, else, the ignorant may abandon their prescribed duties prematurely. Shree Krishna" 
+        "mentions one such example of the enlightened King Janak, who performed his earthly duties as an ideal king and father. Arjun then asks Shree Krishna"
+        "why people commit sin, even when unwilling, as if by some force. Lord Krishna explains that lust alone is the sinful all-devouring enemy."
+        "Similar to a fire that is covered by smoke or a mirror covered in dust, desire shrouds one’s knowledge and deludes the intellect."
+        "In the end, Shree Krishna advises Arjun that by controlling the senses, mind, and intellect—one can slay this enemy" 
+        "called desire, which is an embodiment of sin."
+    ),
+    4: (
+        "To strengthen Arjun’s faith in the knowledge He is imparting, Shree Krishna reveals its pristine origin in this chapter."
+        "He says, “Arjun, as you are my devotee and a dear friend, I am revealing this supreme science of yog to you. It is the same eternal"
+        "science that I taught to the Sun God at the beginning of time. And in a continuous tradition, the same knowledgge; was passed"
+        "to the saintly kings.” Arjun wonders how someone who is almost his age could exist so many eons ago. He asks Shree Krishna"
+        "how is it possible that you gave this knowledge to the Sun God and the others so many years back in time."
+        "Shree Krishna clarifies Arjun’s doubts and discloses the divine mystery of His descension. He says that"
+        "although God is unborn and eternal, it is to establish dharma (the path of righteousness) that He descends on this earth by His Yogmaya power."
+        "His birth and activities are divine and cannot get tainted by material imperfections. Once a devotee knows this secret and engages"
+        "in devotion with great faith, he attains God and does not take birth in this world again."
+        "Then Shree Krishna goes back to karm yog, the subject from the previous chapter. He continues to explain the nature of work and"
+        "its three principles: action, in-action, and forbidden action."
+        "He elaborates that the karm yogis, even while performing the most engaging task, are in the state of inaction and do not get entangled"
+        "in the karmic reactions of such action. Knowing this: the ancient sages performed all their work as an act of"
+        "sacrifice for the pleasure of God.  They were not affected by happiness, distress, success, or failure. He explains that when"
+        "sacrifice is suitably dedicated, performed with proper knowledge and pure sentiments, then its remnants"
+        "become like nectar. And by partaking such nectar, the devotees are cleansed of all impurities. Even the biggest"
+        "sinner can cross over the ocean of material miseries by boarding this boat of eternal knowledge."
+        "Shree Krishna says that such knowledge should be acquired only from a genuine spiritual master, who"
+        "himself is God-realized. Being his Guru, Shree Krishna instructs Arjun to cut apart all doubts that have arisen in his heart with"
+        "this sword of knowledge. He asks Arjun to now get up and perform his duty as a warrior."
+    )
+}
+
+
+# verses
+verses = {
+    1: {
+        1.1: "Dhritarashtra said: O Sanjay, after gathering on the holy field of Kurukshetra, and desiring to fight, what did my sons and the sons of Pandu do?",
+        1.2: "Sanjay said: On observing the Pandava army standing in military formation, King Duryodhan approached his teacher Dronacharya, and said the following words.",
+        1.3: "Duryodhan said: Respected teacher! Behold the mighty army of the sons of Pandu, so expertly arrayed for battle by your own gifted disciple, the son of Drupad.",
+        1.4: "Behold in their ranks are many powerful warriors, like Yuyudhan, Virat, and Drupad, wielding mighty bows and equal in military prowess to Bheem and Arjun.",
+        1.5: "There are also accomplished heroes like Dhrishtaketu, Chekitan, the gallant King of Kashi, Purujit, Kuntibhoj, and Shaibya—all the best of men.",
+        1.6: "In their ranks, they also have the courageous Yudhamanyu, the gallant Uttamauja, the son of Subhadra, and the sons of Draupadi, who are all great warrior chiefs.",
+        1.7: "O best of Brahmins, hear too about the principal generals on our side, who are especially qualified to lead. These I now recount unto you.",
+        1.8: "There are personalities like yourself, Bheeshma, Karna, Kripa, Ashwatthama, Vikarn, and Bhurishrava, who are ever victorious in battle.",
+        1.9: "Also, there are many other heroic warriors, who are prepared to lay down their lives for my sake. They are all skilled in the art of warfare, and equipped with various kinds of weapons.",
+        1.10: "The strength of our army is unlimited and we are safely marshalled by Grandsire Bheeshma, while the strength of the Pandava army, carefully marshalled by Bheem, is limited.",
+        1.11: "Therefore, I call upon all the generals of the Kaurava army now to give full support to Grandsire Bheeshma, even as you defend your respective strategic points.",
+        1.12: "Then, the grand old man of the Kuru dynasty, the glorious patriarch Bheeshma, roared like a lion, and blew his conch shell very loudly, giving joy to Duryodhan.",
+        1.13: "Thereafter, conches, kettledrums, bugles, trumpets, and horns suddenly blared forth, and their combined sound was overwhelming.",
+        1.14: "Then, from amidst the Pandava army, seated in a glorious chariot drawn by white horses, Madhav and Arjun blew their Divine conch shells.",
+        1.15: "Hrishikesh blew his conch shell, called Panchajanya, and Arjun blew the Devadutta. Bheem, the voracious eater and performer of herculean tasks, blew his mighty conch, called Paundra.",
+        1.16: "King Yudhishthir, blew the Anantavijay, while Nakul and Sahadev blew the Sughosh and Manipushpak.",
+        1.17: "The excellent archer and king of Kashi, the great warrior Shikhandi, Dhrishtadyumna, Virat, and the invincible Satyaki,",
+        1.18: "Drupad, the five sons of Draupadi, and the mighty-armed Abhimanyu, son of Subhadra, all blew their respective conch shells, O Ruler of the earth.",
+        1.19: "The terrific sound thundered across the sky and the earth, and shattered the hearts of your sons, O Dhritarasthra.",
+        1.20: "At that time, the son of Pandu, Arjun, who had the insignia of Hanuman on the flag of his chariot, took up his bow. Seeing your sons arrayed against him, O King, Arjun then spoke the following words to Shree Krishna.",
+        1.21: "Arjun said: O Infallible One, please take my chariot to the middle of both armies,",
+        1.22: "so that I may look at the warriors arrayed for battle, whom I must fight in this great combat.",
+        1.23: "I desire to see those who have come here to fight on the side of the evil-minded son of Dhritarasthra, wishing to please him.",
+        1.24: "Sanjay said: O Dhritarasthra, having thus been addressed by Arjun, the conqueror of sleep, Shree Krishna then drew the magnificent chariot between the two armies.",
+        1.25: "In the presence of Bheeshma, Dronacharya, and all the other kings, Shree Krishna said: O Parth, behold these Kurus gathered here.",
+        1.26: "There, Arjun could see stationed in both armies, his fathers, grandfathers, teachers, maternal uncles, brothers, cousins, sons, nephews, grand-nephews, friends, fathers-in-law, and well-wishers.",
+        1.27: "Seeing all his relatives present there, Arjun, the son of Kunti, was overwhelmed with compassion, and with deep sorrow, spoke the following words.",
+        1.28: "Arjun said: O Krishna, seeing my own kinsmen arrayed for battle here and intent on killing each other, my limbs are giving way and my mouth is drying up.",
+        1.29: "My whole body shudders; my hair is standing on end. My bow, the Gāṇḍīv, is slipping from my hand, and my skin is burning all over.",
+        1.30: "My mind is in quandary and whirling in confusion; I am unable to hold myself steady any longer.",
+        1.31: "O Krishna, killer of the Keshi demon, I only see omens of misfortune. I do not foresee how any good can come from killing my own kinsmen in this battle.",
+        1.32: "O Krishna, I do not desire victory, kingdom, or the happiness accruing to it.",
+        1.33: "Of what avail will be a kingdom, pleasures, or even life itself, when the very persons for whom we covet them, are standing before us for battle?",
+        1.34: "Teachers, fathers, sons, grandfathers, maternal uncles, grandsons, fathers-in-law, grand-nephews, brothers-in-law, and other kinsmen are present here, staking their lives and riches.",
+        1.35: "O Madhusudan, I do not wish to slay them, even if they attack me. If we kill the sons of Dhritarashtra, what satisfaction will we derive from the dominion over the three worlds, what to speak of this Earth?",
+        1.36: "O Maintainer of all living entities, what pleasure will we derive from killing the sons of Dhritarasthra? Even though they may be aggressors, sin will certainly come upon us if we slay them.",
+        1.37: "Hence, it does not behoove us to kill our own cousins, the sons of Dhritarashtra, and friends. O Madhav (Krishna), how can we hope to be happy by killing our own kinsmen?",
+        1.38: "Their thoughts are overpowered by greed and they see no wrong in annihilating their relatives or wreaking treachery upon friends.",
+        1.39: "Yet, O Janardan (Krishna), why should we, who can clearly see the crime in killing our kindred, not turn away from this sin?",
+        1.40: "When a dynasty is destroyed, its traditions get vanquished, and the rest of the family becomes involved in irreligion.",
+        1.41: "With the preponderance of vice, O Krishna, the women of the family become immoral; and from the immorality of women, O descendent of Vrishni, unwanted progeny are born.",
+        1.42: "An increase in unwanted children results in hellish life both for the family and for those who destroy the family. Deprived of sacrificial offerings, the ancestors of such corrupt families also fall.",
+        1.43: "Through the evil deeds of those who destroy the family tradition and thus give rise to unwanted progeny, a variety of social and family welfare activities are ruined.",
+        1.44: "O Janardan (Krishna), I have heard from the learned that those who destroy family traditions dwell in hell for an indefinite period of time.",
+        1.45: "Alas! How strange it is that we have set our mind to perform this great sin with horrifying consequences. Driven by the desire for kingly pleasures, we are intent on killing our own kinsmen.",
+        1.46: "It would be better if, with weapons in hand, the sons of Dhritarashtra kill me unarmed and unresisting on the battlefield.",
+        1.47: "Sanjay said: Speaking thus, Arjun cast aside his bow and arrows, and sank into the seat of his chariot, his mind in distress and overwhelmed with grief."
+
+    }
+    # ... all chapters
+}
+
+# quotes
+quotes = [
+    "You have the right to perform your duties, but not to the fruits...",
+    "The soul is neither born nor does it ever die...",
+    # ... more quotes
+]
