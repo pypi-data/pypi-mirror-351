41abe7aa74b479f4a6247b48d44d36d637a33d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:04 +0000

    Add package digitalhub-runtime-python/0.11.0/digitalhub_runtime_python-0.11.0.tar.gz

---
+++ packages/digitalhub-runtime-python/digitalhub_runtime_python-0.11.0.tar.gz/digitalhub_runtime_python-0.11.0/digitalhub_runtime_python/entities/task/python_job/spec.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from digitalhub.entities.task._base.spec import TaskSpecFunction, TaskValidatorFunction
+
+
+class TaskSpecPythonJob(TaskSpecFunction):
+    """
+    TaskSpecPythonJob specifications.
+    """
+
+
+class TaskValidatorPythonJob(TaskValidatorFunction):
+    """
+    TaskValidatorPythonJob validator.
+    """
