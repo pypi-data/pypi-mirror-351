e0f0734b8e1086356daca25302965124d3d2b63d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:04 +0000

    Add package ommlx/0.0.0.dev323/ommlx-0.0.0.dev323.tar.gz

---
+++ packages/ommlx/ommlx-0.0.0.dev323.tar.gz/ommlx-0.0.0.dev323/ommlx/tokens/__init__.py
@@ -0,0 +1,38 @@
+from .specials import (  # noqa
+    StandardSpecialToken,
+    StandardSpecialTokens,
+
+    SpecialTokens,
+)
+
+from .tokenizers import (  # noqa
+    Tokenizer,
+
+    BaseTokenizer,
+)
+
+from .types import (  # noqa
+    NonSpecialToken,
+    SpecialToken,
+    Token,
+
+    cast_token,
+    check_token,
+
+    TokenStr,
+)
+
+from .vocabs import (  # noqa
+    NotSeqVocabError,
+    Vocab,
+)
+
+#
+
+Bos = StandardSpecialTokens.Bos
+Eos = StandardSpecialTokens.Eos
+Unk = StandardSpecialTokens.Unk
+Sep = StandardSpecialTokens.Sep
+Pad = StandardSpecialTokens.Pad
+Cls = StandardSpecialTokens.Cls
+Mask = StandardSpecialTokens.Mask
