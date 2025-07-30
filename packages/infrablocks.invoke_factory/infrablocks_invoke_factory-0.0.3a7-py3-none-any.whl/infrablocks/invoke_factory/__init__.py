0b9e60e757b94de882b165b81591b3bde659aaeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:16 +0000

    Add package infrablocks.invoke_factory/0.0.3a7/infrablocks_invoke_factory-0.0.3a7-py3-none-any.whl

---
+++ packages/infrablocks.invoke_factory/infrablocks_invoke_factory-0.0.3a7-py3-none-any.whl/infrablocks/invoke_factory/__init__.py
@@ -0,0 +1,5 @@
+from .task_factory import Arguments, BodyCallable, Parameters
+from .task_factory import Parameter as Parameter
+from .task_factory import create_task as create_task
+
+__all__ = ["Parameters", "Arguments", "BodyCallable"]
