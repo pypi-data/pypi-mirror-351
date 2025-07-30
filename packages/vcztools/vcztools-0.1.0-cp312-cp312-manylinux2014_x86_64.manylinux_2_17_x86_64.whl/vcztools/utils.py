b5666283eed27075cee7d273d20abc767fc2d112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:35:51 +0000

    Add package vcztools/0.1.0/vcztools-0.1.0-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/vcztools/vcztools-0.1.0-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/vcztools/utils.py
@@ -0,0 +1,56 @@
+from contextlib import ExitStack, contextmanager
+from pathlib import Path
+
+import numpy as np
+
+from vcztools.constants import RESERVED_VCF_FIELDS
+
+
+def search(a, v):
+    """
+    Finds the indices into an array a corresponding to the elements in v.
+    The behaviour is undefined if any elements in v are not in a.
+    """
+    sorter = np.argsort(a)
+    rank = np.searchsorted(a, v, sorter=sorter)
+    return sorter[rank]
+
+
+@contextmanager
+def open_file_like(file):
+    """A context manager for opening a file path or string (and closing on exit),
+    or passing a file-like object through."""
+    with ExitStack() as stack:
+        if isinstance(file, (str, Path)):
+            file = stack.enter_context(open(file, mode="w"))
+        yield file
+
+
+def vcf_name_to_vcz_name(vcz_names: set[str], vcf_name: str) -> str:
+    """
+    Convert the name of a VCF field to the name of the corresponding VCF Zarr array.
+
+    :param set[str] vcz_names: A set of potential VCF Zarr field names
+    :param str vcf_name: The name of the VCF field
+    :return: The name of the corresponding VCF Zarr array
+    :rtype: str
+    """
+    split = vcf_name.split("/")
+    assert 1 <= len(split) <= 2
+    is_genotype_field = split[-1] == "GT"
+    is_format_field = (
+        split[0] in {"FORMAT", "FMT"}
+        if len(split) > 1
+        else is_genotype_field or f"call_{split[-1]}" in vcz_names
+    )
+    is_info_field = split[0] == "INFO" or split[-1] not in RESERVED_VCF_FIELDS
+
+    if is_format_field:
+        if is_genotype_field:
+            return "call_genotype"
+        else:
+            return "call_" + split[-1]
+    elif is_info_field:
+        return f"variant_{split[-1]}"
+    else:
+        return RESERVED_VCF_FIELDS[vcf_name]
