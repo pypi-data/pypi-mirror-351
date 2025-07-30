3df604b9eb0b8dc24496d9bfa6fe963a51dd6e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:13 +0000

    Add package ecss-chat-client/1.0.3/ecss_chat_client-1.0.3-py3-none-any.whl

---
+++ packages/ecss-chat-client/ecss_chat_client-1.0.3-py3-none-any.whl/ecss_chat_client/spotlight.py
@@ -0,0 +1,19 @@
+from requests import Response
+
+from .lib import Base
+
+
+class Spotlight(Base):
+    """Сервис Spotlight."""
+
+    def search_by_spotlight(self, page_count: int, search_query: str,
+                            search_schema: list) -> Response:
+        """Поиск в Spotlight по определенной схеме."""
+        return self._make_request(
+            'spotlight',
+            payload={
+                'count': page_count,
+                'query': search_query,
+                'searchSchema': search_schema,
+            },
+        )
