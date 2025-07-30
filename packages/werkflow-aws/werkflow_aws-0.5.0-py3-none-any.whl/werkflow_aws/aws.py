d41de0e15bca1dac4325f28407cc3920f30bab8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:29 +0000

    Add package werkflow-aws/0.5.0/werkflow_aws-0.5.0-py3-none-any.whl

---
+++ packages/werkflow-aws/werkflow_aws-0.5.0-py3-none-any.whl/werkflow_aws/aws.py
@@ -0,0 +1,37 @@
+from werkflow_core import Module
+from .parsers import AWSConfigParser
+from .services import (
+    AWSCredentials,
+    AWSCodeArtifact,
+    AWSCostExplorer,
+    AWSElastiCache,
+    AWSSTS,
+)
+
+
+class AWS(Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.config = AWSConfigParser()
+        self.credentials = AWSCredentials()
+        self.code_artifact = AWSCodeArtifact()
+        self.cost_explorer = AWSCostExplorer()
+        self.elasticache = AWSElastiCache()
+        self.sts = AWSSTS()
+
+    async def close(self):
+        await self.credentials.close()
+        await self.code_artifact.close()
+        await self.cost_explorer.close()
+        await self.elasticache.close()
+
+    def abort(self):
+        self.credentials.abort()
+        self.code_artifact.abort()
+        self.cost_explorer.abort()
+        self.elasticache.abort()
+
+    
+
