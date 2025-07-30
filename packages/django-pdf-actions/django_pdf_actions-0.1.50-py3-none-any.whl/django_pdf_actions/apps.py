aa406af54331db3ee4535cc5e2c20d835669abd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:07:28 +0000

    Add package django-pdf-actions/0.1.50/django_pdf_actions-0.1.50-py3-none-any.whl

---
+++ packages/django-pdf-actions/django_pdf_actions-0.1.50-py3-none-any.whl/django_pdf_actions/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class ExportPDFConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_pdf_actions'
+    label = 'django_pdf_actions'
+    verbose_name = 'Django PDF Actions Export PDF'
