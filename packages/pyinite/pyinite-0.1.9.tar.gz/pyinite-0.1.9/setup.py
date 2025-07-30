ae84c0276b8926f3086ecfb13079ec594b73aee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:11 +0000

    Add package pyinite/0.1.9/pyinite-0.1.9.tar.gz

---
+++ packages/pyinite/pyinite-0.1.9.tar.gz/pyinite-0.1.9/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyinite',
+    version='0.1.9',
+    description='A Python lib you can use to initialize your Python scripts (Console based / Windowed)',
+    author='Guillaume Plagier',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'pyinite': ['Dll/win32dll.exe']
+    },
+)
