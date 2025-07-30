bbc499656f8483f5b8f545735596b6afe683bdf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:41 +0000

    Add package stads/1.0.6/stads-1.0.6.tar.gz

---
+++ packages/stads/stads-1.0.6.tar.gz/stads-1.0.6/src/stads/config.py
@@ -0,0 +1,14 @@
+from importlib.resources import files, as_file
+import data.video_sequences
+
+from src.stads.read_images import get_frames_from_mp4
+
+SIGMA = 4
+KERNEL_SIZE = int(SIGMA * 3) + 1
+
+video_resource = files(data.video_sequences).joinpath("dendrites_one.mp4")
+with as_file(video_resource) as video_path:
+    REFERENCE_VIDEO_SEQUENCE = get_frames_from_mp4(str(video_path), 100)
+
+if REFERENCE_VIDEO_SEQUENCE is None:
+    raise FileNotFoundError(f"Video could not be loaded: {video_path}")
