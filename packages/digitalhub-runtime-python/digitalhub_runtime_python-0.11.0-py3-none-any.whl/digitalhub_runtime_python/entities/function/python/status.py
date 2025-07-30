d199209e347f6a2f9f715bc3206c1ffd3f1b611b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:04 +0000

    Add package digitalhub-runtime-python/0.11.0/digitalhub_runtime_python-0.11.0-py3-none-any.whl

---
+++ packages/digitalhub-runtime-python/digitalhub_runtime_python-0.11.0-py3-none-any.whl/digitalhub_runtime_python/entities/function/python/status.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from digitalhub.entities.function._base.status import FunctionStatus
+
+
+class FunctionStatusPython(FunctionStatus):
+    """
+    FunctionStatusPython status.
+    """
