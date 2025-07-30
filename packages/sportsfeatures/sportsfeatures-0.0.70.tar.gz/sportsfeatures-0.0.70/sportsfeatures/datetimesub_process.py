b54a36e184c3a93c8bad1e108ee4de5a503a84b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:48 +0000

    Add package sportsfeatures/0.0.70/sportsfeatures-0.0.70.tar.gz

---
+++ packages/sportsfeatures/sportsfeatures-0.0.70.tar.gz/sportsfeatures-0.0.70/sportsfeatures/datetimesub_process.py
@@ -0,0 +1,38 @@
+"""A processor for subbing any datetimes with the dt column."""
+
+import logging
+
+import pandas as pd
+from feature_engine.datetime import DatetimeSubtraction
+
+from .identifier import Identifier
+
+
+def datetimesub_process(
+    df: pd.DataFrame, dt_column: str, identifiers: list[Identifier]
+) -> pd.DataFrame:
+    """Process date time subtractions."""
+    identifier_columns = []
+    for identifier in identifiers:
+        identifier_columns.extend(identifier.columns)
+    columns = (
+        df.drop(columns=[dt_column])
+        .select_dtypes(
+            include=["datetime", "datetime64", "datetime64[ns]", "datetimetz"]
+        )
+        .columns.values.tolist()
+    )
+    if columns:
+        for column in columns:
+            if column not in identifier_columns:
+                continue
+            try:
+                dts = DatetimeSubtraction(variables=[column], reference=[dt_column])  # type: ignore
+                df = dts.fit_transform(df)
+            except TypeError as exc:
+                logging.warning(
+                    "Couldn't use datetime subtraction on column %s: %s",
+                    column,
+                    str(exc),
+                )
+    return df.copy()
