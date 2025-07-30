4f27fd9d2464d992ebb4ed3cf0c329c8328668dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:49 +0000

    Add package nsk/2.3.0/nsk-2.3.0-py3-none-any.whl

---
+++ packages/nsk/nsk-2.3.0-py3-none-any.whl/nsk/bot/__init__.py
@@ -0,0 +1,5 @@
+from .dandoli import Dandoli
+from .mail_dealer import MailDealer
+from .microsoft import SharePoint,Microsoft365
+from .web_access import WebAccess
+__all__ = ['Microsoft365','SharePoint','WebAccess','MailDealer','Dandoli']
