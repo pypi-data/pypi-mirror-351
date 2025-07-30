e2d5f1cbafa50f2ff0d064a392eb95f2c7443439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:22 +0000

    Add package on-the-fly-stats/1.1.1/on_the_fly_stats-1.1.1.tar.gz

---
+++ packages/on-the-fly-stats/on_the_fly_stats-1.1.1.tar.gz/on_the_fly_stats-1.1.1/src/on_the_fly_stats/build_info.py
@@ -0,0 +1,10 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.12.3'
+    os_name = 'posix:ubuntu'
+    version = '1.1.1'
+    git_sha = 'd7eabcb07561835f62256715dac5995f2d9c694a'
+    git_branch = 'master'
+    git_uncommitted = [
+    ]
+    git_unpushed = [
+    ]
