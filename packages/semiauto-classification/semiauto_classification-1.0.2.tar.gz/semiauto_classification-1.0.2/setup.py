0e99d5c888bb4dab13ef1d65d0f800999bedacc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:10:00 +0000

    Add package semiauto-classification/1.0.2/semiauto_classification-1.0.2.tar.gz

---
+++ packages/semiauto-classification/semiauto_classification-1.0.2.tar.gz/semiauto_classification-1.0.2/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="semiauto-classification",
+    version="1.0.2",
+    author="Akshat-Sharma-110011",
+    author_email="akshatsharma.business.1310@gmail.com",
+    description="Automated classification pipeline for machine learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Akshat-Sharma-110011/SemiAuto-Classification",
+    packages=find_packages(),
+    package_data={
+        'semiauto_classification': ['pipeline_utils.py']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'numpy==1.23.5',  # Fixed version instead of range
+        'pandas',
+        'scikit-learn',
+        'scipy',
+        'PyYAML',
+        'cloudpickle==2.2.1',
+        'fastapi',
+        'uvicorn',
+        'matplotlib',
+        'seaborn',
+        'optuna',
+        'featuretools',
+        'imbalanced-learn'
+    ],
+    entry_points={
+        'console_scripts': [
+            'semiauto-classification = semiauto_classification.app:main',
+        ],
+    },
+    include_package_data=True,
+)
