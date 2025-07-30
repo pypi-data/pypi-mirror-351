4b35210b7d5f1b6b331d00f8f4767223703fb317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:28 +0000

    Add package pubcrank-django-app/0.3.0/pubcrank_django_app-0.3.0-py3-none-any.whl

---
+++ packages/pubcrank-django-app/pubcrank_django_app-0.3.0-py3-none-any.whl/pubcrank/settings.py
@@ -0,0 +1,26 @@
+from pubcrank.serialize import DateTimeSerializer
+
+FIELD_SERIALIZERS = {
+  'forms.DateTimeField': DateTimeSerializer
+}
+
+def setup_pubcrank(settings, pubdir, theme):
+  settings['PUBCRANK_DIR'] = pubdir
+  settings['PUBCRANK_THEME'] = theme
+  settings['TEMPLATES'].append({
+    'NAME': 'pubcrank',
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [pubdir / 'themes'],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
+        ],
+    },
+  })
+
+  settings['PUBCRANK_MD_EXTRAS'] = ["fenced-code-blocks", "footnotes", "tables", "strike"]
+  settings['PUBCRANK_FIELD_SERIALIZERS'] = FIELD_SERIALIZERS
+  settings['PUBCRANK_PER_PAGE'] = 5
