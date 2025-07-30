3f820f761c23c0bc4fa8f843743584e1010468b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:06:45 +0000

    Add package textfeats/0.1.4/textfeats-0.1.4.tar.gz

---
+++ packages/textfeats/textfeats-0.1.4.tar.gz/textfeats-0.1.4/textfeats/sentiment_process.py
@@ -0,0 +1,48 @@
+"""Calculate sentiment features."""
+
+# pylint: disable=duplicate-code
+import statistics
+
+import pandas as pd
+import tqdm
+from textblob import TextBlob  # type: ignore
+
+from .columns import (DELIMITER, SENTIMENT_COLUMN, SUBJECTIVITY_COLUMN,
+                      TEXT_FEATURES_COLUMN)
+
+
+def sentiment_process(df: pd.DataFrame, verbose: bool) -> pd.DataFrame:
+    """Process the sentiment for any text columns."""
+    df_dict: dict[str, list[float | None]] = {}
+    df_cols = df.select_dtypes(include="object").columns.tolist()
+
+    written_columns = set()
+    it = df.itertuples(name=None)
+    if verbose:
+        it = tqdm.tqdm(it, desc="Sentiment processing", total=len(df))
+    for row in it:
+        row_dict = {x: row[count + 1] for count, x in enumerate(df_cols)}
+        texts = [x for x in row_dict.values() if isinstance(x, str)]
+        if not texts:
+            continue
+        sentiments = []
+        subjectivities = []
+        for text in texts:
+            blob = TextBlob(text)
+            sentiments.append(blob.polarity)
+            subjectivities.append(blob.subjectivity)
+        col = DELIMITER.join([TEXT_FEATURES_COLUMN, SENTIMENT_COLUMN])
+        if col not in df_dict:
+            df_dict[col] = [None for _ in range(len(df))]
+        df_dict[col][row[0]] = statistics.mean(sentiments)
+        written_columns.add(col)
+        col = DELIMITER.join([TEXT_FEATURES_COLUMN, SUBJECTIVITY_COLUMN])
+        if col not in df_dict:
+            df_dict[col] = [None for _ in range(len(df))]
+        df_dict[col][row[0]] = statistics.mean(subjectivities)
+        written_columns.add(col)
+
+    for column in written_columns:
+        df.loc[:, column] = df_dict[column]
+
+    return df[sorted(df.columns.values.tolist())].copy()
---
+++ packages/textfeats/textfeats-0.1.4.tar.gz/textfeats-0.1.4/textfeats/stemmer_process.py
@@ -0,0 +1,50 @@
+"""Calculate stemmer features."""
+
+# pylint: disable=duplicate-code
+import pandas as pd
+import tqdm
+from nltk.stem import PorterStemmer  # type: ignore
+from nltk.tokenize import word_tokenize  # type: ignore
+
+from .columns import DELIMITER, KEY_WORD_COLUMN, TEXT_FEATURES_COLUMN
+
+_STEMMER = PorterStemmer()
+
+
+def _count_key_word(text: str, key_word: str) -> int:
+    words = word_tokenize(text)
+    stems = [_STEMMER.stem(x) for x in words]
+    return stems.count(key_word)
+
+
+def stemmer_process(
+    df: pd.DataFrame, verbose: bool, key_words: set[str]
+) -> pd.DataFrame:
+    """Process the stems for any text columns."""
+    key_words = {_STEMMER.stem(x) for x in key_words}
+    df_dict: dict[str, list[float | None]] = {}
+    df_cols = df.select_dtypes(include="object").columns.tolist()
+
+    written_columns = set()
+    it = df.itertuples(name=None)
+    if verbose:
+        it = tqdm.tqdm(it, desc="Stemmer processing", total=len(df))
+    for row in it:
+        row_dict = {x: row[count + 1] for count, x in enumerate(df_cols)}
+        texts = [x for x in row_dict.values() if isinstance(x, str)]
+        if not texts:
+            continue
+        for key_word in key_words:
+            key_word_count = 0
+            for text in texts:
+                key_word_count += _count_key_word(text, key_word)
+            column = DELIMITER.join([TEXT_FEATURES_COLUMN, KEY_WORD_COLUMN, key_word])
+            if column not in df_dict:
+                df_dict[column] = [None for _ in range(len(df))]
+            df_dict[column][row[0]] = float(key_word_count)
+            written_columns.add(column)
+
+    for column in written_columns:
+        df.loc[:, column] = df_dict[column]
+
+    return df[sorted(df.columns.values.tolist())].copy()
