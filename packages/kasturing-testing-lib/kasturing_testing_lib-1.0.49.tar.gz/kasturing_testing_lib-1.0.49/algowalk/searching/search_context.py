d60805c15b581b1cec8dffd6a5ba1f2d9b9e4697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:05 +0000

    Add package kasturing-testing-lib/1.0.49/kasturing_testing_lib-1.0.49.tar.gz

---
+++ packages/kasturing-testing-lib/kasturing_testing_lib-1.0.49.tar.gz/kasturing_testing_lib-1.0.49/algowalk/searching/search_context.py
@@ -0,0 +1,20 @@
+from algowalk.utils.tracker import StepTracker
+
+class SearchContext:
+    def __init__(self, strategy, visualizer=None):
+        self.strategy = strategy
+        self.visualizer = visualizer
+
+    def execute_search(self, data, target):
+
+        tracker = StepTracker(visualizer=self.visualizer)
+        result_index = self.strategy.search(data, target, tracker)
+        tracker.print_summary()
+        tracker.print_steps()
+        tracker.visualize()
+
+        if result_index != -1:
+            print(f"\n✅ Target {target} found at index {result_index}\n")
+        else:
+            print(f"\n❌ Target {target} not found in the list\n")
+        return result_index
