64de03dd1224a7c55b02fedcde7f7c71cc60fae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:40 +0000

    Add package ChaterJee/0.2.2/chaterjee-0.2.2-py3-none-any.whl

---
+++ packages/ChaterJee/chaterjee-0.2.2-py3-none-any.whl/ChaterJee/updater.py
@@ -0,0 +1,8 @@
+import ChaterJee
+
+if __name__ == '__main__':
+    TOKEN = '1711613332:AAHzf0GMQPjnBINOfIhfbK9dmIUo7mHiThw'
+    CHATID = '1651529355'
+
+    cbot = ChaterJee.ChatLogs(TOKEN, CHATID)
+    cbot.cmdTRIGGER()
