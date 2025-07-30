79d7344d5d8663122f546839729cfae00588402e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:09:54 +0000

    Add package tugo/0.1.1/tugo-0.1.1.tar.gz

---
+++ packages/tugo/tugo-0.1.1.tar.gz/tugo-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tugo',  # 项目名称
+    version='0.1.1',  # 项目版本
+    packages=find_packages(),  # 自动查找包
+    install_requires=[
+        'anyio==4.9.0',
+        'flet==0.27.0',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
