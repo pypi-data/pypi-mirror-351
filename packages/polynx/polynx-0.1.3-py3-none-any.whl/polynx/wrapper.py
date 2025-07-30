8648b3c7d05e0cafb7e1e41788f079b18bcf250d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:08:54 +0000

    Add package polynx/0.1.3/polynx-0.1.3-py3-none-any.whl

---
+++ packages/polynx/polynx-0.1.3-py3-none-any.whl/polynx/wrapper.py
@@ -0,0 +1,31 @@
+import polars as pl
+
+def unwrap(obj):
+    if hasattr(obj, "_pl"):
+        return obj._pl
+    return obj
+
+def wrap(obj):       
+    from .frame import DataFrame
+    from .lazyframe import LazyFrame
+    from .series import Series
+    from .expr import Expr 
+    from .groupby import GroupBy  
+
+    if getattr(obj, "_is_wrapped", False):
+        return obj 
+    
+    if isinstance(obj, pl.DataFrame):
+        return DataFrame(obj)
+    elif isinstance(obj, pl.LazyFrame):
+        #print(obj)
+        new_obj = LazyFrame()
+        new_obj._pl = obj
+        return new_obj
+    elif isinstance(obj, pl.Series):
+        return Series(obj)
+    elif isinstance(obj, pl.Expr):
+        return Expr(obj)
+    elif isinstance(obj, (pl.dataframe.group_by.GroupBy, pl.lazyframe.group_by.LazyGroupBy)):
+        return GroupBy(obj)
+    return obj
