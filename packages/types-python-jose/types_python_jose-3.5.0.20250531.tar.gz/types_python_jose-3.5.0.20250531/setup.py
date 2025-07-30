3d76fe355528316b20d5f82f159e9350919e6902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:09:58 +0000

    Add package types-python-jose/3.5.0.20250531/types_python_jose-3.5.0.20250531.tar.gz

---
+++ packages/types-python-jose/types_python_jose-3.5.0.20250531.tar.gz/types_python_jose-3.5.0.20250531/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'jose-stubs': ['__init__.pyi', 'backends/__init__.pyi', 'backends/_asn1.pyi', 'backends/base.pyi', 'backends/cryptography_backend.pyi', 'backends/ecdsa_backend.pyi', 'backends/native.pyi', 'backends/rsa_backend.pyi', 'constants.pyi', 'exceptions.pyi', 'jwe.pyi', 'jwk.pyi', 'jws.pyi', 'jwt.pyi', 'utils.pyi', 'METADATA.toml', 'py.typed']})
