ff069f9cfd6d57929e63b5d21d5edca80d9d3076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:12 +0000

    Add package starmap-client/2.3.0/starmap-client-2.3.0.tar.gz

---
+++ packages/starmap-client/starmap-client-2.3.0.tar.gz/starmap-client-2.3.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='starmap-client',
+    description='Client for StArMap',
+    version='2.3.0',
+    keywords='stratosphere content artifact mapping cli',
+    author='Jonathan Gangi',
+    author_email='jgangi@redhat.com',
+    url='https://github.com/release-engineering/starmap-client',
+    license='GPLv3+',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    package_data={"starmap-client": ["py.typed"]},
+    include_package_data=True,
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+    ],
+    install_requires=[
+        'attrs',
+        'requests',
+        'requests_mock',
+        'urllib3<2.0.0'
+    ],
+    zip_safe=False,
+)
