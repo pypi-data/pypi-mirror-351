a33767974789a67048b486e8651975dda99a4779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:42 +0000

    Add package odoo-addon-budget-plan-detail/18.0.1.1.0/odoo_addon_budget_plan_detail-18.0.1.1.0-py3-none-any.whl

---
+++ packages/odoo-addon-budget-plan-detail/odoo_addon_budget_plan_detail-18.0.1.1.0-py3-none-any.whl/odoo/addons/budget_plan_detail/hooks.py
@@ -0,0 +1,93 @@
+# Copyright 2022 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import Command
+
+
+def post_init_hook(env):
+    """Update analytic tag dimension for new module"""
+    # Enable Analytic Tags
+    env.ref("base.group_user").write(
+        {"implied_ids": [(4, env.ref("account_analytic_tag.group_analytic_tags").id)]}
+    )
+
+    AnalyticDimension = env["account.analytic.dimension"]
+    dimensions = AnalyticDimension.search([])
+    # skip it if not dimension
+    if not dimensions:
+        return
+    _models = env["ir.model"].search(
+        [
+            (
+                "model",
+                "in",
+                [
+                    "budget.plan.line.detail",
+                    "account.budget.move",
+                    "budget.move.adjustment.item",
+                    "budget.monitor.report",
+                    "budget.source.fund.report",
+                ],
+            ),
+        ],
+        order="id",
+    )
+    _models.write(
+        {
+            "field_id": [
+                Command.create(
+                    {
+                        "name": AnalyticDimension.get_field_name(dimension.code),
+                        "field_description": dimension.name,
+                        "ttype": "many2one",
+                        "relation": "account.analytic.tag",
+                    }
+                )
+                for dimension in dimensions
+            ],
+        }
+    )
+
+
+def uninstall_hook(env):
+    """Cleanup all dimensions before uninstalling."""
+    AnalyticDimension = env["account.analytic.dimension"]
+    dimensions = AnalyticDimension.search([])
+    # drop relation column x_dimension_<code>
+    for dimension in dimensions:
+        name_column = AnalyticDimension.get_field_name(dimension.code)
+        env.cr.execute(
+            """
+            DELETE FROM ir_model_fields
+            WHERE name=%s and model='budget.plan.line.detail'
+            """,
+            (name_column,),
+        )
+        env.cr.execute(
+            """
+            DELETE FROM ir_model_fields
+            WHERE name=%s and model='account.budget.move'
+            """,
+            (name_column,),
+        )
+        env.cr.execute(
+            """
+            DELETE FROM ir_model_fields
+            WHERE name=%s and model='budget.move.adjustment.item'
+            """,
+            (name_column,),
+        )
+        env.cr.execute(
+            """
+            DELETE FROM ir_model_fields
+            WHERE name=%s and model='budget.monitor.report'
+            """,
+            (name_column,),
+        )
+        env.cr.execute(
+            """
+            DELETE FROM ir_model_fields
+            WHERE name=%s and model='budget.source.fund.report'
+            """,
+            (name_column,),
+        )
---
+++ packages/odoo-addon-budget-plan-detail/odoo_addon_budget_plan_detail-18.0.1.1.0-py3-none-any.whl/odoo/addons/budget_plan_detail/models/budget_move_adjustment.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class BudgetMoveAdjustmentItem(models.Model):
+    _name = "budget.move.adjustment.item"
+    _inherit = ["analytic.dimension.line", "budget.move.adjustment.item"]
+    _analytic_tag_field_name = "analytic_tag_ids"
+
+    analytic_tag_ids = fields.Many2many(
+        comodel_name="account.analytic.tag",
+        string="Analytic Tags",
+    )
