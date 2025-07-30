875dc1d8f91d06aa82d9ed60a19cc075a9cfe763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:35:51 +0000

    Add package odoo-addon-stock-inventory-lockdown/15.0.1.0.1/odoo_addon_stock_inventory_lockdown-15.0.1.0.1-py3-none-any.whl

---
+++ packages/odoo-addon-stock-inventory-lockdown/odoo_addon_stock_inventory_lockdown-15.0.1.0.1-py3-none-any.whl/odoo/addons/stock_inventory_lockdown/models/stock_move_line.py
@@ -0,0 +1,49 @@
+# © 2016 Numérigraphe SARL
+# Copyright 2017 ForgeFlow S.L.
+#   (http://www.forgeflow.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, models
+from odoo.exceptions import ValidationError
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    def _get_reserved_locations(self):
+        self.ensure_one()
+        return self.move_line_ids.mapped("location_id")
+
+    def _get_dest_locations(self):
+        self.ensure_one()
+        return self.move_line_ids.mapped("location_dest_id")
+
+    def _location_usage_without_restriction(self):
+        self.ensure_one()
+        return (
+            self.location_dest_id.usage == "inventory"
+            or self.location_id.usage == "inventory"
+        )
+
+    @api.constrains("location_dest_id", "location_id", "state")
+    def _check_locked_location(self):
+        for move_line in self.filtered(lambda m: m.state == "done"):
+            locked_location_ids = self.env[
+                "stock.inventory"
+            ]._get_locations_open_inventories(
+                [move_line.location_dest_id.id, move_line.location_id.id]
+            )
+            if (
+                locked_location_ids
+                and not move_line._location_usage_without_restriction()
+            ):
+                location_names = locked_location_ids.mapped("complete_name")
+                raise ValidationError(
+                    _(
+                        "Inventory adjusment underway at "
+                        "the following location(s):\n- %s\n"
+                        "Moving products to or from these locations is "
+                        "not allowed until the inventory adjustment is complete."
+                    )
+                    % "\n - ".join(location_names)
+                )
