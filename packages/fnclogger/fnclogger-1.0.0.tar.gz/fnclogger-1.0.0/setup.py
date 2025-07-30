0c4d90b5b39b69a1d4694807063d927d2e69244c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:08:21 +0000

    Add package fnclogger/1.0.0/fnclogger-1.0.0.tar.gz

---
+++ packages/fnclogger/fnclogger-1.0.0.tar.gz/fnclogger-1.0.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='fnclogger',
+    version='1.0.0',
+    description='Простой и мощный логгер для Python с цветным выводом',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/fnclogger',
+    packages=find_packages(),
+    python_requires='>=3.7',
+    install_requires=[],
+    extras_require={
+        'rich': ['rich>=10.0.0'],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Logging',
+    ],
+    keywords='logging logger fancy colored json simple',
+)
