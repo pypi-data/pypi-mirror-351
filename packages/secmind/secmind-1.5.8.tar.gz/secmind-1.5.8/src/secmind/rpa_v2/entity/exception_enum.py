0969e2fc7782cae7661f1475a0aa85ca2091a219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:41 +0000

    Add package secmind/1.5.8/secmind-1.5.8.tar.gz

---
+++ packages/secmind/secmind-1.5.8.tar.gz/secmind-1.5.8/src/secmind/rpa_v2/entity/exception_enum.py
@@ -0,0 +1,43 @@
+exception_map = {
+    "FileNotFoundError": {
+        "ch": "文件未找到",
+        "en": "The file is missing"
+    },
+    "timed out": {
+        "ch": "连接超时",
+        "en": "connection timeout"
+    },
+    "Access denied'": {
+        "ch": "账号，密码错误或服务器地址不允许访问",
+        "en": "Account number, password error or server address not allowed to access"
+    },
+    "Connection refused": {
+        "ch": "连接被拒绝",
+        "en": "Connection refused"
+    },
+    "mysqladmin flush-hosts": {
+        "ch": "由于连接失败次数过多，服务器地址被锁定，请手动连接到数据库使用mysqladmin flush-hosts",
+        "en": "Due to too many connection failures, the server address is locked. Please manually connect to the database and use mysqladmin flush-hosts"
+    },
+    # ======= linux =======
+    "Authentication failed": {
+        "ch": "账号，密码错误或服务器地址不允许访问",
+        "en": "Account number, password error or server address not allowed to access"
+    },
+    "Unable to connect to port": {
+        "ch": "端口无法连接",
+        "en": "Cannot connect to port "
+    },
+    "脚本执行超时": {
+        "ch": "规则执行超时，任务已自动终止",
+        "en": "The rule execution timed out and the task was automatically terminated."
+    },
+    "chage：没有权限": {
+        "ch": "账号权限不足",
+        "en": "Insufficient account permissions"
+    },
+    "chage: Permission denied": {
+        "ch": "账号权限不足",
+        "en": "Insufficient account permissions"
+    },
+}
