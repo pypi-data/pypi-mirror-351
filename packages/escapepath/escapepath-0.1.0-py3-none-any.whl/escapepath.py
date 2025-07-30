1f765a5bc26a27dd4074ad5cf5397e3a292a01fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:27 +0000

    Add package escapepath/0.1.0/escapepath-0.1.0-py3-none-any.whl

---
+++ packages/escapepath/escapepath-0.1.0-py3-none-any.whl/escapepath.py
@@ -0,0 +1,47 @@
+import os
+import shlex
+import subprocess
+
+
+def escape_posix(posix_path):
+    return shlex.quote(posix_path)
+
+
+def escape_nt(nt_path):
+    return subprocess.list2cmdline([nt_path])
+
+
+def get_os_flavor():
+    """
+    Return the underlying *flavour* of the operating system.
+
+    - `'posix'` for Unix-like systems (Linux, macOS, etc.).
+    - `'nt'`    for Windows.
+
+    On Jython `os.name` is the shadow string `'java'`;
+    we call `os.name.getshadow()` to discover the real platform.
+    """
+    name = os.name
+
+    # Fast path: CPython & PyPy
+    if name in ('posix', 'nt'):
+        return name
+
+    # Jython and friends
+    if name == 'java':
+        try:
+            shadow = name.getshadow()  # type: ignore[attr-defined]
+        except AttributeError as exc:
+            raise OSError("`os.name` is 'java' but no shadow value available")
+
+        if shadow in ('posix', 'nt'):
+            return shadow
+
+    raise OSError("Unsupported `os.name` value: %r" % name)
+
+
+def escape(path):
+    if get_os_flavor() == 'nt':
+        return escape_nt(path)
+    else:
+        return escape_posix(path)
