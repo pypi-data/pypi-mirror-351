d512ef4ab3e097c333b98986f8650f54f2448724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:41 +0000

    Add package abstract-hugpy/0.0.0.14/abstract_hugpy-0.0.0.14-py3-none-any.whl

---
+++ packages/abstract-hugpy/abstract_hugpy-0.0.0.14-py3-none-any.whl/abstract_hugpy/routes.py
@@ -0,0 +1,10 @@
+def import_google_flan():
+    from .hugging_face_models.google_flan import *
+def import_keybert_model():
+    from .hugging_face_models.keybert_model import *
+def import_summarizer_model():
+    from .hugging_face_models.summarizer_model import *
+def import_whisper_model():
+    from .hugging_face_models.whisper_model import *
+def import_deepcoder():
+    from .hugging_face_models.deepcoder import *
