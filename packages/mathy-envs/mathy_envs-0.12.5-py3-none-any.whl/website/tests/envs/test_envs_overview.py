8ff929744bfc7003ab782f68f6baafa9564a83d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:31 +0000

    Add package mathy-envs/0.12.5/mathy_envs-0.12.5-py3-none-any.whl

---
+++ packages/mathy-envs/mathy_envs-0.12.5-py3-none-any.whl/website/tests/envs/test_envs_overview.py
@@ -0,0 +1,58 @@
+def test_envs_overview_custom_problem_text():
+    from ...docs.snippets.envs import custom_problem_text  # noqa
+
+
+def test_envs_overview_custom_win_conditions():
+    from ...docs.snippets.envs import custom_win_conditions  # noqa
+
+
+def test_envs_overview_custom_timestep_rewards():
+    from ...docs.snippets.envs import custom_timestep_rewards  # noqa
+
+
+def test_envs_overview_custom_episode_rewards():
+    from ...docs.snippets.envs import custom_episode_rewards  # noqa
+
+
+def test_envs_overview_custom_actions():
+    from ...docs.snippets.envs import custom_actions  # noqa
+
+
+def test_envs_overview_openai_gym():
+    from ...docs.snippets.envs import openai_gym  # noqa
+
+
+def test_envs_overview_text_to_tree():
+    from ...docs.snippets.envs import text_to_tree  # noqa
+
+
+def test_envs_overview_tree_to_list():
+    from ...docs.snippets.envs import tree_to_list  # noqa
+
+
+def test_envs_overview_list_to_observations():
+    from ...docs.snippets.envs import lists_to_observations  # noqa
+
+
+def test_envs_overview_env_observations():
+    from ...docs.snippets.envs import env_observations  # noqa
+
+
+def test_envs_overview_graph_observations():
+    from ...docs.snippets.envs import graph_observations  # noqa
+
+
+def test_envs_overview_message_passing_observations():
+    from ...docs.snippets.envs import message_passing_observations  # noqa
+
+
+def test_envs_overview_flat_observations():
+    from ...docs.snippets.envs import flat_observations  # noqa
+
+
+def test_envs_overview_hierarchical_observations():
+    from ...docs.snippets.envs import hierarchical_observations  # noqa
+
+
+def test_envs_overview_unified_observations():
+    from ...docs.snippets.envs import unified_observations  # noqa
