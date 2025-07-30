64f91e2000a31091d441fa3683689e692c9351c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:42 +0000

    Add package nomadicml/0.0.1/nomadicml-0.0.1.tar.gz

---
+++ packages/nomadicml/nomadicml-0.0.1.tar.gz/nomadicml-0.0.1/setup.py
@@ -0,0 +1,45 @@
+"""Setup script for the NomadicML SDK."""
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="nomadicml",
+    version="0.0.1",
+    author="NomadicML Inc",
+    author_email="info@nomadicml.com",
+    description="Python SDK for NomadicML's DriveMonitor API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    #url="https://github.com/nomadicml/nomadicml-python",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "requests>=2.25.0",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=7.0.0",
+            "pytest-cov>=4.0.0",
+            "black>=23.0.0",
+            "isort>=5.12.0",
+            "flake8>=6.0.0",
+            "mypy>=1.0.0",
+            "types-requests>=2.28.0",
+        ],
+    },
+)
