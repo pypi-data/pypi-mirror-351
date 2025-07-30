c55bc793403c736b7f543ebff253215f2e523fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:39 +0000

    Add package fb2cal/1.3.2/fb2cal-1.3.2-py3-none-any.whl

---
+++ packages/fb2cal/fb2cal-1.3.2-py3-none-any.whl/fb2cal/__meta__.py
@@ -0,0 +1,20 @@
+__author__ = 'Mo Beigi'
+__copyright__ = 'Copyright 2019-2020'
+__description__ = 'Facebook Birthday Events to ICS file converter'
+__email__ = 'me@mobeigi.com'
+__keywords__ = ['facebook', 'birthday', 'calendar', 'export', 'ics']
+__license__ = "GPLv3"
+__maintainer__ = 'Mo Beigi'
+__status__ = 'Production'
+__title__ = 'fb2cal'
+__version_info__ = (1, 3, 2)
+__version__ = '.'.join(map(str, __version_info__))
+
+__github_url__ = 'https://github.com/mobeigi/fb2cal'
+__github_short_url__ = 'https://git.io/fjMwr'
+__github_assets_absolute_url__ = 'https://raw.githubusercontent.com/mobeigi/fb2cal/main'
+__download_url__= f'https://github.com/mobeigi/fb2cal/archive/v{__version__}.tar.gz'
+
+
+# Make metadata public to script
+__all__ = ['__author__', '__copyright__', '__description__', '__email__', '__license__', '__maintainer__', '__status__', '__title__', '__version_info__', '__version__', '__github_url__', '__github_short_url__', '__github_assets_absolute_url__', '__download_url__']
