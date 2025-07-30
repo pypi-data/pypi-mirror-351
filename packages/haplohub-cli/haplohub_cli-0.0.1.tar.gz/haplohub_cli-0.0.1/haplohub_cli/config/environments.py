16407591bbd9a6432337e9af1f9b1d2b85d87755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:03 +0000

    Add package haplohub-cli/0.0.1/haplohub_cli-0.0.1.tar.gz

---
+++ packages/haplohub-cli/haplohub_cli-0.0.1.tar.gz/haplohub_cli-0.0.1/haplohub_cli/config/environments.py
@@ -0,0 +1,20 @@
+ENVIRONMENTS = {
+    "local": {
+        "auth0_domain": "dev-42a7qv136prmsazj.us.auth0.com",
+        "auth0_client_id": "QHC6QYwzm9UW9fty7e5mno3AJrcjV5MY",
+        "auth0_audience": "http://localhost:3000/api/",
+        "api_url": "http://localhost:8000",
+    },
+    "stage": {
+        "auth0_domain": "dev-42a7qv136prmsazj.us.auth0.com",
+        "auth0_client_id": "5u4nnf7lwKtzH1sGxBm8AtM25FJMiYGm",
+        "auth0_audience": "https://stage.haplohub.com/api/",
+        "api_url": "https://stage.haplohub.com",
+    },
+    "prod": {
+        "auth0_domain": "dev-42a7qv136prmsazj.us.auth0.com",
+        "auth0_client_id": "TRi894X7yV3e8EOpNVrDdvQbRvYnjibH",
+        "auth0_audience": "https://haplohub.com/api/",
+        "api_url": "https://haplohub.com",
+    },
+}
