ab1931bd3613536a9ca3e210f010eb6ccb3441cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:14 +0000

    Add package prefect/3.4.4/prefect-3.4.4-py3-none-any.whl

---
+++ packages/prefect/prefect-3.4.4-py3-none-any.whl/prefect/server/ui/assets/404-CXo2ZBur.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,o as a,m as t,dv as r}from"./index-g6FNXcTE.js";import{u as s}from"./usePageTitle-DEr56mEx.js";const _=e({__name:"404",setup(n){return s("Page Not Found"),(c,m)=>(a(),o(t(r)))}});export{_ as default};
+//# sourceMappingURL=404-CXo2ZBur.js.map
