071ce7f1d99eb566aa2cff0e0b61ece6941ec6f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:34 +0000

    Add package laicrypto/0.1.4/laicrypto-0.1.4-py3-none-any.whl

---
+++ packages/laicrypto/laicrypto-0.1.4-py3-none-any.whl/pqcrypto/__init__.py
@@ -0,0 +1,26 @@
+"""
+laicrypto
+
+Post-quantum Lemniscate-AGM Isogeny (LAI) Encryption Scheme.
+"""
+
+from .lai import (
+    H,
+    sqrt_mod,
+    T,
+    keygen,
+    encrypt,
+    decrypt,
+)
+
+__all__ = [
+    "H",
+    "sqrt_mod",
+    "T",
+    "keygen",
+    "encrypt",
+    "decrypt",
+]
+
+# Versi paket, sinkronkan dengan pyproject.toml:
+__version__ = "0.1.4"
