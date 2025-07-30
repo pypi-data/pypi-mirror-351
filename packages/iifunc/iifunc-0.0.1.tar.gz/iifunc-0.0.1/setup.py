7f7480c84f9139be445534eaba3e2111af92529b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:06 +0000

    Add package iifunc/0.0.1/iifunc-0.0.1.tar.gz

---
+++ packages/iifunc/iifunc-0.0.1.tar.gz/iifunc-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+_name = "iifunc"
+
+setup(
+    name=_name,
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.25.1',
+        'numpy>=1.20.0',
+    ],
+    author="lqxnjk",
+    author_email="lqxnjk@qq.com",
+    description="A short description of your package",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url=f"https://github.com/lqxnjk/{_name}",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
