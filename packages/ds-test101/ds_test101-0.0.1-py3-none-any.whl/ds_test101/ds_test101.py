df0d369a76a0a4f95d442930f66214aaef1b42c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:03 +0000

    Add package ds-test101/0.0.1/ds_test101-0.0.1-py3-none-any.whl

---
+++ packages/ds-test101/ds_test101-0.0.1-py3-none-any.whl/ds_test101/ds_test101.py
@@ -0,0 +1,12 @@
+
+from time import sleep
+import time
+import os
+os.system ('clear')
+
+class dark:
+     def storm (Time):
+         for x in range (0,11):
+             x+=1
+             print ('Weat..',x,end='\r')
+             time.sleep (Time)
