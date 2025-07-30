2cd4f3513fc2f84a7854d0c07baafc7fae99ed4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:46:08 +0000

    Add package gr-libs/0.2.5/gr_libs-0.2.5.tar.gz

---
+++ packages/gr-libs/gr_libs-0.2.5.tar.gz/gr_libs-0.2.5/gr_libs/recognizer/_utils/format.py
@@ -0,0 +1,18 @@
+from gr_libs.recognizer.gr_as_rl.gr_as_rl_recognizer import Draco, GCDraco, Graql
+from gr_libs.recognizer.graml.graml_recognizer import (
+    ExpertBasedGraml,
+    GCGraml,
+    MCTSBasedGraml,
+)
+
+
+def recognizer_str_to_obj(recognizer_str: str):
+    recognizer_map = {
+        "GCGraml": GCGraml,
+        "ExpertBasedGraml": ExpertBasedGraml,
+        "MCTSBasedGraml": MCTSBasedGraml,
+        "Graql": Graql,
+        "Draco": Draco,
+        "GCDraco": GCDraco,
+    }
+    return recognizer_map.get(recognizer_str)
