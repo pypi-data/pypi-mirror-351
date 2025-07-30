9f172d206716d88fd86ef0168b8a76866bc1bf4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:38 +0000

    Add package xianxia-quotes/0.1.3/xianxia_quotes-0.1.3.tar.gz

---
+++ packages/xianxia-quotes/xianxia_quotes-0.1.3.tar.gz/xianxia_quotes-0.1.3/xianxia_quotes/__init__.py
@@ -0,0 +1,13 @@
+"""
+xianxia_quotes package initialization.
+"""
+
+__version__ = "0.1.3"
+
+from .xianxia_quotes import single_quote, quote_stream
+
+__all__ = [
+    "__version__",
+    "single_quote",
+    "quote_stream",
+]
