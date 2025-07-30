827224480644f3c1b66ab642f252e7b84dddf123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:01 +0000

    Add package rnet/2.2.11/rnet-2.2.11.tar.gz

---
+++ packages/rnet/rnet-2.2.11.tar.gz/rnet-2.2.11/src/typing/mod.rs
@@ -0,0 +1,122 @@
+mod body;
+mod cookie;
+mod enums;
+mod header;
+mod ipaddr;
+mod json;
+mod multipart;
+pub mod param;
+mod proxy;
+mod ssl;
+mod status;
+
+pub use self::{
+    body::BodyExtractor,
+    cookie::{Cookie, CookieExtractor},
+    enums::{Impersonate, ImpersonateOS, LookupIpStrategy, Method, SameSite, TlsVersion, Version},
+    header::{
+        HeaderMap, HeaderMapExtractor, HeaderMapItemsIter, HeaderMapKeysIter, HeaderMapValuesIter,
+        HeadersOrderExtractor,
+    },
+    ipaddr::{IpAddrExtractor, SocketAddr},
+    json::Json,
+    multipart::{Multipart, Part},
+    proxy::{Proxy, ProxyExtractor},
+    ssl::SslVerify,
+    status::StatusCode,
+};
+use pyo3::{prelude::*, pybacked::PyBackedStr};
+use rquest_util::EmulationOption;
+use serde::ser::{Serialize, SerializeSeq, Serializer};
+
+/// A struct to represent the `ImpersonateOption` class.
+#[pyclass(subclass)]
+pub struct ImpersonateOption {
+    /// The browser version to impersonate.
+    impersonate: Impersonate,
+
+    /// The operating system.
+    impersonate_os: Option<ImpersonateOS>,
+
+    /// Whether to skip HTTP/2.
+    skip_http2: Option<bool>,
+
+    /// Whether to skip headers.
+    skip_headers: Option<bool>,
+}
+
+#[pymethods]
+impl ImpersonateOption {
+    /// Create a new impersonation option instance.
+    #[new]
+    #[pyo3(signature = (
+        impersonate,
+        impersonate_os = None,
+        skip_http2 = None,
+        skip_headers = None
+    ))]
+    fn new(
+        impersonate: Impersonate,
+        impersonate_os: Option<ImpersonateOS>,
+        skip_http2: Option<bool>,
+        skip_headers: Option<bool>,
+    ) -> Self {
+        Self {
+            impersonate,
+            impersonate_os,
+            skip_http2,
+            skip_headers,
+        }
+    }
+}
+
+pub struct UrlEncodedValuesExtractor(Vec<(PyBackedStr, PyBackedStr)>);
+
+impl Serialize for UrlEncodedValuesExtractor {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut seq = serializer.serialize_seq(Some(self.0.len()))?;
+        for (key, value) in &self.0 {
+            seq.serialize_element::<(&str, &str)>(&(key.as_ref(), value.as_ref()))?;
+        }
+        seq.end()
+    }
+}
+
+impl FromPyObject<'_> for UrlEncodedValuesExtractor {
+    fn extract_bound(ob: &Bound<'_, PyAny>) -> PyResult<Self> {
+        ob.extract().map(Self)
+    }
+}
+
+pub struct ImpersonateExtractor(pub EmulationOption);
+
+impl FromPyObject<'_> for ImpersonateExtractor {
+    fn extract_bound(ob: &Bound<'_, PyAny>) -> PyResult<Self> {
+        if let Ok(impersonate) = ob.downcast::<Impersonate>() {
+            let emulation = EmulationOption::builder()
+                .emulation(impersonate.borrow().into_ffi())
+                .build();
+
+            return Ok(Self(emulation));
+        }
+
+        let option = ob.downcast::<ImpersonateOption>()?.borrow();
+
+        Ok(Self(
+            EmulationOption::builder()
+                .emulation(option.impersonate.into_ffi())
+                .emulation_os(
+                    option
+                        .impersonate_os
+                        .map(|os| os.into_ffi())
+                        .unwrap_or_default(),
+                )
+                .skip_http2(option.skip_http2.unwrap_or(false))
+                .skip_headers(option.skip_headers.unwrap_or(false))
+                .build(),
+        ))
+    }
+}
---
+++ packages/rnet/rnet-2.2.11.tar.gz/rnet-2.2.11/src/typing/proxy.rs
@@ -0,0 +1,145 @@
+use super::HeaderMapExtractor;
+use crate::error::Error;
+use pyo3::{prelude::*, pybacked::PyBackedStr, types::PyList};
+use rquest::header::HeaderValue;
+
+macro_rules! proxy_method {
+    ( $( { $(#[$meta:meta])* $name:ident, $proxy_fn:path} ),* ) => {
+        #[pymethods]
+        impl Proxy {
+            $(
+                $(#[$meta])*
+                #[staticmethod]
+                #[pyo3(signature = (
+                    url,
+                    username = None,
+                    password = None,
+                    custom_http_auth = None,
+                    custom_http_headers = None,
+                    exclusion = None,
+                ))]
+                #[inline]
+                fn $name(
+                    py: Python,
+                    url: &str,
+                    username: Option<&str>,
+                    password: Option<&str>,
+                    custom_http_auth: Option<&str>,
+                    custom_http_headers: Option<HeaderMapExtractor>,
+                    exclusion: Option<&str>,
+                ) -> PyResult<Self> {
+                    py.allow_threads(|| {
+                        Self::create_proxy(
+                            $proxy_fn,
+                            url,
+                            username,
+                            password,
+                            custom_http_auth,
+                            custom_http_headers,
+                            exclusion,
+                        )
+                    })
+                }
+            )*
+        }
+    };
+}
+
+/// A proxy server for a request.
+/// Supports HTTP, HTTPS, SOCKS4, SOCKS4a, SOCKS5, and SOCKS5h protocols.
+#[pyclass(subclass)]
+pub struct Proxy(pub rquest::Proxy);
+
+proxy_method! {
+    {
+        /// Creates a new HTTP proxy.
+        ///
+        /// This method sets up a proxy server for HTTP requests.
+        http,
+        rquest::Proxy::http
+    },
+    {
+        /// Creates a new HTTPS proxy.
+        ///
+        /// This method sets up a proxy server for HTTPS requests.
+        https,
+        rquest::Proxy::https
+    },
+    {
+        /// Creates a new proxy for all protocols.
+        ///
+        /// This method sets up a proxy server for all types of requests (HTTP, HTTPS, etc.).
+        all,
+        rquest::Proxy::all
+    }
+}
+
+impl Proxy {
+    fn create_proxy<'py>(
+        proxy_fn: impl Fn(&'py str) -> rquest::Result<rquest::Proxy>,
+        url: &'py str,
+        username: Option<&'py str>,
+        password: Option<&'py str>,
+        custom_http_auth: Option<&'py str>,
+        custom_http_headers: Option<HeaderMapExtractor>,
+        exclusion: Option<&'py str>,
+    ) -> PyResult<Self> {
+        let mut proxy = proxy_fn(url).map_err(Error::Request)?;
+        // Convert the username and password to a basic auth header value.
+        if let (Some(username), Some(password)) = (username, password) {
+            proxy = proxy.basic_auth(username, password)
+        }
+
+        // Convert the custom HTTP auth string to a header value.
+        if let Some(Ok(custom_http_auth)) = custom_http_auth.map(HeaderValue::from_str) {
+            proxy = proxy.custom_http_auth(custom_http_auth)
+        }
+
+        // Convert the custom HTTP headers to a HeaderMap instance.
+        if let Some(custom_http_headers) = custom_http_headers {
+            proxy = proxy.custom_http_headers(custom_http_headers.0)
+        }
+
+        // Convert the exclusion list to a NoProxy instance.
+        if let Some(exclusion) = exclusion {
+            proxy = proxy.no_proxy(rquest::NoProxy::from_string(exclusion))
+        }
+
+        Ok(Proxy(proxy))
+    }
+}
+
+pub struct ProxyExtractor(pub rquest::Proxy);
+
+impl FromPyObject<'_> for ProxyExtractor {
+    fn extract_bound(ob: &Bound<'_, PyAny>) -> PyResult<Self> {
+        if let Ok(proxy_str) = ob.extract::<PyBackedStr>() {
+            let proxy = rquest::Proxy::all(proxy_str.as_ref() as &str)
+                .map(Self)
+                .map_err(Error::Request)?;
+
+            return Ok(proxy);
+        }
+
+        let proxy = ob.downcast::<Proxy>()?;
+        let proxy = proxy.borrow().0.clone();
+        Ok(Self(proxy))
+    }
+}
+
+pub struct ProxyListExtractor(pub Vec<rquest::Proxy>);
+
+impl FromPyObject<'_> for ProxyListExtractor {
+    fn extract_bound(ob: &Bound<'_, PyAny>) -> PyResult<Self> {
+        let proxies = ob.downcast::<PyList>()?;
+        let len = proxies.len();
+        proxies
+            .into_iter()
+            .try_fold(Vec::with_capacity(len), |mut list, proxy| {
+                let proxy = proxy.downcast::<Proxy>()?;
+                list.push(proxy.borrow().0.clone());
+                Ok::<_, PyErr>(list)
+            })
+            .map(Self)
+    }
+}
