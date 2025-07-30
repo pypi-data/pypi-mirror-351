8755a73d525e15c7f341578f22fb03080570c279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:25 +0000

    Add package pygame-retro-games/0.0.3/pygame_retro_games-0.0.3.tar.gz

---
+++ packages/pygame-retro-games/pygame_retro_games-0.0.3.tar.gz/pygame_retro_games-0.0.3/pygames/bounce.py
@@ -0,0 +1,141 @@
+
+import pygame
+
+
+class Ball:
+
+    def __init__(self, *, x=320, y=240, length=10):
+        self.step = 10
+        self.x_dir = self.step
+        self.y_dir = self.step
+        self.length = length
+        self.collided = -1
+        self.highest = 0
+        self.prior = [
+            pygame.Rect(x - 3 + n * self.step, y - 3 + n * self.step, 6, 6)
+            for n in range(self.length-1)
+        ]
+        self.prior.append(
+            pygame.Rect(x - 5 + self.length * self.step, y - 5 + self.length * self.step, 10, 10)
+        )
+
+    def turn_left(self):
+        self.x_dir = -self.step
+
+    def turn_right(self):
+        self.x_dir = self.step
+
+    def turn_up(self):
+        self.y_dir = -self.step
+
+    def turn_down(self):
+        self.y_dir = self.step
+
+    def _check_collision(self) -> int:
+        return self.prior[-1].collidelist(self.prior[0:-1])
+
+    def move(self):
+        head = self.prior[-1]
+        move = head.move(self.x_dir, self.y_dir)
+        redo = False
+        if not 0 <= move.x + move.width//2 <= 640:
+            self.x_dir = -self.x_dir
+            redo = True
+        if not 0 <= move.y + move.height//2 <= 480:
+            self.y_dir = -self.y_dir
+            redo = True
+        if redo:
+            move = head.move(self.x_dir, self.y_dir)
+        head.update(head.x + 5 - 3, head.y + 5 - 3, 6, 6)
+        self.prior.append(move)
+        self.rect = move
+        while len(self.prior) > self.length:
+            self.prior.pop(0)
+        self.collided = self._check_collision()
+        self.length += 0.1
+        self.highest = max(self.highest, int(self.length))
+        if self.collided >= 0:
+            self.length -= (self.collided + 1)
+
+    def draw(self, screen):
+        if self.collided >= 0:
+            color = "red"
+        else:
+            color = "green"
+        for prior in self.prior[0:-1]:
+            pygame.draw.rect(screen, color, prior)
+        prior = self.prior[-1]
+        pygame.draw.rect(screen, color, prior)
+
+
+class Bounce:
+    """
+    Let the snake bounce around the room.
+    Don't let it eat its own tail.
+    See how many segments you can accumulate.
+
+    Left/Right/Up/Down Arrow Keys to change direction.
+
+    +/- to change the speed
+    """
+
+    def __init__(self):
+        self.ball = Ball()
+        self.speed = 10
+        self.pause = 0.0
+
+    def run(self):
+        self.init()
+        while self.process_input():
+            self.update()
+            self.render()
+        pygame.quit()
+
+    def init(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((640,480))
+        self.clock = pygame.time.Clock()
+
+    def process_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return False
+                elif event.key == pygame.K_LEFT:
+                    self.ball.turn_left()
+                elif event.key == pygame.K_RIGHT:
+                    self.ball.turn_right()
+                elif event.key == pygame.K_UP:
+                    self.ball.turn_up()
+                elif event.key == pygame.K_DOWN:
+                    self.ball.turn_down()
+                elif event.key == pygame.K_EQUALS:
+                    self.speed = min(90, self.speed + 1)
+                elif event.key == pygame.K_MINUS:
+                    self.speed = max(10, self.speed - 1)
+        return True
+
+    def update(self):
+        self.pause = max(0.0, self.pause - 0.1)
+        if self.pause <= 0.0:
+            self.ball.move()
+        pygame.display.set_caption(f'Bounce (Speed {self.speed} , Length {int(self.ball.length)} , Highest {self.ball.highest})')
+        self.screen.fill("purple")
+        self.ball.draw(self.screen)
+
+    def render(self):
+        pygame.display.flip()
+        self.clock.tick(self.speed)
+        if self.pause <= 0.0:
+            if self.ball.collided >= 0:
+                self.pause = self.speed / 10.0
+
+
+def main():
+    Bounce().run()
+
+
+if __name__ == '__main__':
+    main()
