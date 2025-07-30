c249930b4262d2df361903022927320f7f449cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:28 +0000

    Add package pan-scm-sdk/0.3.40/pan_scm_sdk-0.3.40-py3-none-any.whl

---
+++ packages/pan-scm-sdk/pan_scm_sdk-0.3.40-py3-none-any.whl/scm/config/setup/__init__.py
@@ -0,0 +1,15 @@
+"""scm.config.setup: Setup-related service classes."""
+# from .devices import Devices
+from .device import Device
+from .folder import Folder
+from .label import Label
+from .snippet import Snippet
+from .variable import Variable
+
+__all__ = [
+    "Device",
+    "Folder",
+    "Label",
+    "Snippet",
+    "Variable",
+]
