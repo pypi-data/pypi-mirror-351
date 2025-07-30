f3211f079f53c48bbc6e5bd078aebf2074e2047c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:54 +0000

    Add package parser-notam-package/0.2/parser_notam_package-0.2.tar.gz

---
+++ packages/parser-notam-package/parser_notam_package-0.2.tar.gz/parser_notam_package-0.2/setup.py
@@ -0,0 +1,11 @@
+from importlib_metadata import entry_points
+from setuptools import setup,find_packages
+
+setup(
+    name='parser_notam_package',
+    version='0.2',
+    packages=find_packages(),
+    entry_points= {
+
+    }
+)
