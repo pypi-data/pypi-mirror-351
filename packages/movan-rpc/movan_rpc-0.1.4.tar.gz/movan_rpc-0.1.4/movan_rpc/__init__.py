6d9a1187a7e9e22d9b3381e5155d163ae24f69e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:28 +0000

    Add package movan-rpc/0.1.4/movan_rpc-0.1.4.tar.gz

---
+++ packages/movan-rpc/movan_rpc-0.1.4.tar.gz/movan_rpc-0.1.4/movan_rpc/__init__.py
@@ -0,0 +1,13 @@
+"""
+Movan RPC - 一个简单的 RPC（远程过程调用）框架
+
+使用 Python 标准库实现，无需第三方依赖。
+支持同步和异步方法调用。
+"""
+
+from .server import RPCServer, AddressType
+from .client import RPCClient
+from .client_threading import RPCClientThreading
+
+__all__ = ['RPCServer', 'RPCClient', 'AddressType','RPCClientThreading']
+__version__ = '0.1.4'
