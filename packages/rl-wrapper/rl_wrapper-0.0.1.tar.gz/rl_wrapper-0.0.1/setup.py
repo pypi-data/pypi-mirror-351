0099adcf2a20856734258849c0e0e20a69bf0b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:51 +0000

    Add package rl-wrapper/0.0.1/rl_wrapper-0.0.1.tar.gz

---
+++ packages/rl-wrapper/rl_wrapper-0.0.1.tar.gz/rl_wrapper-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rl-wrapper",
+    version="0.0.1",
+    description="Reserved for internal use",
+    author="okupita",
+    author_email="devsecops@okta.com",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+    ],
+)
