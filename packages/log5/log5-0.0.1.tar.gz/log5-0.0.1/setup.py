e9d53d97a4414493708af424d24d63e033d83e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:36 +0000

    Add package log5/0.0.1/log5-0.0.1.tar.gz

---
+++ packages/log5/log5-0.0.1.tar.gz/log5-0.0.1/setup.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+LONGDOC = """
+log5
+=====================
+
+Output logs
+
+https://github.com/hailiang-wang/python-log5
+
+Usage::
+
+    import log5
+
+    logger = log5.get_logger(log5.LN(__name__), output_mode=3)
+    logger.debug('bar')
+    logger.info('foo')
+
+
+output_mode:
+
+* 1: append to log file
+
+* 2: print to stdout
+
+* 3: both file and stdout
+
+"""
+
+setup(
+    name='log5',
+    version='0.0.1',
+    description='Output logs',
+    long_description=LONGDOC,
+    author='Hai Liang Wang',
+    author_email='hailiang.hl.wang@gmail.com',
+    url='https://github.com/hailiang-wang/python-log5',
+    license="Chunsong Public License, version 1.0",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+        'Natural Language :: Chinese (Traditional)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.11'
+    ],
+    keywords='logging',
+    packages=find_packages(),
+    install_requires=[
+        'env3>=0.0.3'
+    ],
+    package_data={
+        'log5': [
+            'LICENSE']})
