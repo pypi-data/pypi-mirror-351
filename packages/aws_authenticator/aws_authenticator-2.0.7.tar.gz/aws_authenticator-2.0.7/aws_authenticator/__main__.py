2aa8105a69ebbea88f2788615b16873aa762e280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:48 +0000

    Add package aws_authenticator/2.0.7/aws_authenticator-2.0.7.tar.gz

---
+++ packages/aws_authenticator/aws_authenticator-2.0.7.tar.gz/aws_authenticator-2.0.7/aws_authenticator/__main__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: latin-1 -*-
+"""Login to AWS using named profiles, IAM access key credentials, or SSO."""
+
+import __init__
+
+if __name__ == "__main__":
+    __init__.main()
