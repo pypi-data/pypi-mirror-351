ad1c5480e1a53db01555d9a2e0e723014f448a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:08 +0000

    Add package kensho-kfinance/2.2.5/kensho_kfinance-2.2.5-py3-none-any.whl

---
+++ packages/kensho-kfinance/kensho_kfinance-2.2.5-py3-none-any.whl/kfinance/pydantic_models.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+
+class RelationshipResponseNoName(BaseModel):
+    """A response from the relationship endpoint before adding the company name.
+
+    Each element in `current` and `previous` is a company_id.
+    """
+
+    current: list[int]
+    previous: list[int]
+
+
+class CompanyIdAndName(BaseModel):
+    """A company_id and name"""
+
+    company_id: int
+    company_name: str
+
+
+class RelationshipResponse(BaseModel):
+    """A response from the relationship endpoint that includes both company_id and name."""
+
+    current: list[CompanyIdAndName]
+    previous: list[CompanyIdAndName]
