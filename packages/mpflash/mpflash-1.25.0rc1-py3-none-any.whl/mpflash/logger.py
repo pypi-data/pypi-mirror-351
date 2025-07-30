8ac7aaa6c613bd3fd900b478e7f38e8875c0902f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:46 +0000

    Add package mpflash/1.25.0rc1/mpflash-1.25.0rc1-py3-none-any.whl

---
+++ packages/mpflash/mpflash-1.25.0rc1-py3-none-any.whl/mpflash/logger.py
@@ -0,0 +1,39 @@
+"""Logging."""
+
+from loguru import logger as log
+from rich.console import Console
+
+from .config import config
+
+console = Console()
+
+
+def _log_formatter(record: dict) -> str:
+    """Log message formatter to combine loguru and rich formatting."""
+    color_map = {
+        "TRACE": "cyan",
+        "DEBUG": "orange",
+        "INFO": "bold",
+        "SUCCESS": "bold green",
+        "WARNING": "yellow",
+        "ERROR": "bold red",
+        "CRITICAL": "bold white on red",
+    }
+    lvl_color = color_map.get(record["level"].name, "cyan")
+    return "[not bold green]{time:HH:mm:ss}[/not bold green] | {level.icon} " + f"[{lvl_color}]{{message}}[/{lvl_color}]"
+
+
+def set_loglevel(loglevel: str):
+    """Set the log level for the logger"""
+    try:
+        log.remove()
+    except ValueError:
+        pass
+    log.add(console.print, level=loglevel.upper(), colorize=False, format=_log_formatter)  # type: ignore
+
+
+def make_quiet():
+    """Make the logger quiet"""
+    config.quiet = True
+    console.quiet = True
+    set_loglevel("CRITICAL")
