6270d245cfa95ea0b27f7fd8c3eca0ad9c9f0897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:08:13 +0000

    Add package alpha-booster/1.0/alpha_booster-1.0.tar.gz

---
+++ packages/alpha-booster/alpha_booster-1.0.tar.gz/alpha_booster-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="alpha-booster",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "colorama"
+    ],
+    entry_points={
+        'console_scripts': [
+            'discord_booster=discord_booster:main',
+        ],
+    },
+)
