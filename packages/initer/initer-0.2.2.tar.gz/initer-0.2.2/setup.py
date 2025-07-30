8805b489edce223c256d3da7fc2b5b3887a307bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:55 +0000

    Add package initer/0.2.2/initer-0.2.2.tar.gz

---
+++ packages/initer/initer-0.2.2.tar.gz/initer-0.2.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='initer',
+    version='0.2.2',
+    description='A Python lib you can use to initialize your Python scripts (Console based / Windowed)',
+    author='Lilou Palagier',
+    packages=find_packages()
+)
