93d35e57c8b748d83fca4742171476bba1ac5f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:21 +0000

    Add package mitiq/0.45.0/mitiq-0.45.0.tar.gz

---
+++ packages/mitiq/mitiq-0.45.0.tar.gz/mitiq-0.45.0/mitiq/_version.py
@@ -0,0 +1,14 @@
+# Copyright (C) Unitary Foundation
+#
+# This source code is licensed under the GPL license (v3) found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Reads in version information.
+Note: This file will be overwritten by the packaging process."""
+
+import os
+
+directory_of_this_file = os.path.dirname(os.path.abspath(__file__))
+
+with open(f"{directory_of_this_file}/../VERSION.txt", "r") as f:
+    __version__ = f.read().strip()
---
+++ packages/mitiq/mitiq-0.45.0.tar.gz/mitiq-0.45.0/mitiq/zne/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (C) Unitary Foundation
+#
+# This source code is licensed under the GPL license (v3) found in the
+# LICENSE file in the root directory of this source tree.
+
+from mitiq.zne.zne import (
+    execute_with_zne,
+    mitigate_executor,
+    zne_decorator,
+    construct_circuits,
+    combine_results,
+)
+from mitiq.zne import scaling
+from mitiq.zne.inference import (
+    mitiq_curve_fit,
+    mitiq_polyfit,
+    LinearFactory,
+    PolyFactory,
+    RichardsonFactory,
+    ExpFactory,
+    PolyExpFactory,
+    AdaExpFactory,
+)
