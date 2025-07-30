91d84c47331371e3a83f26bcb69161a30afb6407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:07 +0000

    Add package qa-pytest-examples/0.0.9/qa_pytest_examples-0.0.9.tar.gz

---
+++ packages/qa-pytest-examples/qa_pytest_examples-0.0.9.tar.gz/qa_pytest_examples-0.0.9/src/qa_pytest_examples/terminalx_configuration.py
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: 2025 Adrian Herscu
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import List, final
+from qa_pytest_examples.model.terminalx_credentials import TerminalXCredentials
+from qa_pytest_examples.model.terminalx_user import TerminalXUser
+from qa_pytest_webdriver.selenium_configuration import SeleniumConfiguration
+
+
+class TerminalXConfiguration(SeleniumConfiguration):
+    @property
+    @final
+    def users(self) -> List[TerminalXUser]:
+        users_section = self.parser["users"]
+        return [
+            TerminalXUser(TerminalXCredentials.from_(username_password), name=key)
+            for key, username_password in users_section.items()
+        ]
