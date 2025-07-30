0bc4244af57f35a8d7807d8baed43a2dde7afc8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:03 +0000

    Add package HYGO/0.9.9/hygo-0.9.9.tar.gz

---
+++ packages/HYGO/hygo-0.9.9.tar.gz/hygo-0.9.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='HYGO',
+    version='0.9.9',
+    description='HYGO: A genetic optimization framework',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ipatazas/HYGO",
+    author='Isaac Robledo Martín',
+    packages=find_packages(),  
+    include_package_data=True,
+    package_data={
+        'hygo.tools': ['Parameter_help.txt'],  
+    },
+    install_requires=[
+        'numpy>=1.16,<2.0',
+        'scipy>=1.3',
+        'matplotlib>=3.0',
+        'pandas>=0.25',
+        'dill>=0.3.0',
+        'scipy>=1.15',
+    ],
+    python_requires='>=3.7'
+)
