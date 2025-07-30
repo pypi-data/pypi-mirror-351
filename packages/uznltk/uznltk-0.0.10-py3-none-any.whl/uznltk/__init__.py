b6d511910f0b4dfc7664a0c7d48ba08276a155fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:52 +0000

    Add package uznltk/0.0.10/uznltk-0.0.10-py3-none-any.whl

---
+++ packages/uznltk/uznltk-0.0.10-py3-none-any.whl/uznltk/__init__.py
@@ -0,0 +1,19 @@
+from ._tokenizer import split_sentences, split_words
+from ._stop_words import stop_words_list, clear_stopword
+from ._download import download_resources #, book_download, news_download, numbers_download
+from ._lemmatizer import lemmatize
+from ._stem import stem_word
+from ._clean import clean_text, solid_sign
+from ._syllable import syllables, hyphenation, count
+
+# # Faqat funksiyalarni eksport qilamiz
+# __all__ = [
+#     'stop_word',
+#     'clean_stop_words',
+#     'lemmatize',
+#     'stem_word',
+#     'clean_text',
+#     'solid_sign',
+#     'syllables',
+#     'book', 'news', 'hyphenation', 'count', 'count_text', 'split_sentences', 'split_words'
+# ]
---
+++ packages/uznltk/uznltk-0.0.10-py3-none-any.whl/uznltk/_syllable.py
@@ -0,0 +1,11 @@
+import UzSyllable
+def syllables(text):
+    a= UzSyllable.syllables(text)
+    return a
+def hyphenation(text):
+    a= UzSyllable.hyphenation(text)
+    return a
+def count(text):
+    a= UzSyllable.count(text)
+    return a
+
