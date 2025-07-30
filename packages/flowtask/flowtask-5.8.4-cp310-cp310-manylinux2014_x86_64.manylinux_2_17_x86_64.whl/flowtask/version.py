ddd38bd843997b3823c8b7ecd95bdad70b5966fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:06:59 +0000

    Add package flowtask/5.8.4/flowtask-5.8.4-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/flowtask/flowtask-5.8.4-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/flowtask/version.py
@@ -0,0 +1,11 @@
+"""Flow Task Meta information."""
+
+__title__ = "flowtask"
+__description__ = (
+    "Framework for running Tasks and from CLI and API for orchestation. "
+    "Component-based Task builder/Runner for non-programmers."
+)
+__version__ = "5.8.4"
+__author__ = "Jesus Lara"
+__author_email__ = "jesuslarag@gmail.com"
+__license__ = "Apache-2.0"
