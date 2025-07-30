5e4c7b99ab0711f52ef5ea4f9d7ecf20520da6f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:11:14 +0000

    Add package finsim/1.1.0/finsim-1.1.0.tar.gz

---
+++ packages/finsim/finsim-1.1.0.tar.gz/finsim-1.1.0/test/test_fit.py
@@ -0,0 +1,42 @@
+
+import unittest
+from datetime import datetime, timedelta
+from math import sqrt
+
+import numpy as np
+
+from finsim.simulation.stock import BlackScholesMertonStockPrices
+from finsim.estimate.fit import fit_BlackScholesMerton_model
+
+
+class TestParameterFitting(unittest.TestCase):
+    def fit_differentbackend(self, lowlevellang):
+        bsm_simulator = BlackScholesMertonStockPrices(100, 0.04, 0.02)
+        prices = bsm_simulator.generate_time_series(100, 1, nbsimulations=1000)
+        timestamps = np.array(
+            [
+                datetime(2020, 1, 1, 0, 0, 0) + i*timedelta(1)
+                for i in range(101)
+            ],
+            dtype='datetime64[s]'
+        )
+        info = [
+            (r, sigma)
+            for r, sigma in map(lambda i: fit_BlackScholesMerton_model(timestamps, prices[i, :], unit='day', lowlevellang=lowlevellang), range(1000))
+        ]
+        mean_r = np.mean([item[0] for item in info])
+        mean_sigma = np.mean([item[1] for item in info])
+
+        self.assertAlmostEqual(mean_r, 0.04, delta=1.96*0.02/sqrt(1000))
+        self.assertAlmostEqual(mean_sigma, 0.02, delta=1.96*0.02/sqrt(1000))
+
+    def test_fit_Cython(self):
+        with self.assertRaises(ValueError):
+            self.fit_differentbackend('C')
+
+    def test_fit_python(self):
+        self.fit_differentbackend('P')
+
+
+if __name__ == '__main__':
+    unittest.main()
