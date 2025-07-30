d5ff99b1578d72f7fa55f84983549fc768c77f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:36 +0000

    Add package idlescape/0.1/idlescape-0.1-py3-none-any.whl

---
+++ packages/idlescape/idlescape-0.1-py3-none-any.whl/idlescape/experience_to_level.py
@@ -0,0 +1,13 @@
+import json
+import importlib
+
+
+def xp_to_level(experience: int) -> int:
+    with importlib.resources.open_text("idlescape.data", "experience_to_level.json") as f:
+        xp_level_table = json.load(f)
+
+    for level in reversed(xp_level_table):
+        print(f"{level=}")
+        if experience >= level["min_xp"]:
+            return level["level"]
+    return 1
