6dcae7af76060b3781c15cb4d5f582bb4a066a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:14 +0000

    Add package contact-space/0.1.3/contact_space-0.1.3-py3-none-any.whl

---
+++ packages/contact-space/contact_space-0.1.3-py3-none-any.whl/contact_space/examples/auth_example.py
@@ -0,0 +1,6 @@
+from contact_space import ContactSpace
+
+api_key = "EXAMPLE_API_KEY"
+url = "https://ukapithunder.makecontact.space/" # Update to region specific URL
+
+auth = ContactSpace(api_key, url)
