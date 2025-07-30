5f9955a11fcac342ca32b60dfdcab9e774a7a92f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:11:31 +0000

    Add package virtuered/2.0.0/virtuered-2.0.0-py3-none-any.whl

---
+++ packages/virtuered/virtuered-2.0.0-py3-none-any.whl/virtuered/webui/frontend/config.js
@@ -0,0 +1,4 @@
+// public/config.js
+// window.__NEXT_PUBLIC_API_URL__ = "http://4bd094a2-01.cloud.together.ai:4418"; // placeholder value
+// window.__NEXT_PUBLIC_API_URL__ = "https://virtueredbackend.virtueai.io"; // placeholder value
+window.__NEXT_PUBLIC_API_URL__ = "http://localhost:4401";
