2efe776efe533a55b87d5ac2e3cb44220b394089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:54 +0000

    Add package reportspy/0.2/reportspy-0.2.tar.gz

---
+++ packages/reportspy/reportspy-0.2.tar.gz/reportspy-0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='reportspy',
+    version='0.2',
+    description='Generate HTML and PDF EDA reports from Excel files',
+    author='Your Name',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'plotly',
+        'pdfkit',
+        'openpyxl',
+        'scipy',
+        'matplotlib',
+        'statsmodels',
+        'jinja2',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+)
