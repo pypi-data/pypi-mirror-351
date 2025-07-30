ac5c680e7e76193c269520e75fec2a6dc4224338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:59 +0000

    Add package turbopuffer/0.5.0a1/turbopuffer-0.5.0a1.tar.gz

---
+++ packages/turbopuffer/turbopuffer-0.5.0a1.tar.gz/turbopuffer-0.5.0a1/src/turbopuffer/types/full_text_search_config.py
@@ -0,0 +1,35 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+
+from .._models import BaseModel
+from .language import Language
+from .tokenizer import Tokenizer
+
+__all__ = ["FullTextSearchConfig"]
+
+
+class FullTextSearchConfig(BaseModel):
+    case_sensitive: Optional[bool] = None
+    """Whether searching is case-sensitive.
+
+    Defaults to `false` (i.e. case-insensitive).
+    """
+
+    language: Optional[Language] = None
+    """Describes the language of a text attribute. Defaults to `english`."""
+
+    remove_stopwords: Optional[bool] = None
+    """Removes common words from the text based on language.
+
+    Defaults to `true` (i.e. remove common words).
+    """
+
+    stemming: Optional[bool] = None
+    """Language-specific stemming for the text.
+
+    Defaults to `false` (i.e., do not stem).
+    """
+
+    tokenizer: Optional[Tokenizer] = None
+    """The tokenizer to use for full-text search on an attribute."""
