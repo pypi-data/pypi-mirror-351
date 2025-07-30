1ad4d119cd0c39a7b7740464459d399f88cf9d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:16 +0000

    Add package pyigd/1.2/pyigd-1.2.tar.gz

---
+++ packages/pyigd/pyigd-1.2.tar.gz/pyigd-1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+PACKAGE_NAME = "pyigd"
+VERSION = "1.2"
+
+setup(
+    name=PACKAGE_NAME,
+    packages=find_packages(),
+    version=VERSION,
+    description="Parser for IGD files",
+    author="Drew DeHaas",
+    author_email="",
+    url="https://aprilweilab.github.io/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
