4225f86b2a08b7478817e89797c99c01230b3b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:42 +0000

    Add package rio-tiler/7.7.4/rio_tiler-7.7.4.tar.gz

---
+++ packages/rio-tiler/rio_tiler-7.7.4.tar.gz/rio_tiler-7.7.4/rio_tiler/__init__.py
@@ -0,0 +1,16 @@
+"""rio-tiler."""
+
+__version__ = "7.7.4"
+
+from . import (  # noqa
+    colormap,
+    constants,
+    errors,
+    expression,
+    io,
+    mosaic,
+    profiles,
+    reader,
+    tasks,
+    utils,
+)
