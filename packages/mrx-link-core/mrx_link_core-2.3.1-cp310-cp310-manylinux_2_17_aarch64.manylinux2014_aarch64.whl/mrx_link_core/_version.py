4b5f266f9213ab6cf6444f45a914a11b340d6b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:23 +0000

    Add package mrx-link-core/2.3.1/mrx_link_core-2.3.1-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/mrx-link-core/mrx_link_core-2.3.1-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/mrx_link_core/_version.py
@@ -0,0 +1,26 @@
+#  MAKINAROCKS CONFIDENTIAL
+#  ________________________
+#
+#  [2017] - [2024] MakinaRocks Co., Ltd.
+#  All Rights Reserved.
+#
+#  NOTICE:  All information contained herein is, and remains
+#  the property of MakinaRocks Co., Ltd. and its suppliers, if any.
+#  The intellectual and technical concepts contained herein are
+#  proprietary to MakinaRocks Co., Ltd. and its suppliers and may be
+#  covered by U.S. and Foreign Patents, patents in process, and
+#  are protected by trade secret or copyright law. Dissemination
+#  of this information or reproduction of this material is
+#  strictly forbidden unless prior written permission is obtained
+#  from MakinaRocks Co., Ltd.
+#
+def _fetchVersion() -> str:
+    # pylint: disable=invalid-name
+    version: str = "2.3.1"
+
+    return version
+
+
+__version__ = _fetchVersion()
+
+__all__ = ["__version__"]
