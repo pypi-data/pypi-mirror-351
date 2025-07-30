20a68712cd7105258092c2511e75dceb8d28fe05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:14 +0000

    Add package hebpipe/4.0.1.0/hebpipe-4.0.1.0.tar.gz

---
+++ packages/hebpipe/hebpipe-4.0.1.0.tar.gz/hebpipe-4.0.1.0/hebpipe/lib/_version.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__version__ = "4.0.1.0"
+__author__ = "Amir Zeldes"
+__copyright__ = "Copyright 2018-2024, Amir Zeldes"
+__license__ = "Apache 2.0 License"
