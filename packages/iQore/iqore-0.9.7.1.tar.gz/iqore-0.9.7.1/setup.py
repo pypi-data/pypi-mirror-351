cb71a14affda6d5d3d8b7ea29dbd0841e8cb017a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:31 +0000

    Add package iQore/0.9.7.1/iqore-0.9.7.1.tar.gz

---
+++ packages/iQore/iqore-0.9.7.1.tar.gz/iqore-0.9.7.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='iQore',
+    version='0.9.7.1',
+    description='Quantum-Classical SDK by iQore (Private)',
+    author='iQore Inc.',
+    author_email='team@iqore.com',
+    url='https://iqore.com',
+    packages=find_packages(include=['iQore', 'iQore.*', 'pyarmor_runtime_006585']),
+    include_package_data=True,
+    package_data={
+        'pyarmor_runtime_006585': ['*', 'linux_x86_64/*', 'windows_x86_64/*', 'darwin_x86_64/*'],
+    },
+    install_requires=[
+        'qiskit==1.4.2',
+    ],
+    python_requires='==3.10.*',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Operating System :: OS Independent',
+    ],
+)
+
