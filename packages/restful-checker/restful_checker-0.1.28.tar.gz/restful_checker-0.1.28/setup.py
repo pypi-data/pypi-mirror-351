629f344dcd65e691dbbc51d092b4e8c9f7090c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:06:56 +0000

    Add package restful-checker/0.1.28/restful_checker-0.1.28.tar.gz

---
+++ packages/restful-checker/restful_checker-0.1.28.tar.gz/restful_checker-0.1.28/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='restful-checker',
+    version='0.1.28',
+    description='Check RESTful API compliance from OpenAPI definitions and generate HTML reports',
+    author='Javi Lianes',
+    author_email='jlianesglr@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['pyyaml','requests'],
+    entry_points={
+        'console_scripts': [
+            'restful-checker=restful_checker.main:main'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
