80f01e121a69ea56f453d544bac2b40051559524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:58 +0000

    Add package myskoda/2.0.0/myskoda-2.0.0.tar.gz

---
+++ packages/myskoda/myskoda-2.0.0.tar.gz/myskoda-2.0.0/myskoda/models/position.py
@@ -0,0 +1,39 @@
+"""Models for responses of api/v2/vehicle-status/{vin}/driving-range."""
+
+from dataclasses import dataclass, field
+from enum import StrEnum
+
+from mashumaro import field_options
+from mashumaro.mixins.orjson import DataClassORJSONMixin
+
+from .common import Address, BaseResponse, Coordinates
+
+
+class PositionType(StrEnum):
+    VEHICLE = "VEHICLE"
+
+
+@dataclass
+class Position(DataClassORJSONMixin):
+    gps_coordinates: Coordinates = field(metadata=field_options(alias="gpsCoordinates"))
+    type: PositionType
+    address: Address | None = None
+
+
+class ErrorType(StrEnum):
+    VEHICLE_IN_MOTION = "VEHICLE_IN_MOTION"
+    VEHICLE_POSITION_UNAVAILABLE = "VEHICLE_POSITION_UNAVAILABLE"
+
+
+@dataclass
+class Error(DataClassORJSONMixin):
+    type: ErrorType
+    description: str
+
+
+@dataclass
+class Positions(BaseResponse):
+    """Positional information (GPS) for the vehicle and other things."""
+
+    errors: list[Error]
+    positions: list[Position]
