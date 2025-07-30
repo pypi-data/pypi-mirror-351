e21602239ef0dbe38eef957c77fbd918faf6c504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:54 +0000

    Add package gs1-gpc/0.1.3/gs1_gpc-0.1.3.tar.gz

---
+++ packages/gs1-gpc/gs1_gpc-0.1.3.tar.gz/gs1_gpc-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gs1-gpc",
+    version="0.1.3",
+    packages=find_packages(exclude=["data", "data.*"]),
+    install_requires=[
+        "click>=8.0.0",
+        "gpcc>=1.0.1",
+    ],
+    entry_points={
+        'console_scripts': [
+            'gpc=gs1_gpc.cli:cli',
+        ],
+    },
+)
