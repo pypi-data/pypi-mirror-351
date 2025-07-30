7b30171b218c126b647bad02a441ad380840df4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:07:01 +0000

    Add package django-remix-icon/1.0.1/django_remix_icon-1.0.1.tar.gz

---
+++ packages/django-remix-icon/django_remix_icon-1.0.1.tar.gz/django_remix_icon-1.0.1/django_remix_icon/__init__.py
@@ -0,0 +1,22 @@
+"""
+Django Remix Icon
+
+A Django package that provides model fields and template tags for integrating Remix Icons
+into Django projects. This package makes it easy to use Remix Icons in your Django models,
+forms, and templates with a beautiful and user-friendly interface.
+
+Features:
+    - Model field for storing Remix Icon identifiers
+    - Searchable icon picker in Django admin
+    - Icon previews in dropdown options
+    - Responsive design
+    - Easy template integration
+    - Type-safe with Python type hints
+    - Zero external dependencies (except Django)
+"""
+
+from typing import Final
+
+__version__: Final[str] = "1.0.1"
+__author__: Final[str] = "Berkay Şen"
+__email__: Final[str] = "brktrl@protonmail.ch"
---
+++ packages/django-remix-icon/django_remix_icon-1.0.1.tar.gz/django_remix_icon-1.0.1/django_remix_icon/defaults.py
@@ -0,0 +1,6 @@
+"""
+Default settings for django-remix-icon.
+"""
+
+# Remix Icon CDN URL
+REMIX_ICON_CDN_URL = "https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.6.0/remixicon.css"
