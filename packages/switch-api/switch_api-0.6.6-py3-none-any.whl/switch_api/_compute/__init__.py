209754d22c10abdb91062787823959af15234be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:59 +0000

    Add package switch-api/0.6.6/switch_api-0.6.6-py3-none-any.whl

---
+++ packages/switch-api/switch_api-0.6.6-py3-none-any.whl/switch_api/_compute/__init__.py
@@ -0,0 +1,12 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Switch Automation Pty Ltd. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+"""
+A module for compute.
+"""
+
+from ._compute import CostCarbonCalculation
+
+__all__ = ['CostCarbonCalculation']
