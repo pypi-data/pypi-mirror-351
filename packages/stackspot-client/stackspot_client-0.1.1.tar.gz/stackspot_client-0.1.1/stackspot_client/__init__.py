9345703e035496102eef9d2a1d6757c420535e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:24 +0000

    Add package stackspot-client/0.1.1/stackspot_client-0.1.1.tar.gz

---
+++ packages/stackspot-client/stackspot_client-0.1.1.tar.gz/stackspot_client-0.1.1/stackspot_client/__init__.py
@@ -0,0 +1,11 @@
+from .client import StackSpotConfig, StackSpotClient, StackSpotError, AuthenticationError, APIError
+
+__version__ = "0.1.1"
+
+__all__ = [
+    'StackSpotConfig',
+    'StackSpotClient',
+    'StackSpotError',
+    'AuthenticationError',
+    'APIError'
+] 
