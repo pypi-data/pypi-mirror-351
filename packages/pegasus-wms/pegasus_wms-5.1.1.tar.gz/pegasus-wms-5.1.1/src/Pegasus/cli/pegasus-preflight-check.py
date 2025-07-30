0f399b60006c1a68bd367a3ebe0586985c399cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:46 +0000

    Add package pegasus-wms/5.1.1/pegasus_wms-5.1.1.tar.gz

---
+++ packages/pegasus-wms/pegasus_wms-5.1.1.tar.gz/pegasus-wms-5.1.1/src/Pegasus/cli/pegasus-preflight-check.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+"""
+Pegasus utility for checking that prereqs are installed
+"""
+
+import sys
+
+success = True
+
+if not sys.version_info >= (3, 6):
+    sys.stderr.write("Pegasus requires Python 3.6 or above\n")
+    sys.exit(1)
+
+try:
+    import yaml  # noqa
+except Exception:
+    sys.stderr.write("Pegasus requires the Python3 YAML module to be installed.\n")
+    sys.stderr.write(
+        "Please install it via your OS package manager, via pip, or activate a venv which includes the module.\n"
+    )
+    success = False
+
+try:
+    import git  # noqa
+except Exception:
+    sys.stderr.write("Pegasus requires the Python3 GitPython module to be installed\n")
+    sys.stderr.write(
+        "Please install it via your OS package manager, via pip, or activate a venv which includes the module.\n"
+    )
+    success = False
+
+try:
+    import dataclasses  # noqa
+except Exception:
+    sys.stderr.write(
+        "Pegasus requires the Python3 dataclasses module to be installed\n"
+    )
+    sys.stderr.write(
+        "Please install it via your OS package manager, via pip, or activate a venv which includes the module.\n"
+    )
+    success = False
+
+if not success:
+    sys.stderr.write(f"The PYTHONPATH is {sys.path}\n\n")
+    sys.exit(1)
