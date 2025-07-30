895d91d1bcb7c262459bd5fb062dbb8636d34535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:22:20 +0000

    Add package jinjazzle/1.2.3/jinjazzle-1.2.3-py2.py3-none-any.whl

---
+++ packages/jinjazzle/jinjazzle-1.2.3-py2.py3-none-any.whl/jinjazzle/ext/__init__.py
@@ -0,0 +1,11 @@
+from .python import PythonExtension
+from .string import StringExtension
+from .uuid import UUIDExtension
+from .version import VersionExtension
+
+__all__ = [
+    "PythonExtension",
+    "StringExtension",
+    "UUIDExtension",
+    "VersionExtension",
+]
