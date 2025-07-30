353e2a4a6d5c55adbc741e05cd6b4dea33f73d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:48 +0000

    Add package norfab/0.8.1/norfab-0.8.1.tar.gz

---
+++ packages/norfab/norfab-0.8.1.tar.gz/norfab-0.8.1/norfab/clients/picle_shells/netbox/netbox_picle_shell_update_device.py
@@ -0,0 +1,208 @@
+import logging
+import json
+import yaml
+
+from picle.models import PipeFunctionsModel, Outputters
+from enum import Enum
+from pydantic import (
+    BaseModel,
+    StrictBool,
+    StrictInt,
+    StrictFloat,
+    StrictStr,
+    conlist,
+    Field,
+)
+from typing import Union, Optional, List, Any, Dict, Callable, Tuple
+from ..common import ClientRunJobArgs, log_error_or_result, listen_events
+from ..nornir.nornir_picle_shell import NornirCommonArgs, NorniHostsFilters
+from .netbox_picle_shell_common import NetboxCommonArgs, NetboxClientRunJobArgs
+
+log = logging.getLogger(__name__)
+
+
+class NornirServiceCommands(NornirCommonArgs):
+    @staticmethod
+    def run(*args, **kwargs):
+        kwargs["via"] = "nornir"
+        return UpdateDeviceInterfacesCommand.run(*args, **kwargs)
+
+    class PicleConfig:
+        outputter = Outputters.outputter_nested
+
+
+class UpdateDatasources(BaseModel):
+    nornir: NornirServiceCommands = Field(
+        None,
+        description="Use Nornir service to retrieve data from devices",
+    )
+
+
+class UpdateDeviceFactsCommand(NetboxCommonArgs, NetboxClientRunJobArgs):
+    dry_run: Optional[StrictBool] = Field(
+        None,
+        description="Return information that would be pushed to Netbox but do not push it",
+        json_schema_extra={"presence": True},
+        alias="dry-run",
+    )
+    devices: Union[List[StrictStr], StrictStr] = Field(
+        None,
+        description="List of Netbox devices to update",
+    )
+    progress: Optional[StrictBool] = Field(
+        True,
+        description="Emit execution progress",
+        json_schema_extra={"presence": True},
+    )
+    batch_size: StrictInt = Field(
+        10, description="Number of devices to process at a time", alias="batch-size"
+    )
+    datasource: UpdateDatasources = Field(
+        "nornir",
+        description="Service to use to retrieve device data",
+    )
+
+    @staticmethod
+    @listen_events
+    def run(uuid, **kwargs):
+        workers = kwargs.pop("workers", "any")
+        timeout = kwargs.pop("timeout", 600)
+        kwargs["timeout"] = timeout * 0.9
+        verbose_result = kwargs.pop("verbose_result", False)
+
+        if isinstance(kwargs.get("devices"), str):
+            kwargs["devices"] = [kwargs["devices"]]
+
+        result = NFCLIENT.run_job(
+            "netbox",
+            "update_device_facts",
+            workers=workers,
+            kwargs=kwargs,
+            timeout=timeout,
+            uuid=uuid,
+        )
+
+        result = log_error_or_result(result, verbose_result=verbose_result)
+
+        return result
+
+    class PicleConfig:
+        outputter = Outputters.outputter_nested
+
+
+class UpdateDeviceInterfacesCommand(NetboxCommonArgs, NetboxClientRunJobArgs):
+    dry_run: Optional[StrictBool] = Field(
+        None,
+        description="Return information that would be pushed to Netbox but do not push it",
+        json_schema_extra={"presence": True},
+        alias="dry-run",
+    )
+    devices: Union[List[StrictStr], StrictStr] = Field(
+        None,
+        description="List of Netbox devices to update",
+    )
+    progress: Optional[StrictBool] = Field(
+        True,
+        description="Emit execution progress",
+        json_schema_extra={"presence": True},
+    )
+    datasource: UpdateDatasources = Field(
+        "nornir",
+        description="Service to use to retrieve device data",
+    )
+    batch_size: StrictInt = Field(
+        10, description="Number of devices to process at a time", alias="batch-size"
+    )
+
+    @staticmethod
+    @listen_events
+    def run(uuid, **kwargs):
+        workers = kwargs.pop("workers", "any")
+        timeout = kwargs.pop("timeout", 600)
+        kwargs["timeout"] = timeout * 0.9
+        verbose_result = kwargs.pop("verbose_result", False)
+
+        if isinstance(kwargs.get("devices"), str):
+            kwargs["devices"] = [kwargs["devices"]]
+
+        result = NFCLIENT.run_job(
+            "netbox",
+            "update_device_interfaces",
+            workers=workers,
+            kwargs=kwargs,
+            timeout=timeout,
+            uuid=uuid,
+        )
+
+        result = log_error_or_result(result, verbose_result=verbose_result)
+
+        return result
+
+    class PicleConfig:
+        outputter = Outputters.outputter_nested
+
+
+class UpdateDeviceIPAddressesCommand(NetboxCommonArgs, NetboxClientRunJobArgs):
+    dry_run: Optional[StrictBool] = Field(
+        None,
+        description="Return information that would be pushed to Netbox but do not push it",
+        json_schema_extra={"presence": True},
+        alias="dry-run",
+    )
+    devices: Union[List[StrictStr], StrictStr] = Field(
+        None,
+        description="List of Netbox devices to update",
+    )
+    progress: Optional[StrictBool] = Field(
+        True,
+        description="Emit execution progress",
+        json_schema_extra={"presence": True},
+    )
+    datasource: UpdateDatasources = Field(
+        "nornir",
+        description="Service to use to retrieve device data",
+    )
+    batch_size: StrictInt = Field(
+        10, description="Number of devices to process at a time", alias="batch-size"
+    )
+
+    @staticmethod
+    @listen_events
+    def run(uuid, **kwargs):
+        workers = kwargs.pop("workers", "any")
+        timeout = kwargs.pop("timeout", 600)
+        kwargs["timeout"] = timeout * 0.9
+        verbose_result = kwargs.pop("verbose_result", False)
+
+        if isinstance(kwargs.get("devices"), str):
+            kwargs["devices"] = [kwargs["devices"]]
+
+        result = NFCLIENT.run_job(
+            "netbox",
+            "update_device_ip",
+            workers=workers,
+            kwargs=kwargs,
+            timeout=timeout,
+            uuid=uuid,
+        )
+
+        result = log_error_or_result(result, verbose_result=verbose_result)
+
+        return result
+
+    class PicleConfig:
+        outputter = Outputters.outputter_nested
+
+
+class UpdateDeviceCommands(BaseModel):
+    facts: UpdateDeviceFactsCommand = Field(
+        None,
+        description="Update device serial, OS version",
+    )
+    interfaces: UpdateDeviceInterfacesCommand = Field(
+        None,
+        description="Update device interfaces",
+    )
+    ip_addresses: UpdateDeviceIPAddressesCommand = Field(
+        None, description="Update device interface IP addresses", alias="ip-addresses"
+    )
