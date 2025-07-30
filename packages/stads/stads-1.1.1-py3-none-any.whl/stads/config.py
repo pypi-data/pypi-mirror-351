f223a9188e250b3f7006715c3ebdfa67f054eb00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:07:45 +0000

    Add package stads/1.1.1/stads-1.1.1-py3-none-any.whl

---
+++ packages/stads/stads-1.1.1-py3-none-any.whl/stads/config.py
@@ -0,0 +1,16 @@
+import os
+
+from .read_images import get_frames_from_mp4
+
+# Get absolute path to the root of your project
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+# Path to the hardcoded video file
+VIDEO_PATH = os.path.join(PROJECT_ROOT, "data","ground_truth", "dendrites_one.mp4")
+
+# Now load the reference frames
+SIGMA = 4
+KERNEL_SIZE = int(SIGMA * 3) + 1
+
+# Get the video file path using importlib.resources
+REFERENCE_VIDEO_SEQUENCE = get_frames_from_mp4(VIDEO_PATH, 100)
