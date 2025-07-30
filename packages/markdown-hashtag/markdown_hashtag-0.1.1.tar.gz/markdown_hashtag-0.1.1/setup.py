a050ddd7664e770744989125b7ed84ac32ff5d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:57 +0000

    Add package markdown-hashtag/0.1.1/markdown_hashtag-0.1.1.tar.gz

---
+++ packages/markdown-hashtag/markdown_hashtag-0.1.1.tar.gz/markdown_hashtag-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name='markdown-hashtag',
+    version='0.1.1',
+    description='Extension for Python-Markdown to parse hashtag (e.g. #Python) into a user-defined URL',
+    long_description=long_description,
+    long_description_content_type="text/markdown", 
+    author='Vinzenz Becke-Stauner (Becksta)',
+    author_email='info@becke-stauner.de',
+    url='https://github.com/becksta-dev/markdown-hashtag', 
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'markdown>=3.0',
+    ],
+    entry_points={
+        'markdown.extensions': [
+            'hashtag = markdown_hashtag:makeExtension',
+        ],
+    },
+    license='GPLv3',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+)
