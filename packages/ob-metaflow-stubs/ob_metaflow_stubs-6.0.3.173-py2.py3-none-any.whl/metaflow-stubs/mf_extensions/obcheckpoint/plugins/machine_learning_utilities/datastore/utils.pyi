0f7e4bbd7d1e0a22314cf68f4c74c835165e07b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:10 +0000

    Add package ob-metaflow-stubs/6.0.3.173/ob_metaflow_stubs-6.0.3.173-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.173-py2.py3-none-any.whl/metaflow-stubs/mf_extensions/obcheckpoint/plugins/machine_learning_utilities/datastore/utils.pyi
@@ -0,0 +1,19 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.14.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-05-29T17:52:46.165949                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+
+def is_json_serializable(value):
+    """
+    Check if value is JSON serializable.
+    """
+    ...
+
+def safe_serialize(data):
+    ...
+
