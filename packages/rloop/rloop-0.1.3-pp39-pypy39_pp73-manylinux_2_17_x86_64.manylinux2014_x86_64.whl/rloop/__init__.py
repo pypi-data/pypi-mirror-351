dff41aa04a37533d9454e495fe77e0d1d8c0fa4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:06:15 +0000

    Add package rloop/0.1.3/rloop-0.1.3-pp39-pypy39_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/rloop/rloop-0.1.3-pp39-pypy39_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/rloop/__init__.py
@@ -0,0 +1,13 @@
+from asyncio.events import BaseDefaultEventLoopPolicy as __BasePolicy
+
+from ._rloop import __version__ as __version__
+from .loop import RLoop
+
+
+def new_event_loop() -> RLoop:
+    return RLoop()
+
+
+class EventLoopPolicy(__BasePolicy):
+    def _loop_factory(self) -> RLoop:
+        return new_event_loop()
