db82f472a3758d2579657499174c58788bd8339f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:42 +0000

    Add package opttech/1.0.2/opttech-1.0.2-py3-none-any.whl

---
+++ packages/opttech/opttech-1.0.2-py3-none-any.whl/opttech/__init__.py
@@ -0,0 +1,10 @@
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pacote ainda não instalado
+    __version__ = "0.0.0.dev0"
+
+from .arquivos import map_files
+
+__all__ = ["map_files", "__version__"]
