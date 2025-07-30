d97650d9085d96191dbdf89f2fbbf58c42db1835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:46:15 +0000

    Add package nsk/2.2.4/nsk-2.2.4.tar.gz

---
+++ packages/nsk/nsk-2.2.4.tar.gz/nsk-2.2.4/NSK/bot/__init__.py
@@ -0,0 +1,6 @@
+from .dandoli import Dandoli
+from .mail_dealer import MailDealer
+from .share_point import SharePoint
+from .web_access import WebAccess
+
+__all__ = ['SharePoint','WebAccess','MailDealer','Dandoli']
