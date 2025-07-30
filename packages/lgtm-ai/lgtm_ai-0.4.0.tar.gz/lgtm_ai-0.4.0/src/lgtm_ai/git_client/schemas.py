a808be13e2c38eed2b9546de587b84649bfc888e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:14 +0000

    Add package lgtm-ai/0.4.0/lgtm_ai-0.4.0.tar.gz

---
+++ packages/lgtm-ai/lgtm_ai-0.4.0.tar.gz/lgtm_ai-0.4.0/src/lgtm_ai/git_client/schemas.py
@@ -0,0 +1,35 @@
+from groq import BaseModel
+from lgtm_ai.git_parser.parser import DiffResult
+
+
+class PRDiff(BaseModel):
+    id: int
+    diff: list[DiffResult]
+    changed_files: list[str]
+    target_branch: str
+    source_branch: str
+
+
+class PRContextFileContents(BaseModel):
+    file_path: str
+    content: str
+
+
+class PRContext(BaseModel):
+    """Represents the context a reviewer might need when reviewing PRs.
+
+    At the moment, it is just the contents of the files that are changed in the PR.
+    """
+
+    file_contents: list[PRContextFileContents]
+
+    def __bool__(self) -> bool:
+        return bool(self.file_contents)
+
+    def add_file(self, file_path: str, content: str) -> None:
+        self.file_contents.append(PRContextFileContents(file_path=file_path, content=content))
+
+
+class PRMetadata(BaseModel):
+    title: str
+    description: str
