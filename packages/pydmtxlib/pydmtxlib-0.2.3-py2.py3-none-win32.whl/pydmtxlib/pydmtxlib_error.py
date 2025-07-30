9850a40ba75136e2b26751f7d6c53750ffc20d43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:19 +0000

    Add package pydmtxlib/0.2.3/pydmtxlib-0.2.3-py2.py3-none-win32.whl

---
+++ packages/pydmtxlib/pydmtxlib-0.2.3-py2.py3-none-win32.whl/pydmtxlib/pydmtxlib_error.py
@@ -0,0 +1,10 @@
+"""
+This module defines a custom error class for the pydmtxlib library.
+"""
+
+
+class PydmtxlibError(Exception):
+    """
+    This is a generic error class used to signal issues encountered while
+    interacting with the pydmtxlib library through the Python wrapper.
+    """
