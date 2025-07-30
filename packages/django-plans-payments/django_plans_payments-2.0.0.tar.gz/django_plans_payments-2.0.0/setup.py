3963f8067cf27a454412cf1e07a053da8b2aa363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:44 +0000

    Add package django-plans-payments/2.0.0/django_plans_payments-2.0.0.tar.gz

---
+++ packages/django-plans-payments/django_plans_payments-2.0.0.tar.gz/django_plans_payments-2.0.0/setup.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import re
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def get_version(*file_paths):
+    """Retrieves the version from plans_payments/__init__.py"""
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = open(filename).read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+version = get_version("plans_payments", "__init__.py")
+
+
+if sys.argv[-1] == "publish":
+    try:
+        import wheel
+
+        print("Wheel version: ", wheel.__version__)
+    except ImportError:
+        print('Wheel library missing. Please run "pip install wheel"')
+        sys.exit()
+    os.system("python setup.py sdist upload")
+    os.system("python setup.py bdist_wheel upload")
+    sys.exit()
+
+if sys.argv[-1] == "tag":
+    print("Tagging the version on git:")
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
+    sys.exit()
+
+readme = open("README.rst").read()
+history = open("HISTORY.rst").read().replace(".. :changelog:", "")
+
+setup(
+    name="django-plans-payments",
+    version=version,
+    description="""Integration between django-plans and django-payments.""",
+    long_description=readme + "\n\n" + history,
+    author="Petr Dlouhý",
+    author_email="petr.dlouhy@email.cz",
+    url="https://github.com/PetrDlouhy/django-plans-payments",
+    packages=[
+        "plans_payments",
+    ],
+    include_package_data=True,
+    install_requires=[
+        "django-plans>=1.1.0",
+        "django-payments",
+    ],
+    license="MIT",
+    zip_safe=False,
+    keywords="django-plans-payments",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
+        "Framework :: Django :: 5.0",
+        "Framework :: Django :: 5.1",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+)
