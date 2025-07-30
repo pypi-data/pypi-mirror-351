0f4e41afbea1e1b159dc6a99d41e3c57b2854446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:44 +0000

    Add package rockstore/0.1.1/rockstore-0.1.1-py3-none-any.whl

---
+++ packages/rockstore/rockstore-0.1.1-py3-none-any.whl/rockstore/__init__.py
@@ -0,0 +1,9 @@
+"""
+A Pythonic wrapper for RocksDB using CFFI.
+"""
+
+from .store import RockStore
+from .context import open_database
+
+__version__ = "0.1.1"
+__all__ = ["RockStore", "open_database"] 
