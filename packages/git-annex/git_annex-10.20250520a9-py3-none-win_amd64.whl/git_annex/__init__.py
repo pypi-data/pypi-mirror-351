68939040909ea8cb03956ac2c50d47b1eb8ae736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:21 +0000

    Add package git-annex/10.20250520a9/git_annex-10.20250520a9-py3-none-win_amd64.whl

---
+++ packages/git-annex/git_annex-10.20250520a9-py3-none-win_amd64.whl/git_annex/__init__.py
@@ -0,0 +1,30 @@
+import os.path as op
+import subprocess
+import sys
+
+
+def cli():
+    exe_dir = op.dirname(__file__)
+    exe = op.join(
+        exe_dir,
+        op.basename(sys.argv[0]),
+    )
+    args = [exe] + sys.argv[1:]
+    try:
+        subprocess.run(
+            args,
+            executable=op.join(exe_dir, 'git-annex'),
+            shell=False,
+            check=True,
+        )
+        # try flush here to trigger a BrokenPipeError
+        # within the try-except block so we can handle it
+        # (happens if the calling process closed stdout
+        # already
+        sys.stdout.flush()
+    except (BrokenPipeError, IOError):
+        # setting it to None prevents Python from trying to
+        # flush again
+        sys.stdout = None
+    except subprocess.CalledProcessError as e:
+        sys.exit(e.returncode)
