9cb470feda80c00ff4c36e94199ce0b0dc73dc47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:31 +0000

    Add package recordsql/0.1.4/recordsql-0.1.4-py3-none-any.whl

---
+++ packages/recordsql/recordsql-0.1.4-py3-none-any.whl/recordsQL/__init__.py
@@ -0,0 +1,33 @@
+from .query import (
+    SELECT, WITH, SelectQuery, WithQuery, JoinQuery,
+    UPDATE, UpdateQuery,
+    DELETE, DeleteQuery,
+    INSERT, InsertQuery, OnConflictQuery,
+    COUNT, CountQuery,
+    EXISTS, ExistsQuery
+)
+from .types import (
+    SQLCol,
+    SQLInput,
+    SQLOrderBy
+)
+
+from .dependencies import cols, col, text, set_expr, num
+__all__ = [
+    "SELECT",
+    "WITH",
+    "SelectQuery",
+    "WithQuery",
+    "JoinQuery",
+    "InsertQuery",
+    "INSERT",
+    "OnConflictQuery",
+    "UpdateQuery",
+    "UPDATE",
+    "DeleteQuery",
+    "DELETE",
+    "CountQuery",
+    "COUNT",
+    "ExistsQuery",
+    "EXISTS"
+]
