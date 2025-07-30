28782422401d37ff7d0fc7e1a5f1f9d24e52293d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:43 +0000

    Add package tscluster/1.0.8/tscluster-1.0.8.tar.gz

---
+++ packages/tscluster/tscluster-1.0.8.tar.gz/tscluster-1.0.8/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='tscluster',
+   version='1.0.8',
+   description='A useful package for temporal clustering',
+   license="MIT",
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   author='Jolomi Tosanwumi, Jiazhou Liang',
+   author_email='tjolomi@gmail.com, joe.liang@mail.utoronto.ca',
+   #url="...",
+   packages=find_packages(), 
+   #python_requires='>=3.9.18',
+   install_requires=[
+       'numpy>=1.26', 
+       'scipy>=1.10', 
+       'gurobipy>=11.0', 
+       'tslearn>=0.6.3',   
+       'h5py>=3.10',
+       'pandas>=2.2',
+       'matplotlib>=3.8,<3.9'
+       ], #external packages as dependencies on TEST pypi
+   #ToDo: on real pypi: ['numpy==1.26.4', 'scipy==1.12.0', 'gurobipy==11.0.1']. Also considering using >= a lowerbound
+)
