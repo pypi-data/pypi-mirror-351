71e4c54f709ad69089357d8b93ac5583bff6b392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:04 +0000

    Add package rapidframework-lib/1.0.7.1/rapidframework_lib-1.0.7.1-py3-none-any.whl

---
+++ packages/rapidframework-lib/rapidframework_lib-1.0.7.1-py3-none-any.whl/rapidframework/frameworks/__init__.py
@@ -0,0 +1,6 @@
+from .django import DjangoManager as DjangoManager
+from .flask import FlaskManager as FlaskManager
+from .fastapi import FastapiManager as FastapiManager
+from .template import Template as Template
+
+from .examples import *
