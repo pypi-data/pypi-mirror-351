59a6ddf2414d2394ab359e57ebab556ee98cb401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:31 +0000

    Add package pygeai/0.2.7b22/pygeai-0.2.7b22.tar.gz

---
+++ packages/pygeai/pygeai-0.2.7b22.tar.gz/pygeai-0.2.7b22/pygeai/gam/endpoints.py
@@ -0,0 +1,3 @@
+GET_ACCESS_TOKEN_V2 = "oauth/gam/v2.0/access_token"     # POST -> Sends the user credentials to get a new Token
+GET_USER_INFO_V2 = "oauth/gam/v2.0/userinfo"            # GET -> Send the access_token obtained in the previous request and get the user info depending on the scopes you have indicated.
+GET_AUTHENTICATION_TYPES_V1 = "v1/gam/authentication-types"     # GET -> Get authentication types
