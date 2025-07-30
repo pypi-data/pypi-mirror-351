94bd9bd4f154ddf712b2ab76b510507a6b7ad159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:45 +0000

    Add package shadowshell/0.0.39/shadowshell-0.0.39.tar.gz

---
+++ packages/shadowshell/shadowshell-0.0.39.tar.gz/shadowshell-0.0.39/src/shadowshell/logging/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@author: shadowshell
+"""
+
+from shadowshell.logging.logger import Logger
+from shadowshell.logging.logger_factory import LoggerFactory
+from shadowshell.logging.console_logger import ConsoleLogger
+
+__all__ = ['Logger', 'LoggerFactory', 'ConsoleLogger']
