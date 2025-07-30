dfdb420005a18b82518d9404e3320cb86a460eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:10 +0000

    Add package ren-cli/0.1.2/ren_cli-0.1.2-py3-none-any.whl

---
+++ packages/ren-cli/ren_cli-0.1.2-py3-none-any.whl/ren/formatter.py
@@ -0,0 +1,16 @@
+from .logging import log
+
+try:
+    import gallery_dl.formatter as formatter
+
+    def format_impl(format_str: str, *args, **kwargs: dict[str, str | int]):
+        if '{}' in format_str:
+            # TODO: handle {} with slices, other operations?
+            log.warning('Replacing {} with {fullname} for gallery_dl compat')
+            format_str = format_str.replace('{}', '{fullname}')
+        fmt: formatter.StringFormatter = formatter.parse(format_str)
+        return fmt.format_map(kwargs)
+
+except ImportError:
+    def format_impl(format_str: str, *args, **kwargs: dict[str, str | int]):
+        return format_str.format(*args, **kwargs)
