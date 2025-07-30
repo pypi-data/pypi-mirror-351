04efc414f08f9e7d4e4943e41c1bf1d2cd479994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:20 +0000

    Add package kiba-core/0.5.3.dev33/kiba_core-0.5.3.dev33.tar.gz

---
+++ packages/kiba-core/kiba_core-0.5.3.dev33.tar.gz/kiba_core-0.5.3.dev33/core/util/chain_util.py
@@ -0,0 +1,14 @@
+import eth_utils
+from web3 import Web3
+from web3.types import ChecksumAddress
+
+BURN_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+
+def normalize_address(value: str) -> str:
+    return str(normalize_address_checksum(value=value))
+
+
+def normalize_address_checksum(value: str) -> ChecksumAddress:
+    addressBytes = eth_utils.to_bytes(hexstr=value)
+    return Web3.to_checksum_address(addressBytes[-20:])
