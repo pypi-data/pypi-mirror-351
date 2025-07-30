b22931609e1315066bd788422963627d0f0446d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:05 +0000

    Add package meraki-cli/1.5.1/meraki_cli-1.5.1.tar.gz

---
+++ packages/meraki-cli/meraki_cli-1.5.1.tar.gz/meraki_cli-1.5.1/README.md
@@ -0,0 +1,49 @@
+# Meraki-CLI <img align="right" width="200" height="200" src="icon.png">
+
+
+[![Build Status](https://github.com/PackeTsar/meraki-cli/actions/workflows/python-package.yml/badge.svg)](https://github.com/PackeTsar/meraki-cli/actions/workflows/python-package.yml)
+[![Coverage](https://codecov.io/gh/PackeTsar/meraki-cli/branch/master/graph/badge.svg)](https://codecov.io/gh/PackeTsar/meraki-cli)
+[![PyPI](https://img.shields.io/pypi/v/meraki-cli.svg)](https://pypi.python.org/pypi/meraki-cli)
+[![Python Versions](https://img.shields.io/pypi/pyversions/meraki-cli.svg)](https://pypi.python.org/pypi/meraki-cli)
+[![Cisco DevNet](https://static.production.devnetcloud.com/codeexchange/assets/images/devnet-published.svg)](https://developer.cisco.com/codeexchange/github/repo/PackeTsar/meraki-cli)
+
+A simple CLI tool to automate and control your Cisco Meraki Dashboard!
+
+Quick Install: `pip3 install meraki-cli`
+
+Update/Upgrade Meraki-CLI: `meraki upgrade --upgrade-all`
+
+Try Meraki-CLI: [Try It Out](https://meraki-cli.readthedocs.io/en/latest/try-it-out/)
+
+**Full Documentation**: **[Install Meraki-CLI](https://meraki-cli.readthedocs.io/en/latest/)**
+
+Quick Command Cheat Sheet: [Meraki-CLI Cheat Sheet](CHEATSHEET.md)
+
+Full Command Guide: [Meraki-CLI Command Guide](COMMAND_GUIDE.md)
+
+---
+
+## Documentation
+
+Visit the **[Meraki-CLI Documentation](https://meraki-cli.readthedocs.io/en/latest/)** for installation and usage insructions.
+
+[![documentation](docs_screenshot.png)](https://meraki-cli.readthedocs.io/en/latest/)
+
+---
+
+## Version
+The version of Meraki-CLI documented here is: **1.5.1**
+
+Version History: [Change Log](CHANGELOG.md)
+
+---
+
+## Contributing
+
+This project is very new and has been created out of need. If you have a feature you would like to see built into it, please open up an issue in Github and describe your desired feature. Any accepted feature requests will be listed in the [Enhancement Requests and Known Bugs](https://github.com/PackeTsar/meraki-cli/issues/2) issue page.
+
+If you find a need for a feature and you add it in yourself, or you fix a bug you found, please feel free to open up a merge request!
+
+If neither of those options work for you, and you just want to support this project, you can buy me a coffee :grin:
+
+<a href="https://www.buymeacoffee.com/packetsar" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174"></a>
---
+++ packages/meraki-cli/meraki_cli-1.5.1.tar.gz/meraki_cli-1.5.1/setup.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+
+"""
+PIP installation configuration file
+"""
+
+
+import os
+import sys
+from setuptools import setup
+from setuptools import find_packages
+
+# Find local file path
+localdir = os.path.split(os.path.abspath(__file__))[0]
+
+
+# Using a function to make the damn linter happy
+def version():
+    project_dir = os.path.join(localdir, "meraki_cli")
+    sys.path = [project_dir] + sys.path
+    import __version__
+    return __version__.version
+
+
+with open(os.path.join(
+        localdir, "README.md"), "r", encoding='utf-8') as readme:
+    long_description = readme.read()
+    readme.close()
+
+
+with open(os.path.join(
+        localdir, "requirements.txt"), "r", encoding='utf-8') as req_file:
+    install_requires = []
+    for package in req_file.read().split("\n"):
+        if package:
+            install_requires.append(package)
+    req_file.close()
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Console',
+    'Intended Audience :: Information Technology',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: MacOS',
+    'Operating System :: POSIX',
+    'Operating System :: Microsoft',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+    'Programming Language :: Python :: 3.13',
+    'Topic :: Internet',
+    'Topic :: Utilities',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: System',
+    'Topic :: System :: Networking']
+
+
+setup(name='meraki_cli',
+      version=version(),
+      description='A Simple CLI tool to automate and control your '
+      'Meraki Dashboard',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='John W Kerns',
+      classifiers=CLASSIFIERS,
+      author_email='jkerns@packetsar.com',
+      url='https://github.com/PackeTsar/meraki-cli',
+      license="GNU",
+      packages=find_packages(),
+      install_requires=install_requires,
+      extras_require={'dev': ['jinja2', 'pytest', 'coverage', 'flake8',
+                              'requests-mock', 'tox', 'mkdocs',
+                              'mkdocs-material']},
+      entry_points={
+          'console_scripts': [
+              'meraki = meraki_cli.__main__:main'
+          ]
+      }
+      )
