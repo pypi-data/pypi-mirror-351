9b15f4d1f5b2f21635de649b1e13e3d7df5cd516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:59 +0000

    Add package turbopuffer/0.5.0a1/turbopuffer-0.5.0a1-py3-none-any.whl

---
+++ packages/turbopuffer/turbopuffer-0.5.0a1-py3-none-any.whl/turbopuffer/types/full_text_search_config_param.py
@@ -0,0 +1,36 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import TypedDict
+
+from .language import Language
+from .tokenizer import Tokenizer
+
+__all__ = ["FullTextSearchConfigParam"]
+
+
+class FullTextSearchConfigParam(TypedDict, total=False):
+    case_sensitive: bool
+    """Whether searching is case-sensitive.
+
+    Defaults to `false` (i.e. case-insensitive).
+    """
+
+    language: Language
+    """Describes the language of a text attribute. Defaults to `english`."""
+
+    remove_stopwords: bool
+    """Removes common words from the text based on language.
+
+    Defaults to `true` (i.e. remove common words).
+    """
+
+    stemming: bool
+    """Language-specific stemming for the text.
+
+    Defaults to `false` (i.e., do not stem).
+    """
+
+    tokenizer: Tokenizer
+    """The tokenizer to use for full-text search on an attribute."""
