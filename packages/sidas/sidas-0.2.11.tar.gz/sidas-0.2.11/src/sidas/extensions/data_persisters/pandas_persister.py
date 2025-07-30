bba09582adc2ea4f92444bd7dca618324c91dbe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:21 +0000

    Add package sidas/0.2.11/sidas-0.2.11.tar.gz

---
+++ packages/sidas/sidas-0.2.11.tar.gz/sidas-0.2.11/src/sidas/extensions/data_persisters/pandas_persister.py
@@ -0,0 +1,223 @@
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Any, Literal, Type
+
+import pandas as pd
+import polars as pl
+
+from ...core import (
+    AssetDataFailedToPersist,
+    AssetDataFailedToRetrieve,
+    DataPersistableProtocol,
+    DataPersister,
+)
+from ..resources.databases import DatabaseResource
+from ..resources.folder import FolderResource
+
+if TYPE_CHECKING:
+    from polars._typing import SchemaDict  # type:ignore
+
+PandasPersistable = DataPersistableProtocol[pd.DataFrame]
+
+
+@dataclass
+class PandasPersisterFolderResource:
+    folder: FolderResource
+    format: Literal["csv", "parquet", "json", "ndjson"] = "ndjson"
+
+    def save(self, asset: PandasPersistable) -> None:
+        path = asset.asset_id().as_path(suffix=self.format)
+        data = pl.from_dataframe(asset.data)
+        match self.format:
+            case "csv":
+                with self.folder.open(path, "w") as f:
+                    data.write_csv(f, separator=";")
+
+            case "parquet":
+                with self.folder.open(path, "wb") as f:
+                    data.write_parquet(f)
+
+            case "json":
+                with self.folder.open(path, "w") as f:
+                    data.write_json(f)
+
+            case "ndjson":
+                with self.folder.open(path, "w") as f:
+                    data.write_ndjson(f)
+
+    def load(self, asset: PandasPersistable) -> None:
+        path = asset.asset_id().as_path(suffix=self.format)
+        schema: SchemaDict = asset.schema if hasattr(asset, "schema") else None  # type: ignore
+        columns = list(schema.keys()) if schema else None  # type: ignore
+
+        match self.format:
+            case "csv":
+                with self.folder.open(path, "r") as f:
+                    data = pl.read_csv(
+                        f, separator=";", schema=schema, truncate_ragged_lines=True
+                    )
+
+            case "parquet":
+                with self.folder.open(path, "rb") as f:
+                    data = pl.read_parquet(
+                        f, schema=schema, allow_missing_columns=True, columns=columns
+                    )
+
+            case "json":
+                with self.folder.open(path, "r") as f:
+                    data = pl.read_json(f, schema=schema)
+
+            case "ndjson":
+                with self.folder.open(path, "r") as f:
+                    data = pl.read_ndjson(f, schema=schema)
+
+        asset.data = data.to_pandas()
+
+
+@dataclass
+class PandasPersisterDBResource:
+    db: DatabaseResource
+    if_table_exists: Literal["append", "replace", "fail"] = "replace"
+    batch: int | None = None
+
+    def save(self, asset: PandasPersistable) -> None:
+        name = asset.asset_id().as_path().name
+        data = pl.from_dataframe(asset.data)
+        chunks = [data]
+        if self.batch:
+            chunks = [
+                data[i : i + self.batch] for i in range(0, data.height, self.batch)
+            ]
+
+        with self.db.get_connection() as con:
+            first_batch = True
+            for chunk in chunks:
+                if first_batch:
+                    chunk.write_database(
+                        name, con, if_table_exists=self.if_table_exists
+                    )
+                    first_batch = False
+                else:
+                    chunk.write_database(name, con, if_table_exists="append")
+
+        with self.db.get_connection() as con:
+            data.write_database(name, con, if_table_exists=self.if_table_exists)
+
+    def load(self, asset: PandasPersistable) -> None:
+        name = asset.asset_id().as_path().name
+        schema: SchemaDict = asset.schema if hasattr(asset, "schema") else None  # type: ignore
+        columns = list(schema.keys()) if schema else None  # type: ignore
+
+        query = f'select * from "{name}";'
+        with self.db.get_connection() as con:
+            data = pl.read_database(query, con, schema_overrides=schema)
+
+        if columns:
+            for c in data.columns:
+                if c not in columns:
+                    data = data.drop(c)
+
+            for c in columns:
+                if c not in data.columns:
+                    data = data.with_columns(pl.lit(None).alias(c))
+
+        asset.data = data.to_pandas()
+
+
+# @dataclass
+# class PandasPersisterFolderResource:
+#     file: FolderResource
+#     format: Literal["csv", "parquet", "json", "ndjson"] = "ndjson"
+
+#     def save(self, asset: PandasPersistable) -> None:
+#         path = asset.asset_id().as_path(suffix=self.format)
+
+#         match self.format:
+#             case "csv":
+#                 with self.folder.open(path, "w") as f:
+#                     asset.data.to_csv(f, sep=";")
+
+#             case "parquet":
+#                 with self.folder.open(path, "wb") as f:
+#                     asset.data.to_parquet(f)
+
+#             case "json":
+#                 with self.folder.open(path, "w") as f:
+#                     asset.data.to_json(f, orient="records")
+
+#             case "ndjson":
+#                 with self.folder.open(path, "w") as f:
+#                     asset.data.to_json(f, orient="records", lines=True)
+
+#     def load(self, asset: PandasPersistable) -> None:
+#         path = asset.asset_id().as_path(suffix=self.format)
+
+#         match self.format:
+#             case "csv":
+#                 with self.folder.open(path, "r") as f:
+#                     asset.data = pd.read_csv(f, sep=";")
+
+#             case "parquet":
+#                 with self.folder.open(path, "rb") as f:
+#                     asset.data = pd.read_parquet(f)
+
+#             case "json":
+#                 with self.folder.open(path, "r") as f:
+#                     asset.data = pd.read_json(f, orient="records")
+
+#             case "ndjson":
+#                 with self.folder.open(path, "r") as f:
+#                     asset.data = pd.read_json(f, orient="records", lines=True)
+
+
+# @dataclass
+# class PandasPersisterDBResource:
+#     db: DatabaseResource
+#     if_table_exists: Literal["append", "replace", "fail"] = "replace"
+#     batch: int | None = None
+
+#     def save(self, asset: PandasPersistable) -> None:
+#         name = asset.asset_id().as_path().name
+#         with self.db.get_connection() as con:
+#             asset.data.to_sql(
+#                 name,
+#                 con,
+#                 if_exists=self.if_table_exists,
+#                 index=False,
+#                 chunksize=self.batch,
+#             )
+
+#     def load(self, asset: PandasPersistable) -> None:
+#         name = asset.asset_id().as_path().name
+#         with self.db.get_connection() as con:
+#             asset.data = pd.read_sql_table(name, con)
+
+
+PandasPersisterResource = PandasPersisterFolderResource | PandasPersisterDBResource
+
+
+class PandasPersister(DataPersister):
+    """
+    The InMemoryDataPersister provides functionality to register, load, save,
+    and directly set data for assets, using an in-memory dictionary to store the data.
+    """
+
+    def __init__(self, resource: PandasPersisterResource) -> None:
+        self.resource = resource
+
+    def register(
+        self, *asset: PandasPersistable | Type[PandasPersistable], **kwargs: Any
+    ) -> None:
+        for a in asset:
+            self.patch_asset(a)
+
+    def save(self, asset: PandasPersistable) -> None:
+        try:
+            self.resource.save(asset)
+        except Exception as e:
+            raise AssetDataFailedToPersist(asset.asset_id(), e) from e
+
+    def load(self, asset: PandasPersistable) -> None:
+        try:
+            self.resource.load(asset)
+        except Exception as e:
+            raise AssetDataFailedToRetrieve(asset.asset_id(), e) from e
---
+++ packages/sidas/sidas-0.2.11.tar.gz/sidas-0.2.11/src/sidas/extensions/resources/folder.py
@@ -0,0 +1,155 @@
+import io
+from contextlib import contextmanager
+from pathlib import Path, PurePath
+from typing import Iterator, Literal, Protocol, overload, runtime_checkable
+
+import smart_open as so  # type: ignore
+
+from .aws import AwsAccount
+
+FolderResourceModeStr = Literal["r"] | Literal["w"]
+FolderResourceModeBin = Literal["rb"] | Literal["wb"]
+FolderResourceMode = FolderResourceModeStr | FolderResourceModeBin
+
+
+@runtime_checkable
+class FolderResource(Protocol):
+    def full_path(self, path: PurePath) -> Path: ...
+
+    @overload
+    @contextmanager
+    def open(
+        self, path: PurePath, mode: FolderResourceModeStr = "r"
+    ) -> Iterator[io.TextIOWrapper]: ...
+
+    @overload
+    @contextmanager
+    def open(
+        self, path: PurePath, mode: FolderResourceModeBin = "rb"
+    ) -> Iterator[io.FileIO]: ...
+
+    @contextmanager
+    def open(
+        self, path: PurePath, mode: FolderResourceMode = "r"
+    ) -> Iterator[io.TextIOWrapper | io.FileIO]: ...
+
+    def exists(self, path: PurePath) -> bool: ...
+
+
+class InMemoryFolder(FolderResource):
+    def __init__(self) -> None:
+        self._files: dict[PurePath, io.TextIOWrapper] = {}
+
+    def full_path(self, path: PurePath) -> Path:
+        return Path(path)
+
+    @overload
+    @contextmanager
+    def open(
+        self, path: PurePath, mode: FolderResourceModeStr = "r"
+    ) -> Iterator[io.TextIOWrapper]: ...
+
+    @overload
+    @contextmanager
+    def open(
+        self, path: PurePath, mode: FolderResourceModeBin = "rb"
+    ) -> Iterator[io.FileIO]: ...
+
+    @contextmanager
+    def open(
+        self, path: PurePath, mode: FolderResourceMode = "r"
+    ) -> Iterator[io.TextIOWrapper | io.FileIO]:
+        if path not in self._files:
+            self._files[path] = io.TextIOWrapper(
+                io.BytesIO(), encoding="utf-8", line_buffering=True
+            )
+
+        try:
+            wrapper = self._files[path]
+            wrapper.seek(0, 0)
+            yield wrapper
+
+        finally:
+            pass
+
+    def exists(self, path: PurePath) -> bool:
+        return path in self._files.keys()
+
+
+class LocalFolder(FolderResource):
+    def __init__(self, base_path: str | Path) -> None:
+        self.base_path = Path(base_path)
+
+    def full_path(self, path: PurePath) -> Path:
+        return Path(self.base_path, path)
+
+    @overload
+    @contextmanager
+    def open(
+        self, path: PurePath, mode: FolderResourceModeStr = "r"
+    ) -> Iterator[io.TextIOWrapper]: ...
+
+    @overload
+    @contextmanager
+    def open(
+        self, path: PurePath, mode: FolderResourceModeBin = "rb"
+    ) -> Iterator[io.FileIO]: ...
+
+    @contextmanager
+    def open(
+        self, path: PurePath, mode: FolderResourceMode = "r"
+    ) -> Iterator[io.TextIOWrapper | io.FileIO]:
+        full_path = self.full_path(path)
+        full_path.parent.mkdir(parents=True, exist_ok=True)
+        yield so.open(full_path, mode)
+
+    def exists(self, path: PurePath) -> bool:
+        return self.full_path(path).is_file()
+
+
+class S3Bucket(FolderResource):
+    def __init__(
+        self,
+        account: AwsAccount,
+        bucket: str,
+    ) -> None:
+        self.account = account
+        self.bucket = bucket
+
+    def full_path(self, path: PurePath) -> Path:
+        return Path("s3://", self.bucket, path)
+
+    @overload
+    @contextmanager
+    def open(
+        self, path: PurePath, mode: FolderResourceModeStr = "r"
+    ) -> Iterator[io.TextIOWrapper]: ...
+
+    @overload
+    @contextmanager
+    def open(
+        self, path: PurePath, mode: FolderResourceModeBin = "rb"
+    ) -> Iterator[io.FileIO]: ...
+
+    @contextmanager
+    def open(
+        self, path: PurePath, mode: FolderResourceMode = "r"
+    ) -> Iterator[io.TextIOWrapper | io.FileIO]:
+        session = self.account.session()
+
+        yield so.open(
+            self.full_path(path),
+            mode,
+            transport_params={"client": session.client("s3")},
+        )
+
+    def exists(self, path: PurePath) -> bool:
+        session = self.account.session()
+
+        try:
+            with so.open(
+                self.full_path(path), transport_params={"client": session.client("s3")}
+            ):
+                return True
+        except ValueError:
+            return False
